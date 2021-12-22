0c1d48cc23f6c6e40acc928d1fd3a1bc7645dce1
Igor Antonov <antonov.igor.khv@gmail.com>
2021-12-22 14:20:45 +0300

    1.9. Инициализирует менеджер, зарегистрирует команды.
    Подключаем команды в точке входа, регистрируем команды.

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,8 @@
-// Put your awesome code here…
+import CLIApplication from './cli/cli-application.js';
+import HelpCommand from './cli/commands/help-command.js';
+import ImportCommand from './cli/commands/import-command.js';
+import VersionCommand from './cli/commands/version-command.js';
+
+const myManager = new CLIApplication();
+myManager.registerCommands([HelpCommand, ImportCommand, VersionCommand]);
+myManager.processCommand(process.argv);
