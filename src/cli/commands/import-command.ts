fd96725ff8979b84f035974dcdb02964b49ae191
Igor Antonov <antonov.igor.khv@gmail.com>
2022-01-18 20:35:24 +0300

    2.9. Обновим ImportCommand
    Воспользуемся новым классом в ImportCommand. Интерфейс соблюдён, поэтому изменения минимальны. Меняем TSVFileReader на AsynTSVFileReader и обновляем вызов метода toArray(), так как теперь нам возвращается промис.
    
    Можно попробовать запустить импорт на тестовом файле (пока не сильно большом). Импорт проходит успешно, следовательно, воспользоваться потоками у нас получилось. Но решена ли задача? Попробуйте сформировать файл на гигабайта полтора и импортировать из него данные.
    
    Спустя некоторое время приложение завершится с ошибкой. Почему? Дело в том, что мы привысили размер HEAP. Каждый импортированный объект занимает какой-то объём памяти, а память небесконечна. Вот поэтому ошибка и возникла. То есть ошибка не в том, что Node.js не справилась с обработкой большого файла, а ошибка в том, что случилось переполнение. Что делать?
    
    Решений всегда несколько. Самый банальный — воспользоваться флагом --max-old-space-size и задать больший объём памяти. Если нам действительно нужно, чтобы в памяти были все объекты, то это корректное решение.

--- src/cli/commands/import-command.ts
+++ src/cli/commands/import-command.ts
@@ -1,14 +1,14 @@
-import CSVFileReader from '../csv-file-reader.js';
 import { Command } from '../types/command.js';
+import AsyncTSVFileReader from '../tsv-async-reader.js';
 
 export default class ImportCommand implements Command {
   public readonly name = '--import';
   public execute(filename: string): void {
-    const fileReader = new CSVFileReader(filename.trim());
+    const fileReader = new AsyncTSVFileReader(filename.trim());
 
     try {
       fileReader.read();
-      console.log(fileReader.toArray());
+      fileReader.toArray().then((tickets) => console.log(tickets));
     } catch (err) {
 
       if (!(err instanceof Error)) {
