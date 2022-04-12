455578651a203fc8a5d33d51c43ece175ec3016a
Igor Antonov <antonov.igor.khv@gmail.com>
2022-04-12 13:42:20 +0300

    1.8. Добавит метод parseCommand менеджеру. WIP
    Менеджер умеет регистрировать список команд. Теперь научим разбирать пользовательский ввод. Например, если пользователь вводит `--help` нам необходимо найти команду `HelpCommand` и запустить её. Решим пока первую задачу — разобрать ввод.
    
    Для этого создадим метод `parseCommand`. Аргументом он принимает список аргументов, которые передал пользователь в терминале. Разобранные команды сохраним в виде объекта.
    
    WIP: Линтер ругается на неиспользуемые переменные.

--- src/app/cli-application.ts
+++ src/app/cli-application.ts
@@ -7,6 +7,22 @@ type ParsedCommand = {
 export default class CLIApplication {
   private commands: {[propertyName: string]: CliCommandInterface} = {};
 
+  private parseCommand(cliArguments: string[]): ParsedCommand {
+    const parsedCommand: ParsedCommand = {};
+    let command = '';
+
+    return cliArguments.reduce((acc, item) => {
+      if (item.startsWith('--')) {
+        acc[item] = [];
+        command = item;
+      } else if (command && item) {
+        acc[command].push(item);
+      }
+
+      return acc;
+    }, parsedCommand);
+  }
+
   public registerCommands(commandList: CliCommandInterface[]): void {
     this.commands = commandList.reduce((acc, Command) => {
       const cliCommand = Command;
