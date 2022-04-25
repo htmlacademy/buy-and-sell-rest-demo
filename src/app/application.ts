71348959e6747409f7422312ac585726a08e2d4e
Igor Antonov <antonov.igor.khv@gmail.com>
2022-04-25 18:09:14 +0300

    3.10. Добавит ConfigService в Application.
    В `main.ts` добавим создание экземпляра класса `ConfigService`. При
    создании экземпляра передадим в конструктор настроенный `logger`.
    
    Настроенный экземпляр `config` передадим в конструктор `Application`
    вторым аргументом.
    
    Внутри класса `Application` добавим один параметр для конструктора
    (для приёма экземпляра `ConfigService`).
    
    В методе `init` добавим вывод значения переменной `PORT`. Пока только
    для проверки.
    
    Протестируйте пример на данном шаге. Создайте в корне проекта
    файл `.env` и определите в нём переменную `PORT=5000`. Затем запустите
    приложение. Значение переменной должно быть выведено в терминал.

--- src/app/application.ts
+++ src/app/application.ts
@@ -1,13 +1,17 @@
 import {LoggerInterface} from '../common/logger/logger.interface.js';
+import {ConfigInterface} from '../common/config/config.interface.js';
 
 export default class Application {
   private logger!: LoggerInterface;
+  private config!: ConfigInterface;
 
-  constructor(logger: LoggerInterface) {
+  constructor(logger: LoggerInterface, config: ConfigInterface) {
     this.logger = logger;
+    this.config = config;
   }
 
   public async init() {
     this.logger.info('Application initialization…');
+    this.logger.info(`Get value from env $PORT: ${this.config.get('PORT')}`);
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
 import Application from './app/application.js';
 import LoggerService from './common/logger/logger.service.js';
+import ConfigService from './common/config/config.service.js';
 
 const logger = new LoggerService();
+const config = new ConfigService(logger);
 
-const application = new Application(logger);
+const application = new Application(logger, config);
 await application.init();
