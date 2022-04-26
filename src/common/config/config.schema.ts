ab623d64614f95d1a2eb1f2fe317d797be924caa
Igor Antonov <antonov.igor.khv@gmail.com>
2022-04-26 12:46:02 +0300

    3.15. Расширит тип ConfigSchema.
    Пока мы используем только одну переменную окружения, но нам вскоре
    потребуется больше. Потренируем подключать новые переменные окружения.
    
    Расширим определение типа `ConfigSchema`. Добавим `SALT` и `DB_HOST`.
    Напомним, имена соответствуют одноимённым переменным окружения.
    
    Через `SALT` будем передавать значение «соли» — случайный набор
    символов для хеширования пароля.
    
    `DB_HOST` будет содержать IP-адрес сервера баз данных. Что такое и как
    использовать базы данных узнаем позже, а пока только подготовим
    переменную.
    
    Обратите внимание на описание `SALT` в схеме. Значение «соли»
    обязательно должно задаваться снаружи. Поэтому в качестве значения
    указываем `null`. Если забыть установить переменную в `.env`, то
    `convict` выбросит исключение (это то что нам нужно).
    
    В переменной `DB_HOST` мы используем внешний форматер. Форматер
    (`format`) — функция, которая знает как проверить то или иное значение.
    В качестве значения мы указываем `ipaddress`.
    
    `ipaddress` — это внешний форматер. Мы подключили его из пакета
    `convict-format-with-validator`. В нём есть и другие форматеры.
    
    Повторимся, форматер — это функция. Вы можете определять собственные
    форматеры.

--- src/common/config/config.schema.ts
+++ src/common/config/config.schema.ts
@@ -1,7 +1,12 @@
 import convict from 'convict';
+import validator from 'convict-format-with-validator';
+
+convict.addFormats(validator);
 
 export type ConfigSchema = {
   PORT: number;
+  SALT: string;
+  DB_HOST: string;
 }
 
 export const configSchema = convict<ConfigSchema>({
@@ -11,6 +16,16 @@ export const configSchema = convict<ConfigSchema>({
     env: 'PORT',
     default: 4000
   },
+  SALT: {
+    doc: 'Salt for password hash',
+    format: String,
+    env: 'SALT',
+    default: null
+  },
+  DB_HOST: {
+    doc: 'IP address of the database server (MongoDB)',
+    format: 'ipaddress',
+    env: 'DB_HOST',
+    default: '127.0.0.1'
+  }
 });
-
-
