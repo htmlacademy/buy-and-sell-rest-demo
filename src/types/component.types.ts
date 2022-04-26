4cdac2fcb08a0ea45d12e1f458fdb4da8d8b5cbd
Igor Antonov <antonov.igor.khv@gmail.com>
2022-04-26 14:55:13 +0300

    3.17. Добавит тип Component.
    Подготовим новый модуль с перечислением компонентов нашего приложения,
    которые будут участвовать при внедрении зависимостей.
    
    Новый модуль создаём в директории `types`, хотя фактически это не
    является описание типа в контексте TypeScript. Мы описываем простой
    константный объект. Ключи объекта — название компонентов, а если точней
    интерфейсов. Значения ключей — символы. Можно использовать строки, но
    символы уникальны «из коробки».
    
    Объект нам потребуется при настройки контейнера.

---
+++ src/types/component.types.ts
@@ -0,0 +1,5 @@
+export const Component = {
+  Application: Symbol.for('Application'),
+  LoggerInterface: Symbol.for('LoggerInterface'),
+  ConfigInterface: Symbol.for('ConfigInterface'),
+} as const;
