28a50ab0d3aad49099bd11b9646e4e76908682dc
Igor Antonov <antonov.igor.khv@gmail.com>
2022-04-28 21:43:29 +0300

    4.2. Добавит описание новых переменных окружения.
    Для установки соединения с базой данных нам потребуются дополнительные
    сведения. Для этого определим несколько переменных окружения:
    имя пользователя (`DB_USER`), пароль пользователя (`DB_PASSWORD`),
    порт сервера БД (`DB_PORT`) и название базы данных (`DB_NAME`).
    
    Опишем перечисленные переменные в схеме конфигурации
    (`config.schema.ts`). Обратите внимание, сначала расширяем тип
    `ConfigSchema`, а затем добавляем описание полей в саму схему.
    
    При описании полей в схеме следует помнить: конфиденциальные сведения
    не должны попадать в систему контроля версий. Поэтому для имени
    пользователя (`DB_USER`) и пароля (`DB_PASSWORD`) в качестве значения
    по умолчанию мы используем `null`. Это означает, что значения для этих
    переменных придётся явно прописать в файле `.env`, иначе возникнет
    ошибка.

--- src/common/config/config.schema.ts
+++ src/common/config/config.schema.ts
@@ -7,6 +7,10 @@ export type ConfigSchema = {
   PORT: number;
   SALT: string;
   DB_HOST: string;
+  DB_USER: string;
+  DB_PASSWORD: string;
+  DB_PORT: number;
+  DB_NAME: string;
 }
 
 export const configSchema = convict<ConfigSchema>({
@@ -27,5 +31,29 @@ export const configSchema = convict<ConfigSchema>({
     format: 'ipaddress',
     env: 'DB_HOST',
     default: '127.0.0.1'
+  },
+  DB_USER: {
+    doc: 'Username to connect to the database (MongoDB)',
+    format: String,
+    env: 'DB_USER',
+    default: null,
+  },
+  DB_PASSWORD: {
+    doc: 'Database connection password (MongoDB)',
+    format: String,
+    env: 'DB_PASSWORD',
+    default: null,
+  },
+  DB_PORT: {
+    doc: 'Port to connect to the database (MongoDB)',
+    format: 'port',
+    env: 'DB_PORT',
+    default: 27017,
+  },
+  DB_NAME: {
+    doc: 'Database name (MongoDB)',
+    format: String,
+    env: 'DB_NAME',
+    default: 'course-nodejs-restapi'
   }
 });
