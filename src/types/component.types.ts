e29828fa83ba214172b6d44bd091127fcad30ae7
Igor Antonov <antonov.igor.khv@gmail.com>
2022-04-29 16:11:05 +0300

    4.6. Добавит новый компонент в Component.
    В объект `Component` добавим новое значение — `DatabaseInterface`.
    Это необходимо, чтобы добавить связку интерфейса `DatabaseInterface`
    с определённым классом в IoC-контейнере.

--- src/types/component.types.ts
+++ src/types/component.types.ts
@@ -2,4 +2,5 @@ export const Component = {
   Application: Symbol.for('Application'),
   LoggerInterface: Symbol.for('LoggerInterface'),
   ConfigInterface: Symbol.for('ConfigInterface'),
+  DatabaseInterface: Symbol.for('DatabaseInterface')
 } as const;
