9d3ef8cfba5da7d0c2f853dc4a87920379a092c8
Igor Antonov <antonov.igor.khv@gmail.com>
2022-04-29 16:18:14 +0300

    4.7. Добавит привязку для DatabaseInterface.
    Добавим в контейнер новую привязку. На этот раз для `DatabaseInterface`.
    Свяжем с реализацией `DatabaseService`. Не забудем указать
    `inSingletonScope`. Достаточно одного экземпляра.

--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,14 @@ import {Component} from './types/component.types.js';
 import {ConfigInterface} from './common/config/config.interface.js';
 import ConfigService from './common/config/config.service.js';
 import Application from './app/application.js';
+import DatabaseService from './common/database-client/database.service.js';
+import {DatabaseInterface} from './common/database-client/database.interface.js';
 
 const applicationContainer = new Container();
 applicationContainer.bind<Application>(Component.Application).to(Application).inSingletonScope();
 applicationContainer.bind<LoggerInterface>(Component.LoggerInterface).to(LoggerService).inSingletonScope();
 applicationContainer.bind<ConfigInterface>(Component.ConfigInterface).to(ConfigService).inSingletonScope();
+applicationContainer.bind<DatabaseInterface>(Component.DatabaseInterface).to(DatabaseService).inSingletonScope();
 
 const application = applicationContainer.get<Application>(Component.Application);
 await application.init();
