4e1620fd32d9ea3485c25c2238b935743bfab91a
Igor Antonov <antonov.igor.khv@gmail.com>
2022-04-29 16:24:11 +0300

    4.8. Завершит настройку подключения к базе данных.
    Для установки соединения с базой данных всё готово. Остаётся внедрить
    `DatabaseService` в виде зависимости в `Application` и воспользоваться
    им в методе `init`. Внедрять будем по аналогии с другими
    зависимостями — через конструктор.
    
    В методе `init` воспользуемся функцией `getURI` и передадим ей значения
    всех переменных окружения. Получим строку соединения, а затем передадим
    аргументом в метод `connect`.
    
    На этом шаге попробуйте запустить приложение и убедиться, что
    соединение с сервером баз данных успешно устанавливается. Перед
    запуском не забудьте проверить установку всех переменных окружения
    в файле `.env`.
    
    И последнее, на что следует обратить внимание. Теперь наше приложение
    не завершает работу, оно остаётся запущенным.

--- src/app/application.ts
+++ src/app/application.ts
@@ -3,15 +3,29 @@ import {inject, injectable} from 'inversify';
 import {LoggerInterface} from '../common/logger/logger.interface.js';
 import {ConfigInterface} from '../common/config/config.interface.js';
 import {Component} from '../types/component.types.js';
+import {getURI} from '../utils/db.js';
+import {DatabaseInterface} from '../common/database-client/database.interface.js';
 
 @injectable()
 export default class Application {
   constructor(
     @inject(Component.LoggerInterface) private logger: LoggerInterface,
-    @inject(Component.ConfigInterface) private config: ConfigInterface) {}
+    @inject(Component.ConfigInterface) private config: ConfigInterface,
+    @inject(Component.DatabaseInterface) private databaseClient: DatabaseInterface
+  ) {}
 
   public async init() {
     this.logger.info('Application initialization…');
     this.logger.info(`Get value from env $PORT: ${this.config.get('PORT')}`);
+
+    const uri = getURI(
+      this.config.get('DB_USER'),
+      this.config.get('DB_PASSWORD'),
+      this.config.get('DB_HOST'),
+      this.config.get('DB_PORT'),
+      this.config.get('DB_NAME'),
+    );
+
+    await this.databaseClient.connect(uri);
   }
 }
