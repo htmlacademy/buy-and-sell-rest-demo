58856416b09bec46178ec01c663e7fbffc9b053a
Igor Antonov <antonov.igor.khv@gmail.com>
2022-05-02 13:45:08 +0300

    4.22. Добавит внедрение зависимостей в UserService. WIP
    Воспользуемся декоратором `inject` и внедрим в конструктор `UserService`
    в качестве зависимости `logger`. После этого внутри класса `UserService`
    мы сможем залогировать событие через `this.logger.<method>`.
    
    Последним шагом добавим для класса `UserService` декоратор `injectable`.
    Этим классом мы будем пользоваться из разных мест приложения,
    поэтому сразу поместим его в контейнер. Для этого откроем объект
    `Component` и добавим ещё один компонент — `UserServiceInterface`.
    А затем в `main.ts` добавим для него привязку.
    
    WIP: Имплементирован только один метод интерфейса `UserServiceInterface`.
    Линтер ругается.

--- src/main.ts
+++ src/main.ts
@@ -7,12 +7,16 @@ import ConfigService from './common/config/config.service.js';
 import Application from './app/application.js';
 import DatabaseService from './common/database-client/database.service.js';
 import {DatabaseInterface} from './common/database-client/database.interface.js';
+import UserService from './modules/user/user.service.js';
+import {UserServiceInterface} from './modules/user/user-service.interface.js';
 
 const applicationContainer = new Container();
 applicationContainer.bind<Application>(Component.Application).to(Application).inSingletonScope();
 applicationContainer.bind<LoggerInterface>(Component.LoggerInterface).to(LoggerService).inSingletonScope();
 applicationContainer.bind<ConfigInterface>(Component.ConfigInterface).to(ConfigService).inSingletonScope();
 applicationContainer.bind<DatabaseInterface>(Component.DatabaseInterface).to(DatabaseService).inSingletonScope();
+applicationContainer.bind<UserServiceInterface>(Component.UserServiceInterface).to(UserService);
+
 
 const application = applicationContainer.get<Application>(Component.Application);
 await application.init();
