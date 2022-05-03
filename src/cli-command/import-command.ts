b511622a72707da31a12ded5e065c98b4f47afca
Igor Antonov <antonov.igor.khv@gmail.com>
2022-05-03 16:53:22 +0300

    4.31. Добавит поддержку БД в команду import.
    Последним шагом добавим поддержку сохранения данных в базу данных для
    команды `--import`. Для CLI мы не будем собирать отдельный контейнер
    зависимостей. Всё необходимое передадим вручную. Данные для подключения
    к базе попросим напрямую у пользователя. Сценарий считывания настроек
    из `.env` здесь плохо подходит.

--- src/cli-command/import-command.ts
+++ src/cli-command/import-command.ts
@@ -1,21 +1,83 @@
 import TSVFileReader from '../common/file-reader/tsv-file-reader.js';
 import { CliCommandInterface } from './cli-command.interface.js';
 import {createOffer, getErrorMessage} from '../utils/common.js';
+import DatabaseService from '../common/database-client/database.service.js';
+import ConsoleLoggerService from '../common/logger/console-logger.service.js';
+import {getURI} from '../utils/db.js';
+import {UserServiceInterface} from '../modules/user/user-service.interface.js';
+import {CategoryServiceInterface} from '../modules/category/category-service.interface.js';
+import {OfferServiceInterface} from '../modules/offer/offer-service.interface.js';
+import UserService from '../modules/user/user.service.js';
+import OfferService from '../modules/offer/offer.service.js';
+import {OfferModel} from '../modules/offer/offer.entity.js';
+import CategoryService from '../modules/category/category.service.js';
+import {CategoryModel} from '../modules/category/category.entity.js';
+import {UserModel} from '../modules/user/user.entity.js';
+import {Offer} from '../types/offer.type.js';
+import {LoggerInterface} from '../common/logger/logger.interface.js';
+import {DatabaseInterface} from '../common/database-client/database.interface.js';
+
+const DEFAULT_DB_PORT = 27017;
+const DEFAULT_USER_PASSWORD = '123456';
 
 export default class ImportCommand implements CliCommandInterface {
   public readonly name = '--import';
+  private userService!: UserServiceInterface;
+  private categoryService!: CategoryServiceInterface;
+  private offerService!: OfferServiceInterface;
+  private databaseService!: DatabaseInterface;
+  private logger: LoggerInterface;
+  private salt!: string;
 
-  private onLine(line: string) {
+  constructor() {
+    this.onLine = this.onLine.bind(this);
+    this.onComplete = this.onComplete.bind(this);
+
+    this.logger = new ConsoleLoggerService();
+    this.offerService = new OfferService(this.logger, OfferModel);
+    this.categoryService = new CategoryService(this.logger, CategoryModel);
+    this.userService = new UserService(this.logger, UserModel);
+    this.databaseService = new DatabaseService(this.logger);
+  }
+
+  private async saveOffer(offer: Offer) {
+    const categories = [];
+    const user = await this.userService.findOrCreate({
+      ...offer.user,
+      password: DEFAULT_USER_PASSWORD
+    }, this.salt);
+
+    for (const {name} of offer.categories) {
+      const existCategory = await this.categoryService.findByCategoryNameOrCreate(name, {name});
+      categories.push(existCategory.id);
+    }
+
+    await this.offerService.create({
+      ...offer,
+      categories,
+      userId: user.id,
+    });
+  }
+
+  private async onLine(line: string, resolve: () => void) {
     const offer = createOffer(line);
-    console.log(offer);
+    await this.saveOffer(offer);
+    resolve();
   }
 
   private onComplete(count: number) {
     console.log(`${count} rows imported.`);
+    this.databaseService.disconnect();
   }
 
-  public async execute(filename: string): Promise<void> {
+  public async execute(filename: string, login: string, password: string, host: string, dbname: string, salt: string): Promise<void> {
+    const uri = getURI(login, password, host, DEFAULT_DB_PORT, dbname);
+    this.salt = salt;
+
+    await this.databaseService.connect(uri);
+
     const fileReader = new TSVFileReader(filename.trim());
+
     fileReader.on('line', this.onLine);
     fileReader.on('end', this.onComplete);
 
