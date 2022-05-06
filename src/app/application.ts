7b39a17b61e6bf5cff49ce3726cd1074eab3dd94
Igor Antonov <antonov.igor.khv@gmail.com>
2022-05-06 17:04:23 +0300

    5.3. Добавит создание и запуск сервера Express.
    Добавим создание экземпляра сервера с помощью Express в класс
    `Application`. Экземпляр сохраним в приватном поле `expressApp`.
    После этого в методе `init` воспользуемся экземпляром `express` и
    запустим сервер на прослушивание входящих подключений. Для этого
    вызовем метод `listen`. Ему нужно пережать порт, на который будем
    принимать подключения. Номер порта возьмём из конфигурации (`PORT`).

--- src/app/application.ts
+++ src/app/application.ts
@@ -1,5 +1,6 @@
 import 'reflect-metadata';
 import {inject, injectable} from 'inversify';
+import express, {Express} from 'express';
 import {LoggerInterface} from '../common/logger/logger.interface.js';
 import {ConfigInterface} from '../common/config/config.interface.js';
 import {Component} from '../types/component.types.js';
@@ -8,11 +9,15 @@ import {DatabaseInterface} from '../common/database-client/database.interface.js
 
 @injectable()
 export default class Application {
+  private expressApp: Express;
+
   constructor(
     @inject(Component.LoggerInterface) private logger: LoggerInterface,
     @inject(Component.ConfigInterface) private config: ConfigInterface,
     @inject(Component.DatabaseInterface) private databaseClient: DatabaseInterface
-  ) {}
+  ) {
+    this.expressApp = express();
+  }
 
   public async init() {
     this.logger.info('Application initialization…');
@@ -27,5 +32,8 @@ export default class Application {
     );
 
     await this.databaseClient.connect(uri);
+
+    this.expressApp.listen(this.config.get('PORT'));
+    this.logger.info(`Server started on http://localhost:${this.config.get('PORT')}`);
   }
 }
