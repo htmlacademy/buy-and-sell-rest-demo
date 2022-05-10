8b33793ee6bd83411a386c0399c30ec2bf588cc0
Igor Antonov <antonov.igor.khv@gmail.com>
2022-05-10 14:23:28 +0300

    5.32. Добавит создание пользователей в UserController.
    Последним шагом закончим с обработчиком `create` в `UserController`.
    Он отвечает за добавление новых пользователей в базу данных. Проведём
    похожие действия, как и при создании `CategoryController`. Сначала
    проверим существование пользователя. Если он существует, то вернём
    код `409`. Обратите внимание, мы напрямую не вызываем `send`. Вместо
    этого бросаем ошибку типа `HttpError`.
    
    Если всё хорошо и пользователь создан, то вернём объект пользователя
    без поля «пароль». Структуру объекта для клиента предварительно описали
    в `UserDto`.

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -3,13 +3,21 @@ import {inject, injectable} from 'inversify';
 import {Component} from '../../types/component.types.js';
 import {LoggerInterface} from '../../common/logger/logger.interface.js';
 import {HttpMethod} from '../../types/http-method.enum.js';
-import {NextFunction, Request, Response} from 'express';
+import {Request, Response} from 'express';
 import CreateUserDto from './dto/create-user.dto.js';
+import {UserServiceInterface} from './user-service.interface.js';
+import HttpError from '../../common/errors/http-error.js';
+import {StatusCodes} from 'http-status-codes';
+import {fillDTO} from '../../utils/common.js';
+import UserDto from './dto/user.dto.js';
+import {ConfigInterface} from '../../common/config/config.interface.js';
 
 @injectable()
 export default class UserController extends Controller {
   constructor(
     @inject(Component.LoggerInterface) logger: LoggerInterface,
+    @inject(Component.UserServiceInterface) private readonly userService: UserServiceInterface,
+    @inject(Component.ConfigInterface) private readonly configService: ConfigInterface,
   ) {
     super(logger);
     this.logger.info('Register routes for UserController…');
@@ -18,11 +26,24 @@ export default class UserController extends Controller {
   }
 
   public async create(
-    _req: Request<Record<string, unknown>, Record<string, unknown>, CreateUserDto>,
-    _res: Response,
-    _next: NextFunction
+    {body}: Request<Record<string, unknown>, Record<string, unknown>, CreateUserDto>,
+    res: Response,
   ): Promise<void> {
+    const existsUser = await this.userService.findByEmail(body.email);
 
-    throw new Error('[UserController] Oops');
+    if (existsUser) {
+      throw new HttpError(
+        StatusCodes.CONFLICT,
+        `User with email «${body.email}» exists.`,
+        'UserController'
+      );
+    }
+
+    const result = await this.userService.create(body, this.configService.get('SALT'));
+    this.send(
+      res,
+      StatusCodes.CREATED,
+      fillDTO(UserDto, result)
+    );
   }
 }
