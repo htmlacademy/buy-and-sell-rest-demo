7bf851ee25013e5d2efd3428d5e4e06c94b8289a
Igor Antonov <antonov.igor.khv@gmail.com>
2022-05-22 19:15:19 +0300

    7.8. Добавит DocumentExistsInterface и DocumentExistsMiddleware.
    В обработчиках маршрутах, где в пути используется идентификатор
    документа в БД нам приходится делать одну и ту же проверку: проверить
    существование документа, а только потом выполнять основную операцию.
    В этом нет ничего страшного, но код контроллеров немного раздувается
    и появляется дополнительный шум.
    
    Можно немного сократить объём кода, если перенести задачу проверки
    сущности в `middleware`. Проверим на практике. Создадим новую
    middleware `DocumentExistsMiddleware`.
    
    Чтобы из middleware проверить наличие документа в базе нам потребуется
    определённый сервис. Общего интерфейса для всех сервисов у нас нет.
    Можно перечислить все созданные сервисы, но этот вариант не сильно
    гибкий. Придётся поддерживать список сервисов в актуальном виде, да
    ещё проследить, чтобы у всех сервисов был метод `exists`.
    
    Оптимальный создать отдельный интерфейс `DocumentExistsInterface` и в
    нём описать метод `exists`. Поскольку метод `exists` не привязан к
    определённой сущности, мы сможем все интерфейсы сервисов, где
    требуется реализация метода — унаследовать от него.
    
    Метод `execute` у middleware выглядит достаточно простым. Мы извлекаем
    значение параметра, содержащего ID проверяемого документа и обращаемся
    к методу `exists` у сервиса. Если документ не существует — кидаем
    ошибку.

---
+++ src/common/middlewares/document-exists.middleware.ts
@@ -0,0 +1,26 @@
+import {MiddlewareInterface} from '../../types/middleware.interface.js';
+import {NextFunction, Request, Response} from 'express';
+import {DocumentExistsInterface} from '../../types/document-exists.interface.js';
+import HttpError from '../errors/http-error.js';
+import {StatusCodes} from 'http-status-codes';
+
+export class DocumentExistsMiddleware implements MiddlewareInterface {
+  constructor(
+    private readonly service: DocumentExistsInterface,
+    private readonly entityName: string,
+    private readonly paramName: string,
+  ) {}
+
+  public async execute({params}: Request, _res: Response, next: NextFunction): Promise<void> {
+    const documentId = params[this.paramName];
+    if (!await this.service.exists(documentId)) {
+      throw new HttpError(
+        StatusCodes.NOT_FOUND,
+        `${this.entityName} with ${documentId} not found.`,
+        'DocumentExistsMiddleware'
+      );
+    }
+
+    next();
+  }
+}
