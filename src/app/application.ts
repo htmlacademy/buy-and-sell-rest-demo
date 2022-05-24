96146a1155e0c8c93e30aa6dcef345002fa5551a
Igor Antonov <antonov.igor.khv@gmail.com>
2022-05-24 16:58:38 +0300

    7.24. Подключит AuthenticateMiddleware.
    Теперь подключим middleware в классе `Application`. Для этого
    модифицируем метод `registerMiddlewares`. При подключении не забудьте
    про контекст. «Прибьём» нужный контекст с помощью `bind`.

--- src/app/application.ts
+++ src/app/application.ts
@@ -9,6 +9,7 @@ import {DatabaseInterface} from '../common/database-client/database.interface.js
 import {ControllerInterface} from '../common/controller/controller.interface.js';
 import {ExceptionFilterInterface} from '../common/errors/exception-filter.interface.js';
 import UserController from '../modules/user/user.controller.js';
+import {AuthenticateMiddleware} from '../common/middlewares/authenticate.middleware.js';
 
 @injectable()
 export default class Application {
@@ -40,6 +41,9 @@ export default class Application {
       '/upload',
       express.static(this.config.get('UPLOAD_DIRECTORY'))
     );
+
+    const authenticateMiddleware = new AuthenticateMiddleware(this.config.get('JWT_SECRET'));
+    this.expressApp.use(authenticateMiddleware.execute.bind(authenticateMiddleware));
   }
 
   public registerExceptionFilters() {
