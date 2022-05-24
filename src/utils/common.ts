0284aaa379373bd5a959311f3b3230f4943c9e08
Igor Antonov <antonov.igor.khv@gmail.com>
2022-05-24 16:33:06 +0300

    7.21. Добавит функцию `createJWT`.
    Создавать новый токен мы будем в контроллере `UserController`. Нам для
    этого достаточно воспользоваться классом `SignJWT` из пакета `jose`.
    Как говорили выше, пакет `jose` предоставляет удобный API. В принципе,
    можем напрямую вызывать нужные методы прямо в `UserController`, но лучше
    сделаем небольшую функцию-обёртку — `createJWT`.
    
    Функция определяет несколько параметров:
    
    1. Алгоритм шифрования.
    2. Секрет
    3. Полезная нагрузка (payload). Данные, которые хотим поместить в токен.
    
    В качестве значения для первого аргумента мы будем использовать
    `HS256`. Сразу создадим для него отдельную константу в
    `user.constant.ts`.
    
    Внутри `createJWT` создадим экземпляр класса `SignJWT`. В конструктор
    ему передадим полезную нагрузку. Под полезной нагрузкой подразумеваются
    любые данные, которые мы хотим отправить клиенту вместе с токеном. Как
    правило, такими данными являются `email` и `id` пользователя. Понять
    какие данные следует поместить в токен можно на основании сценариев,
    в которых мы хотим их использовать. Нам точно потребуется идентификатор,
    чтобы подставлять его при создании каких-либо сущностей.
    
    Затем мы вызываем цепочку методов. Каждый метод решает маленькую
    задачу в формировании токена. `setProtectedHeader` подготовит заголовок
    в соответствии с алгоритмом шифрования, `setIssuedAt` заполнит поле
    `iat` (дата выпуска токена), `setExpirationTime` (установит дату
    истечения срока действия токена).
    
    В конце цепочки вызываем метод `sign`. Он сформирует сам токен.
    Аргументами передадим `SecretKey`. Получить секретный ключ поможет метод
    `createSecretkey` из встроенного модуля `crypto`.

--- src/utils/common.ts
+++ src/utils/common.ts
@@ -1,3 +1,4 @@
+import * as jose from 'jose';
 import crypto from 'crypto';
 import {plainToInstance} from 'class-transformer';
 import {ClassConstructor} from 'class-transformer/types/interfaces/class-constructor.type.js';
@@ -34,3 +35,10 @@ export const fillDTO = <T, V>(someDto: ClassConstructor<T>, plainObject: V) =>
 export const createErrorObject = (message: string) => ({
   error: message,
 });
+
+export const createJWT = async (algoritm: string, jwtSecret: string, payload: object): Promise<string> =>
+  new jose.SignJWT({...payload})
+    .setProtectedHeader({ alg: algoritm})
+    .setIssuedAt()
+    .setExpirationTime('2d')
+    .sign(crypto.createSecretKey(jwtSecret, 'utf-8'));
