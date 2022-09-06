610a6cfba43add877fbe728cd363fbed4301a21e
Igor Antonov <antonov.igor.khv@gmail.com>
2022-09-06 15:17:03 +0300

    1.2. Добавит команду для получения версии.
    Создадим новый модуль `version-command.ts` и опишем класс, который имплементирует ранее созданный интерфейс (`CliCommandInterface`).
    
    Команда `--version` должна считывать из `package.json` версию приложения.
    
    Для чтения файла воспользуемся синхронным API. Для CLI приложений это вполне допустимо. Импортируем из модуля `fs` функцию `readFileSync` и прочитаем с её помощью файл `package.json`. Не забываем указывать кодировку.

---
+++ src/cli-command/version-command.ts
@@ -0,0 +1,17 @@
+import { readFileSync } from 'fs';
+import { CliCommandInterface } from './cli-command.interface.js';
+
+export default class VersionCommand implements CliCommandInterface {
+  public readonly name = '--version';
+
+  private readVersion(): string {
+    const contentPageJSON = readFileSync('./package.json', 'utf-8');
+    const content = JSON.parse(contentPageJSON);
+    return content.version;
+  }
+
+  public async execute(): Promise<void> {
+    const version = this.readVersion();
+    console.log(version);
+  }
+}
