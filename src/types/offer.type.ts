0783b94f26ada5310ee51c0d144ad626cc0c0dd1
Igor Antonov <antonov.igor.khv@gmail.com>
2022-09-06 15:26:20 +0300

    1.10. Добавит типы для импортируемых сущностей.
    Перед тем, как написать код для считывания данных, подумаем о структурах и типах. Мы считываем данные определённой «формы». Подготовим описание типов.
    
    Создадим директорию `types` и опишем в ней общие типы. Для удобства каждый тип опишем в отдельном модуле. Нам потребуется несколько типов:
    
    - Объявление (Offer);
    - Категория (Category);
    - Пользователь (User);
    - Тип объявления (OfferType).
    
    При описании типов важно опираться на техническое задание, чтобы ничего не пропустить.

---
+++ src/types/offer.type.ts
@@ -0,0 +1,14 @@
+import { Category } from './category.type.js';
+import { OfferType } from './offer-type.enum.js';
+import { User } from './user.type.js';
+
+export type Offer = {
+  title: string;
+  description: string;
+  postDate: Date;
+  image: string;
+  type: OfferType
+  price: number;
+  categories: Category[];
+  user: User;
+}
