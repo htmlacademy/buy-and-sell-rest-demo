d1b48f33ab6de8fdf4560121277989b911ee7d8e
Igor Antonov <antonov.igor.khv@gmail.com>
2022-09-09 14:51:35 +0300

    2.18. Добавит класс TSVFileWriter.
    Потоки можно использовать не только для чтения, но и для записи. Для
    эксперимента обновим подход к записи информации в файл.
    
    Создадим интерфейс `FileWriteInterface` и класс `TSVFileWriter`. Внутри
    класса воспользуемся `createWriteStream` для создания потока на запись.
    
    Аргументами передадим путь к файлу (в него будем записывать информацию)
    и объект настроек. В объекте настроек передадим опцию `w`. Файл
    открывается на запись. Если файл отсутствует — он будет создан. Также
    указываем кодировку и размер «порции» данных. Ставим 64KB.
    
    Затем имплементируем метод `write`. Аргументом он принимает строку для
    записи в поток. Внутри вызываем метод `write`. Важно не забыть, что
    скорость записи ниже, чем скорость подготовки данных.
    
    Чтобы не случилось переполнения буфера, подпишемся на событие `drain`.
    Когда он сработает — возобновим запись.

---
+++ src/common/file-writer/tsv-file-writer.ts
@@ -0,0 +1,24 @@
+import { createWriteStream, WriteStream} from 'fs';
+import { FileWriterInterface } from './file-writer.interface.js';
+
+export default class TSVFileWriter implements FileWriterInterface {
+  private stream: WriteStream;
+
+  constructor(public readonly filename: string) {
+    this.stream = createWriteStream(this.filename, {
+      flags: 'w',
+      encoding: 'utf8',
+      highWaterMark: 2 ** 16, // 64KB
+      autoClose: true,
+    });
+  }
+
+  public async write(row: string): Promise<void> {
+    if (!this.stream.write(`${row}\n`)) {
+      return new Promise((resolve) => {
+        this.stream.once('drain', () => resolve());
+      });
+    }
+    return Promise.resolve();
+  }
+}
