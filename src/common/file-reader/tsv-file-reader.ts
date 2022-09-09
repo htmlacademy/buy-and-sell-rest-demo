7bae31a5bbb75fe17cb6133fbb245f936cd575bd
Igor Antonov <antonov.igor.khv@gmail.com>
2022-09-09 14:49:37 +0300

    2.16. Добавит потоки (stream). WIP
    Обновим метод `read`. Для чтения файлов воспользуемся потоками (Stream).
    Для этого импортируем функцию `createReadStream`. Он позволяет создать
    поток для чтения.
    
    Аргументами передадим путь к файлу, который необходимо прочитать и
    объект настроек. В объекте настроек передадим информацию о кодировке
    файла и объём порции данных. Именно столько данных будет считываться за
    один раз. По умолчанию значение равно 16KB, но при необходимости его
    можно увеличить или уменьшить.
    
    С потоками можно работать разными способами. Один из вариантов —
    итерироваться с помощью цикла `for of`. Обратите внимание, мы
    используем `await`.
    
    На каждой итерации происходит считывания порции данных. Обратите внимание,
    порция — не означает строка. Это может быть как часть строки или наоборот,
    строка и часть следующей строки. Получив порцию данных, её необходимо
    обработать.
    
    Для этого в цикле проверяем наличие символа перевода строки. Если он
    есть, значит строка считана и её можно передать дальше. Например, функции
    для создания объявления.
    
    Ну а если этого символа нет, нужно продолжить считывание файла.
    
    Чтобы оповестить подписчиков о считывании строки, используем метод
    `emit`. Этот метод нам предоставляет `EventEmitter`. Первым аргументом
    передаём название события (оно может быть любым), а во втором — данные.
    
    Все подписчики, которые оформили подписку на событие `line` смогу
    получить считанную строку.
    
    В самом конце отправляем ещё одно событие — `end`. Оно символизирует о
    завершении чтения файла. В качестве данных передадим количество
    импортированных из файла строк.
    
    WIP: Импорт не работает.

--- src/common/file-reader/tsv-file-reader.ts
+++ src/common/file-reader/tsv-file-reader.ts
@@ -1,4 +1,5 @@
 import EventEmitter from 'events';
+import { createReadStream } from 'fs';
 import { FileReaderInterface } from './file-reader.interface.js';
 
 export default class TSVFileReader extends EventEmitter implements FileReaderInterface {
@@ -6,7 +7,28 @@ export default class TSVFileReader extends EventEmitter implements FileReaderInt
     super();
   }
 
-  public async read(): Promise<void> {
-    // Код для работы с потоками
+  public async read():Promise<void> {
+    const stream = createReadStream(this.filename, {
+      highWaterMark: 16384, // 16KB
+      encoding: 'utf-8',
+    });
+
+    let lineRead = '';
+    let endLinePosition = -1;
+    let importedRowCount = 0;
+
+    for await (const chunk of stream) {
+      lineRead += chunk.toString();
+
+      while ((endLinePosition = lineRead.indexOf('\n')) >= 0) {
+        const completeRow = lineRead.slice(0, endLinePosition + 1);
+        lineRead = lineRead.slice(++endLinePosition);
+        importedRowCount++;
+
+        this.emit('line', completeRow);
+      }
+    }
+
+    this.emit('end', importedRowCount);
   }
 }
