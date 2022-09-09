44bc3796d0a9a9376862544eee1523e6f9b2ae02
Igor Antonov <antonov.igor.khv@gmail.com>
2022-09-09 14:26:57 +0300

    2.9. Добавит команду формирования объявлений. WIP
    Для запуска генератора объявлений подготовим новую команду —
    `GenerateCommand`. Как и предыдущие аналогичные команды, класс
    команды должен реализовать интерфейс `CliCommandInterface`.
    
    Команда `GenerateCommand` ожидает несколько аргументов:
    
    * `<count>` — обязательный. Количество объявлений для генерации.
    * `<filepath>` — обязательный. Путь к файлу для записи результата.
    * `<url>` — обязательный. URL сервиса (JSON-server).
    
    Вызов команды должен выглядеть так:
    
    ```
    $: ./cli.js --generate 10 ./mocks/mock-offers.tsv http://localhost:3123/api
    ```
    
    На первом шаге создадим заготовку для команды `GenerateCommand`.
    В методе `execute` подготовим значения, полученные от пользователя.
    
    Обратите внимание, все данные от пользователя приходят в виде строк.
    Поэтому важно не забыть привести данные к нужному типу (при необходимости).
    
    WIP: Линтер ругается на неиспользуемые переменные.

---
+++ src/cli-command/generate-command.ts
@@ -0,0 +1,12 @@
+import { MockData } from '../types/mock-data.type.js';
+import { CliCommandInterface } from './cli-command.interface.js';
+
+export default class GenerateCommand implements CliCommandInterface {
+  public readonly name = '--generate';
+  private initialData!: MockData;
+
+  public async execute(...parameters:string[]): Promise<void> {
+    const [count, filepath, url] = parameters;
+    const offerCount = Number.parseInt(count, 10);
+  }
+}
