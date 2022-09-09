3acb6a69da73253e64151830da9ba13814958a25
Igor Antonov <antonov.igor.khv@gmail.com>
2022-09-09 14:47:46 +0300

    2.14. Рефакторинг. Функция `createOffer`. WIP
    Приложение работает, но есть проблема: при попытке импортировать большой
    файл (> 2GB) возникнет ошибка — `ERR_FS_FILE_TOO_LARGE`.
    
    Можно предположить, что проблема проявляется из-за использования
    синхронной функции `readFileSync`. Но это не так. Переход на асинхронный
    вариант в виде `readFile` ничего не решит. Ошибка сохранится.
    
    Node.js ограничивает размер буфера, в который считывается файл. Размер
    буфера зависит от версии Node.js (x32/x64). В x64 он равен
    приблизительно 4GB. Однако в функциях для работы с файлами есть
    дополнительное ограничение — 2GB.
    
    Такие ограничения введены намеренно. Считываемый файл загружается
    целиком в оперативную память и памяти может не хватить. Да и вряд ли
    вам потребуется огроменный файл в оперативной памяти.
    
    Прочитать и поработать с большими файлами помогут потоки (Streams).
    
    Подходить к работе с потоками начнём с рефакторинга. Перенесём код для
    создания объявления из строки в отдельную функцию. Назовём её
    `createOffer` и разместим в модуле `common.ts`.

---
+++ src/utils/common.ts
@@ -0,0 +1,18 @@
+import { OfferType } from '../types/offer-type.enum.js';
+import { Offer } from '../types/offer.type.js';
+
+export const createOffer = (row: string) => {
+  const tokens = row.replace('\n', '').split('\t');
+  const [title, description, createdDate, image, type, price, categories, firstname, lastname, email, avatarPath] = tokens;
+  return {
+    title,
+    description,
+    postDate: new Date(createdDate),
+    image,
+    type: OfferType[type as 'Buy' | 'Sell'],
+    categories: categories.split(';')
+      .map((name) => ({name})),
+    price: Number.parseInt(price, 10),
+    user: {email, firstname, lastname, avatarPath},
+  } as Offer;
+};
