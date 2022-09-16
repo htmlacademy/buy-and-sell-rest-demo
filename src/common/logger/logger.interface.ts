293cc012883224091ca100393004e1f49a6ed743
Igor Antonov <antonov.igor.khv@gmail.com>
2022-09-16 15:42:00 +0300

    3.1. Добавит интерфейс Logger.
    С этого коммита мы начнём готовить инфраструктуру приложения. Под
    инфраструктурой подразумеваются общие сервисы, которые потребуются для
    работы приложения.
    
    Одна из важных задач любого приложения для бэкенда — логирование.
    Хорошие логи похожи на судовой журнал. В них фиксируются внештатные
    ситуации, ошибки и все важные события, которые произошли во время
    работы приложения.
    
    Во многих случаях изучение логов — единственный способ обнаружить
    трудно воспроизводимые ошибки.
    
    Подготовим логер для приложения. Начнём проектирование интерфейса.
    В приложении может быть несколько логеров, предусмотрим интерфейс.
    
    Что должен уметь абсолютно любой логер? Выводить информацию в консоль
    и сохранять её в файлы.
    
    Предусмотрим в интерфейсе несколько методов: `info` (для логирования
    информационных сообщений), `warn` (для логирования предупреждений),
    `error` (для логирования ошибок), `debug` (для логирования
    отладочных сообщений).

---
+++ src/common/logger/logger.interface.ts
@@ -0,0 +1,6 @@
+export interface LoggerInterface {
+  info(message: string, ...args: unknown[]): void;
+  warn(message: string, ...args: unknown[]): void;
+  error(message: string, ...args: unknown[]): void;
+  debug(message: string, ...args: unknown[]): void;
+}
