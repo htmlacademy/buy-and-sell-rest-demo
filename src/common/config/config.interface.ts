11f4e0b68d9abdc3d9d1563ff58a9d7f01d8c6b0
Igor Antonov <antonov.igor.khv@gmail.com>
2022-09-16 16:07:11 +0300

    3.14. Обновит интерфейс ConfigInterface. WIP
    Поскольку мы переходим на использование пакета `convict`, обновим
    интерфейс `ConfigInterface`.Можно создать новый интерфейс. Прежний
    вариант нам больше не потребуется.
    
    При описании интерфейса предусмотрим кейс, когда значением для аргумента
    `key` может быть одно из значений типа `ConfigSchema`. Это добавит
    дополнительную безопасность: мы не сможем получить значение опции,
    если она не определена в типе `ConfigSchema`.
    
    WIP: Линтер ругается, что интерфейс класса `ConfigService` не
    соответствует интерфейсу `ConfigInterface`.

--- src/common/config/config.interface.ts
+++ src/common/config/config.interface.ts
@@ -1,3 +1,5 @@
+import {ConfigSchema} from './config.schema.js';
+
 export interface ConfigInterface {
-  get(key: string): string | undefined;
+  get<T extends keyof ConfigSchema>(key: T): ConfigSchema[T];
 }
