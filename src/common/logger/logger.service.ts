5848c80b9daa05dd20c337d6100cbcc8e251aba9
Igor Antonov <antonov.igor.khv@gmail.com>
2022-09-16 16:13:56 +0300

    3.20. Добавит декоратор injectable в LoggerService. WIP
    Теперь необходимо воспользоваться декоратором `injectable` в классах,
    которые могут внедряться в виде зависимости.
    
    Для этого перед объявлением класса используем декоратор `@injectable`.
    Ему не нужно передавать дополнительных аргументов.
    
    Мы помечаем класс `LoggerService`, что он может использоваться в
    качестве зависимости. `injectable` помещает метаданные о классе в
    контейнер.
    
    WIP: Приложение не запускается.

--- src/common/logger/logger.service.ts
+++ src/common/logger/logger.service.ts
@@ -1,6 +1,8 @@
 import pino, {Logger} from 'pino';
+import {injectable} from 'inversify';
 import {LoggerInterface} from './logger.interface.js';
 
+@injectable()
 export default class LoggerService implements LoggerInterface{
   private logger!: Logger;
 
