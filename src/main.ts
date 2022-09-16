bd4f249038f8b071010f03225bc0196568b8b4cd
Igor Antonov <antonov.igor.khv@gmail.com>
2022-09-16 16:12:34 +0300

    3.19. Добавит контейнер с зависимостями. WIP
    Перейдём к файлу `main.ts` (точка входа). В нём мы создаём экземпляры
    всех необходимых классов и зависимостей для них. Попробуем сделать то же
    самое с помощью возможностей `inversify`.
    
    Первым делом нам потребуется создать контейнер. В контексте библиотеки
    `inversify`, контейнер — это место, где происходит настройка и привязка
    зависимостей.
    
    При создании контейнера мы объясняем `inversify` какую реализацию
    следует использовать для определённого интерфейса.
    
    Например, интерфейсу `LoggerInterface` соответствует реализация в
    виде класса `LoggerService`. А интерфейсу `ConfigInterface` реализация
    `ConfigService` и так далее.
    
    За счёт построения контейнера, в любом другом классе мы сможем получить
    нужную реализацию. При этом `inversify` возьмёт на себя создание
    необходимых экземпляров.
    
    WIP: Приложение не запускается.

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,15 @@
-import Application from './app/application.js';
+import {Container} from 'inversify';
+import {LoggerInterface} from './common/logger/logger.interface.js';
 import LoggerService from './common/logger/logger.service.js';
+import {Component} from './types/component.types.js';
+import {ConfigInterface} from './common/config/config.interface.js';
 import ConfigService from './common/config/config.service.js';
+import Application from './app/application.js';
 
-const logger = new LoggerService();
-const config = new ConfigService(logger);
+const applicationContainer = new Container();
+applicationContainer.bind<Application>(Component.Application).to(Application);
+applicationContainer.bind<LoggerInterface>(Component.LoggerInterface).to(LoggerService);
+applicationContainer.bind<ConfigInterface>(Component.ConfigInterface).to(ConfigService);
 
-const application = new Application(logger, config);
+const application = applicationContainer.get<Application>(Component.Application);
 await application.init();
