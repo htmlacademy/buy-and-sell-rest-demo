a65b15b09c871b872226e9a19035fe0fef85d54a
Igor Antonov <antonov.igor.khv@gmail.com>
2022-09-16 16:03:22 +0300

    3.10. Добавит поддержку dotenv в ConfigService.
    Обновим класс `ConfigService`. Вместо прямого обращения к `process.env`
    считаем данные из `.env` файла. Для этого достаточно вызвать функцию
    `config` пакета `dontenv`.
    
    После этого необходимо проверить результат разбора файла. Для этого
    проверим значение свойства `error`. Если оно `true`, значит возникла
    ошибка и дальше продолжать смысла нет.
    
    Можно просто прервать работу всего приложения (завершить процесс), а
    можно бросить ошибку. Второй вариант более предпочтительней.
    
    Обратите внимание, мы также добавили поддержку логирования в наш класс.
    Настроенный логер будем передавать через конструктор.

--- src/common/config/config.service.ts
+++ src/common/config/config.service.ts
@@ -1,10 +1,22 @@
 import {ConfigInterface} from './config.interface.js';
+import {config, DotenvParseOutput} from 'dotenv';
+import {LoggerInterface} from '../logger/logger.interface.js';
 
 export default class ConfigService implements ConfigInterface {
-  private config: NodeJS.ProcessEnv;
+  private config: DotenvParseOutput;
+  private logger: LoggerInterface;
 
-  constructor() {
-    this.config = process.env;
+  constructor(logger: LoggerInterface) {
+    this.logger = logger;
+
+    const parsedOutput = config();
+
+    if (parsedOutput.error) {
+      throw new Error('Can\'t read .env file. Perhaps the file does not exists.');
+    }
+
+    this.config = <DotenvParseOutput>parsedOutput.parsed;
+    this.logger.info('.env file found and successfully parsed!');
   }
 
   public get(key: string): string | undefined {
