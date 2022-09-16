a1726ce2844e1ccb664d6ef5f90ecdba35e01594
Igor Antonov <antonov.igor.khv@gmail.com>
2022-09-16 16:07:49 +0300

    3.15. Обновит класс `ConfigService`.
    Последним шагом обновим класс `ConfigService`. Начнём с обновления
    полей класса.
    
    Для поля `config` в качестве типа установим `ConfigSchema`. Напомним,
    этот тип мы определили самостоятельно — в модуле, где определяли схему.
    
    От `DotenvParseOutput` избавляемся. Теперь все значения будем получать
    только через `convict`. Сам `dotenv` остаётся. Он по-прежнему решает
    задачу чтения `.env` файла.
    
    После чтения `.env` мы вызываем метод `load` у `configSchema`. Он
    загрузит значения из переменных окружения.
    
    Сразу после него следует вызов метода `validate`. С его помощью
    выполним проверку на соответствие схемы.
    
    Последним шагом в конструкторе сохраним результат вызова
    `getProperties` в поле `config`. Он вернёт объект, соответствующий
    схеме конфигурации.
    
    Метод `get` также нуждается в обновлении. Обновим в соответствии с
    интерфейсом. Обратите внимание на использование оператора `keyof`.
    Мы сможем передавать в `get` имена опций, предусмотренных типом
    `ConfigSchema`.

--- src/common/config/config.service.ts
+++ src/common/config/config.service.ts
@@ -1,9 +1,10 @@
 import {ConfigInterface} from './config.interface.js';
-import {config, DotenvParseOutput} from 'dotenv';
+import {config} from 'dotenv';
 import {LoggerInterface} from '../logger/logger.interface.js';
+import {configSchema, ConfigSchema} from './config.schema.js';
 
 export default class ConfigService implements ConfigInterface {
-  private config: DotenvParseOutput;
+  private config: ConfigSchema;
   private logger: LoggerInterface;
 
   constructor(logger: LoggerInterface) {
@@ -15,11 +16,14 @@ export default class ConfigService implements ConfigInterface {
       throw new Error('Can\'t read .env file. Perhaps the file does not exists.');
     }
 
-    this.config = <DotenvParseOutput>parsedOutput.parsed;
+    configSchema.load({});
+    configSchema.validate({allowed: 'strict', output: this.logger.info});
+
+    this.config = configSchema.getProperties();
     this.logger.info('.env file found and successfully parsed!');
   }
 
-  public get(key: string): string | undefined {
+  public get<T extends keyof ConfigSchema>(key: T) {
     return this.config[key];
   }
 }
