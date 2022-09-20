4f37e9c14b577370ba2d0791d348e8deea1d7080
Igor Antonov <antonov.igor.khv@gmail.com>
2022-09-20 16:01:39 +0300

    4.20. Добавит класс CreateUserDTO.
    DTO (Data Transfer Object) — ещё один паттерн проектирования.
    Его применяют для описания контрактов передачи данных между слоями
    приложения или подсистемами приложения.
    
    Чтобы создать в базе данных новый документ с информацией о пользователе
    нужно передать определённы набор данных. Мы с ним разбирались при
    описании `UserEntity`. Этот набор может приходить из разных мест: от
    клиента (с фронтенда), из другого слоя приложения и так далее. DTO
    позволяет добавить согласованность. Мы определяем «контракт» и
    поддерживаем его на разных слоях приложения.
    
    Опишем первый DTO, который назовём `CreateUserDTO`. DTO опишем в виде
    класса. Это чаще удобней, чем просто интерфейс/или тип. В нём
    перечислим все поля, которые необходимы для создания нового документа
    пользователя.
    
    Обратите внимание, DTO всегда будет несколько. Поэтому удобней сразу
    завести для них отдельную директорию внутри определённого
    модуля.

---
+++ src/modules/user/dto/create-user.dto.ts
@@ -0,0 +1,7 @@
+export default class CreateUserDto {
+  public email!: string ;
+  public avatarPath!: string;
+  public firstname!: string;
+  public lastname!: string;
+  public password!: string;
+}
