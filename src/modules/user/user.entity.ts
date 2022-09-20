9e0e2045b7ead55217ba958d795c369a28b61993
Igor Antonov <antonov.igor.khv@gmail.com>
2022-09-20 16:01:00 +0300

    4.19. Добавит методы в модель UserModel.
    Пропишем значения по умолчанию для свойств и добавим признак
    обязательного заполнения.
    
    При описании класса модели мы можем создавать не только свойства, но и
    методы. Методы позволяют реализовать дополнительную логику. Например,
    с помощью методов можно рассчитывать значения свойств на основании
    других.
    
    Но какие методы могут потребоваться в модели `User`? Модель
    предусматривает поле `password`. В нём должен храниться пароль
    пользователя. Однако хранить пароли в открытом виде небезопасно.
    Поэтому мы можем предусмотреть метод, который будет хешировать пароль.
    
    Для реализации этой идеи сделаем свойство `password` приватным. Это
    необходимо, чтобы не было возможности напрямую взаимодействовать с
    содержимым поля. Пароль для пользователя будет устанавливаться с
    помощью метода `setPassword`.
    
    Для генерации хэша потребуется соль. Соль — случайный набор символов,
    который будет использоваться при хешировании. Метод `setPassword`
    аргументом будет принимать пароль и соль.
    
    Для получения хеша создадим в модуле `common.ts` вспомогательную
    функцию и воспользуемся в ней встроенным модулем `crypto`. Внутри
    функции `createSHA256` воспользуемся методом `createHmac`. Методу
    нужно передать алгоритм хеширования и соль. Существуют разные
    алгоритмы хеширования, мы воспользуемся `sha256`. Он быстр и надёжен.
    
    Затем запишем в свойство `password` результат выполнения метода
    `update` у функции хеширования. Методу передаём открытый пароль
    (из одноимённого аргумента).
    
    Получается, что теперь единственный способ установить пароль —
    воспользоваться методом `setPassword`. А как получить результат
    хеширования? Для этого опишем отдельный метод `getPassword`. Он
    возвращает содержимое поля `this.password`. То же самое можно сделать
    в виде геттера, но мы воспользуемся методом для консистентности.

--- src/modules/user/user.entity.ts
+++ src/modules/user/user.entity.ts
@@ -1,25 +1,48 @@
 import {User} from '../../types/user.type.js';
 import typegoose, {getModelForClass, defaultClasses} from '@typegoose/typegoose';
+import {createSHA256} from '../../utils/common.js';
 
-const {prop} = typegoose;
+const {prop, modelOptions} = typegoose;
 
 export interface UserEntity extends defaultClasses.Base {}
 
+@modelOptions({
+  schemaOptions: {
+    collection: 'users'
+  }
+})
 export class UserEntity extends defaultClasses.TimeStamps implements User {
+  constructor(data: User) {
+    super();
+
+    this.email = data.email;
+    this.avatarPath = data.avatarPath;
+    this.firstname = data.firstname;
+    this.lastname = data.lastname;
+  }
+
   @prop({ unique: true, required: true })
   public email!: string;
 
-  @prop()
+  @prop({required: true, default: ''})
   public avatarPath!: string;
 
-  @prop()
+  @prop({required: true, default: ''})
   public firstname!: string;
 
-  @prop()
+  @prop({required: true, default: ''})
   public lastname!: string;
 
-  @prop()
-  public password!: string;
+  @prop({required: true, default: ''})
+  private password!: string;
+
+  public setPassword(password: string, salt: string) {
+    this.password = createSHA256(password, salt);
+  }
+
+  public getPassword() {
+    return this.password;
+  }
 }
 
 export const UserModel = getModelForClass(UserEntity);
