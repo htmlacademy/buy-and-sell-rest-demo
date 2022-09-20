5850426712a23752ac06dac33a8b94779b49fc9d
Igor Antonov <antonov.igor.khv@gmail.com>
2022-09-20 16:11:44 +0300

    4.23. Добавит внедрение зависимостей в UserService. WIP
    Воспользуемся декоратором `inject` и внедрим в конструктор `UserService`
    в качестве зависимости `logger`. После этого внутри класса `UserService`
    мы сможем залогировать событие через `this.logger.<method>`.
    
    Последним шагом добавим для класса `UserService` декоратор `injectable`.
    Этим классом мы будем пользоваться из разных мест приложения,
    поэтому сразу поместим его в контейнер. Для этого откроем объект
    `Component` и добавим ещё один компонент — `UserServiceInterface`.
    А затем в `main.ts` добавим для него привязку.
    
    WIP: Имплементирован только один метод интерфейса `UserServiceInterface`.
    Линтер ругается.

--- src/types/component.types.ts
+++ src/types/component.types.ts
@@ -2,5 +2,6 @@ export const Component = {
   Application: Symbol.for('Application'),
   LoggerInterface: Symbol.for('LoggerInterface'),
   ConfigInterface: Symbol.for('ConfigInterface'),
-  DatabaseInterface: Symbol.for('DatabaseInterface')
+  DatabaseInterface: Symbol.for('DatabaseInterface'),
+  UserServiceInterface: Symbol.for('UserServiceInterface'),
 } as const;
