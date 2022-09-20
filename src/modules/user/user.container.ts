7acdd447af3cfb74758c6ca61b66f948383b66f7
Igor Antonov <antonov.igor.khv@gmail.com>
2022-09-20 16:20:27 +0300

    4.25. Добавит внедрение модели в зависимости. WIP
    Класс `UserService` напрямую обращается к модели `UserModel`.
    Мы можем понизить эту связь и внедрять модель в качестве зависимости.
    Для этого нам потребуется проделать несколько шагов.
    
    Начнём с добавления нового компонента приложения в `Component`.
    Определим `UserModel`. Затем создадим новый контейнер `user.container.ts`
    и добавим его в общий контейнер. На этот раз мы воспользуемся методом
    `toConstantValue`. Дело в том, что нам не нужно создавать новый экземпляр
    модели. Мы напрямую сможем работать с `UserModel`.
    
    Затем вернёмся в `UserService` и внедрим модель в качестве зависимости.
    После этого сможем обращаться к модели через `this.userModel`.
    
    WIP: Имплементирован только один метод интерфейса `UserServiceInterface`.
    Линтер ругается.

--- src/modules/user/user.container.ts
+++ src/modules/user/user.container.ts
@@ -1,4 +1,6 @@
 import {Container} from 'inversify';
+import {types} from '@typegoose/typegoose';
+import {UserEntity, UserModel} from './user.entity.js';
 import {UserServiceInterface} from './user-service.interface.js';
 import UserService from './user.service.js';
 import {Component} from '../../types/component.types.js';
@@ -6,5 +8,6 @@ import {Component} from '../../types/component.types.js';
 const userContainer = new Container();
 
 userContainer.bind<UserServiceInterface>(Component.UserServiceInterface).to(UserService);
+userContainer.bind<types.ModelType<UserEntity>>(Component.UserModel).toConstantValue(UserModel);
 
 export {userContainer};
