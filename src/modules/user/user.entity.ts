5ab78171c428ef3c812a962f48e5755ad4d3b84d
Igor Antonov <antonov.igor.khv@gmail.com>
2022-09-20 15:54:21 +0300

    4.16. Добавит описание схемы. WIP
    Мы подготовили класс будущей модели, но пока он не решает задачу
    описания схемы. Чтобы класс стал одновременно и описанием схемы
    необходимо воспользоваться декораторами. Для этого в составе `typegoose`
    есть декоратор `@prop`. Этим декоратором следует пометить свойства
    класса, значения которых должны сохраняться в базе данных.
    
    Получается, что за счёт декоратора `@prop` мы описываем схему как бы
    поверх класса. У класса могут быть и другие свойства, которые не должны
    сохраняться в базу данных, а использоваться только на уровне модели, для
    каких-то служебных целей.
    
    Декоратор `@prop` в качестве аргумента принимает объект настроек. В нём
    можно воспользоваться всеми свойствами, которые использовали при
    построении схемы с помощью объекта `Schema`. Например, чтобы сделать
    поле `email` уникальным, можно передать объект настроек с ключом
    `unique`. С правилами валидации аналогичная история.
    
    Обратите внимание, что в декоратор мы не передаём информацию о типе
    поле. Для примитивных типов он сможет получить её автоматически. Но
    если поле в качестве типа принимает, например, массив, то тип придётся
    указать явно. Для этого в объекте настроек придётся добавить ключ `type`
    и его значением должна стать функция, которая возвращает конструктор
    типа.
    
    Последнее, что необходимо сделать — создать на основании класса модель
    в терминах Mongoose. Для этого в `typegoose` предусмотрена специальная
    функция — `getModelForClass`. Она создаст объект модели.
    
    Прямо сейчас можно воспользоваться моделью и протестировать создание
    записей. Предыдущий код для теста по-прежнему будет работать:
    
    ```
    const user = UserModel.create({
      email: 'test@emailru',
      avatarPath: 'keks.jpg',
      firstname: '2',
      lastname: 'Unknown'
     });
    
     console.log(user);
    ```
    
    WIP: Модель не работает. Переход на `typegoose`.

--- src/modules/user/user.entity.ts
+++ src/modules/user/user.entity.ts
@@ -1,8 +1,23 @@
 import {User} from '../../types/user.type.js';
+import typegoose, {getModelForClass} from '@typegoose/typegoose';
+
+const {prop} = typegoose;
 
 export class UserEntity implements User {
+  @prop({ unique: true, required: true })
   public email!: string;
+
+  @prop()
   public avatarPath!: string;
+
+  @prop()
   public firstname!: string;
+
+  @prop()
   public lastname!: string;
+
+  @prop()
+  public password!: string;
 }
+
+export const UserModel = getModelForClass(UserEntity);
