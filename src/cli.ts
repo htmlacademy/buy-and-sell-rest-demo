e55cf81a9faf6053090a0ae22baccf1d5ed9cda2
Igor Antonov <antonov.igor.khv@gmail.com>
2022-09-20 16:45:42 +0300

    4.34. Добавит импорт reflect-metadata.

--- src/cli.ts
+++ src/cli.ts
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 
+import 'reflect-metadata';
 import VersionCommand from './cli-command/version-command.js';
 import HelpCommand from './cli-command/help-command.js';
 import ImportCommand from './cli-command/import-command.js';
