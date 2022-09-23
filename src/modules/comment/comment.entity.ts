f5fbc11f89a04975e18eb5ada13a3ecdc3a118ca
Igor Antonov <antonov.igor.khv@gmail.com>
2022-09-23 14:31:00 +0300

    5.11. Добавит CommentEntity.
    Реализуем поддержку комментариев. Начнём с описания
    сущности — `CommentEntity`. Комментарии будем хранить в отдельной
    коллекции `comments`. Мы конечно можем их сохранять в коллекции
    `Offers`, но это неоптимальное решение. Для одного объявления может
    быть множество комментариев. Извлекать их все, когда требуется только
    информация об объявлении — затратная операция. Поэтому хранить
    комментарии будем в отдельной коллекции.
    
    Каждый комментарий содержит минимальное количество информации: текст
    комментария, дату публикации (потребуется для сортировки). Также
    потребуется фиксировать автора комментария и объявление для которого
    этот комментарий был создан. Полная информация об объявлении и авторе
    хранится в отдельных коллекциях, поэтому можем указать ссылки на
    соответствующие сущности с помощью `Ref`. На уровне БД мы сохраним
    идентификаторы соответствующих сущностей — `OfferEntity` и `UserEntity`.
    
    Отдельное поле для хранения даты добавления комментария заводить не
    будем. Мы отнаследуемся от `TimeStamp`, поэтому дату добавления
    комментария сможем получить из поля `createdAt`.

---
+++ src/modules/comment/comment.entity.ts
@@ -0,0 +1,31 @@
+import typegoose, {getModelForClass, Ref, defaultClasses} from '@typegoose/typegoose';
+import {UserEntity} from '../user/user.entity.js';
+import {OfferEntity} from '../offer/offer.entity.js';
+
+const {prop, modelOptions} = typegoose;
+
+export interface CommentEntity extends  defaultClasses.Base {}
+
+@modelOptions({
+  schemaOptions: {
+    collection: 'comments'
+  }
+})
+export class CommentEntity extends defaultClasses.TimeStamps {
+  @prop({trim: true, required: true})
+  public text!: string;
+
+  @prop({
+    ref: OfferEntity,
+    required: true
+  })
+  public offerId!: Ref<OfferEntity>;
+
+  @prop({
+    ref: UserEntity,
+    required: true,
+  })
+  public userId!: Ref<UserEntity>;
+}
+
+export const CommentModel = getModelForClass(CommentEntity);
