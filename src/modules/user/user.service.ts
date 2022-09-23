a71b7e228c7298955d90859ec08d3db6f1057b4c
Igor Antonov <antonov.igor.khv@gmail.com>
2022-09-23 15:01:35 +0300

    5.14. Расширит интерфейс UserServiceInterface.
    Метод для обновления
    `UserEntity` в настоящее время нереализован, поэтому расширим
    интерфейс `UserServiceInterface` и добавить метод `updateById`. Метод
    имплементируем по аналогии с аналогичным в `OfferService`. Для
    выполнения фактического обновления потребуется создать `UpdateUserDto`.

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -5,6 +5,7 @@ import CreateUserDto from './dto/create-user.dto.js';
 import {UserServiceInterface} from './user-service.interface.js';
 import {LoggerInterface} from '../../common/logger/logger.interface.js';
 import {Component} from '../../types/component.types.js';
+import UpdateUserDto from './dto/update-user.dto.js';
 
 @injectable()
 export default class UserService implements UserServiceInterface {
@@ -35,5 +36,11 @@ export default class UserService implements UserServiceInterface {
     }
 
     return this.create(dto, salt);
+  }
+
+  public async updateById(userId: string, dto: UpdateUserDto): Promise<DocumentType<UserEntity> | null> {
+    return this.userModel
+      .findByIdAndUpdate(userId, dto, {new: true})
+      .exec();
   }
 }
