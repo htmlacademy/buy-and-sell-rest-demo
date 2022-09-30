81b9e893f7621270b8931e82399969ed0748122e
Igor Antonov <antonov.igor.khv@gmail.com>
2022-09-30 15:45:35 +0300

    6.27. Добавим обработку HttpError в ExceptionFilter.
    Теперь добавим обработку ошибок тип `HttpError` в `ExceptionFilter`.
    Для этого создадим два приватных метода. Один отвечает за обработку
    ошибок типа `HttpError`, другой — всех остальных.
    
    Вызывать эти методы будем в методе `catch` в зависимости от типа `error`.

--- src/common/errors/exception-filter.ts
+++ src/common/errors/exception-filter.ts
@@ -4,6 +4,8 @@ import {StatusCodes} from 'http-status-codes';
 import {ExceptionFilterInterface} from './exception-filter.interface.js';
 import {Component} from '../../types/component.types.js';
 import {LoggerInterface} from '../logger/logger.interface.js';
+import HttpError from './http-error.js';
+import {createErrorObject} from '../../utils/common.js';
 
 @injectable()
 export default class ExceptionFilter implements ExceptionFilterInterface {
@@ -13,10 +15,25 @@ export default class ExceptionFilter implements ExceptionFilterInterface {
     this.logger.info('Register ExceptionFilter');
   }
 
-  public catch(error: Error, _req: Request, res: Response, _next: NextFunction): void {
+  private handleHttpError(error: HttpError, _req: Request, res: Response, _next: NextFunction) {
+    this.logger.error(`[${error.detail}]: ${error.httpStatusCode} — ${error.message}`);
+    res
+      .status(error.httpStatusCode)
+      .json(createErrorObject(error.message));
+  }
+
+  private handleOtherError(error: Error, _req: Request, res: Response, _next: NextFunction) {
     this.logger.error(error.message);
     res
       .status(StatusCodes.INTERNAL_SERVER_ERROR)
-      .json({error: error.message});
+      .json(createErrorObject(error.message));
+  }
+
+  public catch(error: Error | HttpError, req: Request, res: Response, next: NextFunction): void {
+    if (error instanceof HttpError) {
+      return this.handleHttpError(error, req, res, next);
+    }
+
+    this.handleOtherError(error, req, res, next);
   }
 }
