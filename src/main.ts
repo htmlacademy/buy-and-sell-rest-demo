d29641c3bdc9cc66f11916b9ac5fef65853a4769
Igor Antonov <antonov.igor.khv@gmail.com>
2022-09-30 15:19:27 +0300

    6.18. Подключит ExceptionFilter в Application.
    Первая версия `ExceptionFilter` готова. Подключим её в `Application`.
    Подключать будем в виде зависимости, поэтому сразу обновим `Component`
    и добавим привязку в контейнер (`main.ts`).
    
    Затем немного обновим реализацию `Application`. Добавим внедрение
    `ExceptionFilterInterface` в конструктор и метод
    `initExceptionFilters`. В методе добавим обработчики фильтров в
    экземпляр express-приложения. Обработчиков ошибок может быть несколько,
    поэтому сразу предусмотрим для этого отдельный метод. Вызов метода
    выполним в методе `init`, после регистрации маршрутов.

--- src/main.ts
+++ src/main.ts
@@ -24,6 +24,8 @@ import {CommentServiceInterface} from './modules/comment/comment-service.interfa
 import {CommentEntity, CommentModel} from './modules/comment/comment.entity.js';
 import {ControllerInterface} from './common/controller/controller.interface.js';
 import CategoryController from './modules/category/category.controller.js';
+import ExceptionFilter from './common/errors/exception-filter.js';
+import {ExceptionFilterInterface} from './common/errors/exception-filter.interface.js';
 
 const applicationContainer = new Container();
 applicationContainer.bind<Application>(Component.Application).to(Application).inSingletonScope();
@@ -40,6 +42,7 @@ applicationContainer.bind<CommentServiceInterface>(Component.CommentServiceInter
 applicationContainer.bind<types.ModelType<CommentEntity>>(Component.CommentModel).toConstantValue(CommentModel);
 
 applicationContainer.bind<ControllerInterface>(Component.CategoryController).to(CategoryController).inSingletonScope();
+applicationContainer.bind<ExceptionFilterInterface>(Component.ExceptionFilterInterface).to(ExceptionFilter).inSingletonScope();
 
 const application = applicationContainer.get<Application>(Component.Application);
 await application.init();
--- src/types/component.types.ts
+++ src/types/component.types.ts
@@ -12,4 +12,5 @@ export const Component = {
   CommentModel: Symbol.for('CommentModel'),
   CommentServiceInterface: Symbol.for('CommentServiceInterface'),
   CategoryController: Symbol.for('CategoryController'),
+  ExceptionFilterInterface: Symbol.for('ExceptionFilterInterface'),
 } as const;
