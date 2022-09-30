13ed14df7968ea4fdf0e578490404b6f6a0ccbcb
Igor Antonov <antonov.igor.khv@gmail.com>
2022-09-30 15:19:27 +0300

    6.18. Подключит ExceptionFilter в Application.
    Первая версия `ExceptionFilter` готова. Подключим её в `Application`.
    Подключать будем в виде зависимости, поэтому сразу обновим `Component`
    и добавим привязку в контейнер (`main.ts`).
    
    Затем немного обновим реализацию `Application`. Добавим внедрение
    `ExceptionFilterInterface` в конструктор и метод
    `initExceptionFilters`. В методе добавим обработчики фильтров в
    экземпляр express-приложения. Обработчиков ошибок может быть несколько,
    поэтому сразу предусмотрим для этого отдельный метод. Вызов метода
    выполним в методе `init`, после регистрации маршрутов.

--- src/app/application.container.ts
+++ src/app/application.container.ts
@@ -2,10 +2,12 @@ import {Container} from 'inversify';
 import {LoggerInterface} from '../common/logger/logger.interface.js';
 import {ConfigInterface} from '../common/config/config.interface.js';
 import {DatabaseInterface} from '../common/database-client/database.interface.js';
+import {ExceptionFilterInterface} from '../common/errors/exception-filter.interface.js';
 import Application from './application.js';
 import LoggerService from '../common/logger/logger.service.js';
 import ConfigService from '../common/config/config.service.js';
 import DatabaseService from '../common/database-client/database.service.js';
+import ExceptionFilter from '../common/errors/exception-filter.js';
 import {Component} from '../types/component.types.js';
 
 const applicationContainer = new Container();
@@ -14,5 +16,6 @@ applicationContainer.bind<Application>(Component.Application).to(Application).in
 applicationContainer.bind<LoggerInterface>(Component.LoggerInterface).to(LoggerService).inSingletonScope();
 applicationContainer.bind<ConfigInterface>(Component.ConfigInterface).to(ConfigService).inSingletonScope();
 applicationContainer.bind<DatabaseInterface>(Component.DatabaseInterface).to(DatabaseService).inSingletonScope();
+applicationContainer.bind<ExceptionFilterInterface>(Component.ExceptionFilterInterface).to(ExceptionFilter).inSingletonScope();
 
 export {applicationContainer};
--- src/app/application.ts
+++ src/app/application.ts
@@ -7,6 +7,7 @@ import {Component} from '../types/component.types.js';
 import {getURI} from '../utils/db.js';
 import {DatabaseInterface} from '../common/database-client/database.interface.js';
 import {ControllerInterface} from '../common/controller/controller.interface.js';
+import {ExceptionFilterInterface} from '../common/errors/exception-filter.interface.js';
 
 @injectable()
 export default class Application {
@@ -16,7 +17,8 @@ export default class Application {
     @inject(Component.LoggerInterface) private logger: LoggerInterface,
     @inject(Component.ConfigInterface) private config: ConfigInterface,
     @inject(Component.DatabaseInterface) private databaseClient: DatabaseInterface,
-    @inject(Component.CategoryController) private categoryController: ControllerInterface
+    @inject(Component.CategoryController) private categoryController: ControllerInterface,
+    @inject(Component.ExceptionFilterInterface) private exceptionFilter: ExceptionFilterInterface,
   ) {
     this.expressApp = express();
   }
@@ -27,6 +29,10 @@ export default class Application {
 
   public initMiddleware() {
     this.expressApp.use(express.json());
+  }
+
+  public initExceptionFilters() {
+    this.expressApp.use(this.exceptionFilter.catch.bind(this.exceptionFilter));
   }
 
   public async init() {
@@ -45,6 +51,7 @@ export default class Application {
 
     this.initMiddleware();
     this.initRoutes();
+    this.initExceptionFilters();
     this.expressApp.listen(this.config.get('PORT'));
     this.logger.info(`Server started on http://localhost:${this.config.get('PORT')}`);
   }
