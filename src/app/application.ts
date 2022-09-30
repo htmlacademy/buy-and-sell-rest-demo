dca47550861c44d991710bd7598b7fa60dc50d7f
Igor Antonov <antonov.igor.khv@gmail.com>
2022-09-30 14:27:36 +0300

    6.10. Добавит регистрацию контроллеров. WIP
    Следующим шагом доработаем класс `Application`. Ему необходимо научить
    регистрировать маршруты, которые мы определяем с помощью контроллеров.
    Для решения этой задачи создадим метод `registerRouters`.
    
    В этом методе зарегистрируем все экземпляры Express-роутеров с
    помощью метода `use` у `expressApp`. Для `CategoryController` первым
    аргументом передадим название общий путь к ресурсу (`/categories`),
    а вторым — экземпляр нужного `Router`. Его получим через геттер
    `router` нужного контроллера. Сам контроллер `CategoryController`
    внедрим в виде зависимости.
    
    Получается, что маршруту, которые мы добавили в `CategoryController`
    будут доступны через сегмент `/categories`. Например,
    `GET http://localhost:4000/categories`.
    
    Вызов метода `registerRoutes` напишем в методе `init`.
    
    WIP. Линтер ругается на неиспользуемые переменные.

--- src/app/application.ts
+++ src/app/application.ts
@@ -6,6 +6,7 @@ import {ConfigInterface} from '../common/config/config.interface.js';
 import {Component} from '../types/component.types.js';
 import {getURI} from '../utils/db.js';
 import {DatabaseInterface} from '../common/database-client/database.interface.js';
+import {ControllerInterface} from '../common/controller/controller.interface.js';
 
 @injectable()
 export default class Application {
@@ -14,9 +15,14 @@ export default class Application {
   constructor(
     @inject(Component.LoggerInterface) private logger: LoggerInterface,
     @inject(Component.ConfigInterface) private config: ConfigInterface,
-    @inject(Component.DatabaseInterface) private databaseClient: DatabaseInterface
+    @inject(Component.DatabaseInterface) private databaseClient: DatabaseInterface,
+    @inject(Component.CategoryController) private categoryController: ControllerInterface
   ) {
     this.expressApp = express();
+  }
+
+  public initRoutes() {
+    this.expressApp.use('/categories', this.categoryController.router);
   }
 
   public async init() {
@@ -33,6 +39,7 @@ export default class Application {
 
     await this.databaseClient.connect(uri);
 
+    this.initRoutes();
     this.expressApp.listen(this.config.get('PORT'));
     this.logger.info(`Server started on http://localhost:${this.config.get('PORT')}`);
   }
