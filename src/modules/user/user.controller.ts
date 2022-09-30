4cb8668c8063a076197c2fe3669263a358dfa818
Igor Antonov <antonov.igor.khv@gmail.com>
2022-09-30 15:56:33 +0300

    6.30. Добавит метод login в UserController.
    Добавим в контроллер `UserController` метод `login`. Он будет
    использоваться для авторизации пользователя в системе. Авторизацию мы
    реализуем позже, а пока опишем одну проверку (существует пользователь
    или нет) и вернём в качестве результата «Not implemented».
    
    При реализации нам потребуется DTO — `LoginUserDto`. Поэтому создадим
    новый класс для DTO по аналогии с ранее созданными.
    
    Для тестирования обработчика сразу добавим  запрос в `queries.http`.

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -11,6 +11,7 @@ import {StatusCodes} from 'http-status-codes';
 import {fillDTO} from '../../utils/common.js';
 import UserResponse from './response/user.response.js';
 import {ConfigInterface} from '../../common/config/config.interface.js';
+import LoginUserDto from './dto/login-user.dto.js';
 
 @injectable()
 export default class UserController extends Controller {
@@ -23,6 +24,7 @@ export default class UserController extends Controller {
     this.logger.info('Register routes for UserController…');
 
     this.addRoute({path: '/register', method: HttpMethod.Post, handler: this.create});
+    this.addRoute({path: '/login', method: HttpMethod.Post, handler: this.login});
   }
 
   public async create(
@@ -44,6 +46,27 @@ export default class UserController extends Controller {
       res,
       StatusCodes.CREATED,
       fillDTO(UserResponse, result)
+    );
+  }
+
+  public async login(
+    {body}: Request<Record<string, unknown>, Record<string, unknown>, LoginUserDto>,
+    _res: Response,
+  ): Promise<void> {
+    const existsUser = await this.userService.findByEmail(body.email);
+
+    if (!existsUser) {
+      throw new HttpError(
+        StatusCodes.UNAUTHORIZED,
+        `User with email ${body.email} not found.`,
+        'UserController',
+      );
+    }
+
+    throw new HttpError(
+      StatusCodes.NOT_IMPLEMENTED,
+      'Not implemented',
+      'UserController',
     );
   }
 }
