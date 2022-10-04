be9f56f0050dec6409db86da61982bef4c4985fe
Igor Antonov <antonov.igor.khv@gmail.com>
2022-10-04 16:09:42 +0300

    7.13. Добавит регистрацию middleware в Controller.
    Передавать middleware при регистрации обработчиков маршрутов мы можем,
    но чтобы они заработали, необходимо их зарегистрировать. Для этого
    немного модифицируем метод `addRoute` в классе `Controller`.
    
    Для начала позаботимся о привязке контекста и оборачивании middleware
    в `asyncHandler`. Для этого трансформируем массив `middlewares`.
    Обратите внимание, что мы используем оператор `?`, чтобы не делать
    проверку существования свойства `middlewares` в блоке `if`. Таким
    образом, в `middlewares` будет либо `undefined` (если middlewares не
    были переданы), либо массив из функций `execute` (middlewares).
    
    Затем заведём отдельную переменную, в которой сохраним общую цепочку
    обработчиков (вместе с middlewares). Если middleware есть, то копируем
    на них ссылки, а в конце добавляем оригинальный обработчик. В противном
    случае используем обработчик. Последним шагом заменим `handler` на
    `chainHandlers`.

--- src/common/controller/controller.ts
+++ src/common/controller/controller.ts
@@ -19,7 +19,13 @@ export abstract class Controller implements ControllerInterface {
   }
 
   public addRoute(route: RouteInterface) {
-    this._router[route.method](route.path, asyncHandler(route.handler.bind(this)));
+    const routeHandler = asyncHandler(route.handler.bind(this));
+    const middlewares = route.middlewares?.map(
+      (middleware) => asyncHandler(middleware.execute.bind(middleware))
+    );
+
+    const allHandlers = middlewares ? [...middlewares, routeHandler] : routeHandler;
+    this._router[route.method](route.path, allHandlers);
     this.logger.info(`Route registered: ${route.method.toUpperCase()} ${route.path}`);
   }
 
