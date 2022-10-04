e5d8b492fc2db2610e331e9c9d915d04c88ec091
Igor Antonov <antonov.igor.khv@gmail.com>
2022-10-04 17:13:44 +0300

    7.30. Добавит новую переменную окружения UPLOAD_FILE_DIRECTORY.
    Пришло время разобраться с загрузкой файлов. Клиенты помимо текстовых
    данных могут отправлять и полноценные файлы. В нашем сервисе два таких
    кейса: аватар пользователя и изображение для объявления.
    
    Чтобы начать принимать файлы от клиентов необходимо разрешить
    несколько организационных вопросов. Первый из них: где хранить файлы.
    Файлы можно хранить в разных местах: файловая система, база данных,
    внешние сервисы и так далее. У каждого варианта свои плюсы и минусы.
    Мы будем хранить файлы на сервере, в отдельной директории.
    
    Создадим в корне проекта новую директорию, например `uploads` и добавим
    в схему конфигурации новую переменную окружения `UPLOAD_DIRECTORY`. В
    этой переменной передадим путь к директории, в которую следует
    сохранять файлы от клиентов.
    
    Поскольку файлы нет смысла помещать в систему контроля версий сразу
    внесём исключение в `.gitignore`.

--- src/common/config/config.schema.ts
+++ src/common/config/config.schema.ts
@@ -11,6 +11,7 @@ export type ConfigSchema = {
   DB_PASSWORD: string;
   DB_PORT: number;
   DB_NAME: string;
+  UPLOAD_DIRECTORY: string
 }
 
 export const configSchema = convict<ConfigSchema>({
@@ -55,5 +56,11 @@ export const configSchema = convict<ConfigSchema>({
     format: String,
     env: 'DB_NAME',
     default: 'course-nodejs-restapi'
-  }
+  },
+  UPLOAD_DIRECTORY: {
+    doc: 'Directory for upload files',
+    format: String,
+    env: 'UPLOAD_DIRECTORY',
+    default: null
+  },
 });
