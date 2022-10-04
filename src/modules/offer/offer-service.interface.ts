112eb464c78ad20345f63f4b74914dd114ee940f
Igor Antonov <antonov.igor.khv@gmail.com>
2022-10-04 17:05:56 +0300

    7.28. Унаследует интерфейс, добавит middleware для проверки документа в коллекции.

--- src/modules/offer/offer-service.interface.ts
+++ src/modules/offer/offer-service.interface.ts
@@ -2,8 +2,9 @@ import {DocumentType} from '@typegoose/typegoose';
 import {OfferEntity} from './offer.entity.js';
 import CreateOfferDto from './dto/create-offer.dto.js';
 import UpdateOfferDto from './dto/update-offer.dto.js';
+import { DocumentExistsInterface } from '../../types/document-exists.interface.js';
 
-export interface OfferServiceInterface {
+export interface OfferServiceInterface extends DocumentExistsInterface {
   create(dto: CreateOfferDto): Promise<DocumentType<OfferEntity>>;
   findById(offerId: string): Promise<DocumentType<OfferEntity> | null>;
   find(): Promise<DocumentType<OfferEntity>[]>;
