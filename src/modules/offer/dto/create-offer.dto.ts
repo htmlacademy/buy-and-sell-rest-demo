0814fcd36e2738e90dc300b8167f6eb7ea698b90
Igor Antonov <antonov.igor.khv@gmail.com>
2022-10-04 16:40:15 +0300

    7.19. Добавит валидаторы для CreateOfferDto.
    Опишем правила валидации для `CreateOfferDto`. Напомним, с помощью него
    мы создаём новые объявления. Чтобы указать правила валидации, мы должны
    воспользоваться одним из множества декораторов.
    
    Например, если мы хотим ограничить длину поля, то для этого
    `class-validator` предлагает декораторы `@MinLength` и `@MaxLength`.
    Другой кейс: проверка строки, которая содержит дату. Для этого есть
    декоратор `@IsDateString`. Ещё один полезный декоратор — `@IsMongoId`.
    С его помощью можно проверить поля, где должен передаваться идентификатор
    записи (`ObjectId`) на корректность.
    
    Каждый декоратор для валидации аргументами принимает объект настроек.
    Настройки разнятся от валидатора к валидатору и подробней про доступные
    можно прочитать в документации. Однако, у любого объекта настроек есть
    поле `message`. В него можно прописать сообщение, которое будет
    формироваться при ошибке валидации.
    
    Обратите внимание на применение декоратора для поля `categories`. В
    этом поле находится массив идентификаторов категорий. Чтобы
    провалидировать каждый элемент с помощью валидатора `@IsMongoId` можно
    указать в объекте настроек `each: true`. Таким образом, валидатор будет
    применён для каждого элемента массива.

--- src/modules/offer/dto/create-offer.dto.ts
+++ src/modules/offer/dto/create-offer.dto.ts
@@ -1,12 +1,33 @@
 import {OfferType} from '../../../types/offer-type.enum.js';
+import {IsArray, IsDateString, IsEnum, IsInt, IsMongoId, Max, MaxLength, Min, MinLength} from 'class-validator';
 
 export default class CreateOfferDto {
+  @MinLength(10, {message: 'Minimum title length must be 10'})
+  @MaxLength(100, {message: 'Maximum title length must be 100'})
   public title!: string;
+
+  @MinLength(20, {message: 'Minimum description length must be 20'})
+  @MaxLength(1024, {message: 'Maximum description length must be 1024'})
   public description!: string;
+
+  @IsDateString({}, {message: 'postDate must be valid ISO date'})
   public postDate!: Date;
+
+  @MaxLength(256, {message: 'Too short for field «image»'})
   public image!: string;
+
+  @IsEnum(OfferType, {message: 'type must be Buy and Sell'})
   public type!: OfferType;
+
+  @IsInt({message: 'Price must be an integer'})
+  @Min(100, {message: 'Minimum price is 100'})
+  @Max(200000, {message: 'Maximum price is 200000'})
   public price!: number;
+
+  @IsArray({message: 'Field categories must be an array'})
+  @IsMongoId({each: true, message: 'Categories field must be an array of valid id'})
   public categories!: string[];
+
+  @IsMongoId({message: 'userId field must be valid an id'})
   public userId!: string;
 }
