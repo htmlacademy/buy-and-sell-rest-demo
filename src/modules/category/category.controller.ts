27603d193e8867b222ff7c23b10f3de84be6ed62
Igor Antonov <antonov.igor.khv@gmail.com>
2022-10-04 14:31:22 +0300

    7.8. Добавит метод getOffersFromCategory в CategoryController.
    Теперь реализуем метод для получения списка объявлений из определённой
    категории. Для этого немного доработаем контроллер `CategoryController`.
    Добавим метод `getOfferFromCategory`. В нём воспользуемся методом
    `findByCategoryId`.
    
    Обратите внимание на параметр `count`. Техническое задание определяет,
    что пользователь может запросить любое количество объявлений из
    категории, но по умолчанию сервис возвращает `25` объявлений.
    
    В `CategoryController` из параметров запроса извлечём опциональный
    параметр `limit`. Если он задан, передадим в метод `findByCategoryId`.
    
    Для проверки обработчика добавим запрос в `queries.http`.

--- src/modules/category/category.controller.ts
+++ src/modules/category/category.controller.ts
@@ -10,12 +10,22 @@ import CategoryResponse from './response/category.response.js';
 import {fillDTO} from '../../utils/common.js';
 import CreateCategoryDto from './dto/create-category.dto.js';
 import HttpError from '../../common/errors/http-error.js';
+import * as core from 'express-serve-static-core';
+import {OfferServiceInterface} from '../offer/offer-service.interface.js';
+import OfferResponse from '../offer/response/offer.response.js';
+import {RequestQuery} from '../../types/request-query.type.js';
+
+
+type ParamsGetCategory = {
+  categoryId: string;
+}
 
 @injectable()
 export default class CategoryController extends Controller {
   constructor(
     @inject(Component.LoggerInterface) logger: LoggerInterface,
     @inject(Component.CategoryServiceInterface) private readonly categoryService: CategoryServiceInterface,
+    @inject(Component.OfferServiceInterface) private readonly offerService: OfferServiceInterface
   ) {
     super(logger);
 
@@ -23,6 +33,7 @@ export default class CategoryController extends Controller {
 
     this.addRoute({path: '/', method: HttpMethod.Get, handler: this.index});
     this.addRoute({path: '/', method: HttpMethod.Post, handler: this.create});
+    this.addRoute({path: '/:categoryId/offers', method: HttpMethod.Get, handler: this.getOffersFromCategory});
   }
 
   public async index(_req: Request, res: Response): Promise<void> {
@@ -51,5 +62,13 @@ export default class CategoryController extends Controller {
       StatusCodes.CREATED,
       fillDTO(CategoryResponse, result)
     );
+  }
+
+  public async getOffersFromCategory(
+    {params, query}: Request<core.ParamsDictionary | ParamsGetCategory, unknown, unknown, RequestQuery>,
+    res: Response
+  ):Promise<void> {
+    const offers = await this.offerService.findByCategoryId(params.categoryId, query.limit);
+    this.ok(res, fillDTO(OfferResponse, offers));
   }
 }
