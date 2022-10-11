c02a2e5679920dbf55b67b18229f2e3c2fb4e85a
Igor Antonov <antonov.igor.khv@gmail.com>
2022-10-11 13:29:53 +0300

    8.4. Добавит метод verifyUser в UserService.
    Теперь нужно решить откуда вызывать метод для проверки пароля. Напрямую
    обратиться к модели нельзя (мы разделяем приложение на слои), поэтому
    расширим интерфейс `UserServiceInterface` и добавим метод `verifyUser`.
    Он принимает данные пользователя, необходимые для авторизации
    (`email`, `password`). Эти поля мы уже зафиксировали в DTO
    `LoginUserDto`.
    
    Алгоритм проверки такой: находим пользователя по email. Если не нашли,
    значит нет такого пользователя, вернём `null`. Если нашли, вызовем
    метод модели `verifyPassword`. Передадим пароль из `LoginUserDto` и соль.
    
    Если проверка пройдена — вернём `null`.

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -6,6 +6,7 @@ import {UserServiceInterface} from './user-service.interface.js';
 import {LoggerInterface} from '../../common/logger/logger.interface.js';
 import {Component} from '../../types/component.types.js';
 import UpdateUserDto from './dto/update-user.dto.js';
+import LoginUserDto from './dto/login-user.dto.js';
 
 @injectable()
 export default class UserService implements UserServiceInterface {
@@ -42,5 +43,19 @@ export default class UserService implements UserServiceInterface {
     return this.userModel
       .findByIdAndUpdate(userId, dto, {new: true})
       .exec();
+  }
+
+  public async verifyUser(dto: LoginUserDto, salt: string): Promise<DocumentType<UserEntity> | null> {
+    const user = await this.findByEmail(dto.email);
+
+    if (! user) {
+      return null;
+    }
+
+    if (user.verifyPassword(dto.password, salt)) {
+      return user;
+    }
+
+    return null;
   }
 }
