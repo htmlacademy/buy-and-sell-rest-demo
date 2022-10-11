fc18140f82338760a6e1a0b4bae77b319739efd3
Igor Antonov <antonov.igor.khv@gmail.com>
2022-10-11 14:41:16 +0300

    8.9. Добавит автоматическое заполнение `userId` автора комментария.
    Пришло время протестировать нашу middleware. Рассмотрим на примере
    модификации контроллера `CommentController`. При создании комментария
    будем автоматически заполнять `userId`. Это логично, ведь пользователь
    должен оставлять свои комментарии.
    
    Начнём с пересмотра `CreateCommentDto`. Удалим валидатор для поля
    `uerId`. Это поле больше не нужно валидировать, так как мы будем
    заполнять его автоматически. Затем пересмотрим метод `create`.
    Идентификатор пользователя возьмём из объекта запроса.
    
    Всё. Можно выполнить проверку. Попробуйте создать новый комментарий,
    предварительно добавив к нему валидный токен. Комментарий будет создан
    от имени пользователя, токен которого мы использовали.

--- src/modules/comment/dto/create-comment.dto.ts
+++ src/modules/comment/dto/create-comment.dto.ts
@@ -8,6 +8,5 @@ export default class CreateCommentDto {
   @IsMongoId({message: 'offerId field must be a valid id'})
   public offerId!: string;
 
-  @IsMongoId({message: 'userId field must be a valid id'})
   public userId!: string;
 }
