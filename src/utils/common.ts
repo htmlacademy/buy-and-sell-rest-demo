1f899197ca03f70e68ab29c4d033264946275d76
Igor Antonov <antonov.igor.khv@gmail.com>
2022-10-14 15:11:47 +0300

    9.8. Добавит функцию трансформации путей к статическим ресурсам.
    Добавим функцию для трансформации. Её можно сделать разными способами.
    Воспользуемся наиболее простым: пробежимся по объекту в поисках имён
    полей, к значению которых необходимо добавить полный путь. Таких полей
    может быть несколько: `image` и `avatarPath`. В следующем шаге мы их
    зафиксируем в отдельной константе. А пока создадим отдельный модуль с
    константами приложения и в нём определим список имён файлов, к которым
    нужно добавлять путь к директории со статичными файлами. Пока определим
    один файл — `default-avatar.jpg`. Позже мы добавим сюда другие ресурсы,
    которые доступны в приложении изначально и не изменяются.
    
    Функция `transformObject` определяет несколько параметров. В первом
    передаётся список имён полей, где необходимо сделать замену. Во втором
    и третьем — путь к директориям с файлами. В нашем приложении таких
    директории две: статичные файлы и файлы, загруженные клиентом. Последний
    параметр необходим для передачи объекта с данными, который должен
    отправляться клиенту.

--- src/utils/common.ts
+++ src/utils/common.ts
@@ -6,6 +6,8 @@ import { OfferType } from '../types/offer-type.enum.js';
 import { Offer } from '../types/offer.type.js';
 import {ValidationErrorField} from '../types/validation-error-field.type.js';
 import {ServiceError} from '../types/service-error.enum.js';
+import {UnknownObject} from '../types/unknown-object.type.js';
+import {DEFAULT_STATIC_IMAGES} from '../app/application.constant.js';
 
 export const createOffer = (row: string) => {
   const tokens = row.replace('\n', '').split('\t');
@@ -55,3 +57,29 @@ export const transformErrors = (errors: ValidationError[]): ValidationErrorField
   }));
 
 export const getFullServerPath = (host: string, port: number) => `http://${host}:${port}`;
+
+const isObject = (value: unknown) => typeof value === 'object' && value !== null;
+
+export const transformProperty = (
+  property: string,
+  someObject: UnknownObject,
+  transformFn: (object: UnknownObject) => void
+) => {
+  Object.keys(someObject)
+    .forEach((key) => {
+      if (key === property) {
+        transformFn(someObject);
+      } else if (isObject(someObject[key])) {
+        transformProperty(property, someObject[key] as UnknownObject, transformFn);
+      }
+    });
+};
+
+export const transformObject = (properties: string[], staticPath: string, uploadPath: string, data:UnknownObject) => {
+  properties
+    .forEach((property) => transformProperty(property, data, (target: UnknownObject) => {
+      const rootPath = DEFAULT_STATIC_IMAGES.includes(target[property] as string) ? staticPath : uploadPath;
+      target[property] = `${rootPath}/${target[property]}`;
+    }));
+};
+
