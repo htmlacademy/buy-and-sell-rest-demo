aab3be709ce2a5b74f2d17ca6a47f8be43489de6
Igor Antonov <antonov.igor.khv@gmail.com>
2022-10-14 15:09:09 +0300

    9.7. Добавит `ConfigService` в `Controller`.
    Пришло время решить, где добавлять полный путь к изображениям
    (статическим ресурсам). Сделать это можно в нескольких местах. Самый
    простой — в определённом контроллере. Но если пойти этим путём, то это
    придётся делать в нескольких обработчиках и нескольких контроллерах.
    Вдобавок придётся постоянно держать в памяти необходимость модификации
    сформированного ответа.
    
    Более гибкий вариант — решить эту задачу в базовом контроллере. Есть
    только одна проблема. Для получения пути нам потребуется доступ к
    `ConfigService`. Это не проблема: некоторые наши контроллеры и так
    используют его в качестве зависимости. Остаётся только сделать эту
    зависимость обязательной для других контроллеров. Внесём изменения в
    абстрактный класс и сделаем передачу сервиса конфигурации обязательным.
    
    Этот шаг потребует внести изменения в существующие контроллеры: им
    необходимо добавить внедрение сервиса конфигурации.

--- src/common/controller/controller.ts
+++ src/common/controller/controller.ts
@@ -5,12 +5,16 @@ import asyncHandler from 'express-async-handler';
 import {LoggerInterface} from '../logger/logger.interface.js';
 import {RouteInterface} from '../../types/route.interface.js';
 import {ControllerInterface} from './controller.interface.js';
+import {ConfigInterface} from '../config/config.interface.js';
 
 @injectable()
 export abstract class Controller implements ControllerInterface {
   private readonly _router: Router;
 
-  constructor(protected readonly logger: LoggerInterface) {
+  constructor(
+    protected readonly logger: LoggerInterface,
+    protected readonly configService: ConfigInterface
+  ) {
     this._router = Router();
   }
 
--- src/modules/offer/offer.controller.ts
+++ src/modules/offer/offer.controller.ts
@@ -17,6 +17,7 @@ import { DEFAULT_DISCUSSED_OFFER_COUNT, DEFAULT_NEW_OFFER_COUNT } from './offer.
 import { ValidateDtoMiddleware } from '../../common/middlewares/validate-dto.middleware.js';
 import {DocumentExistsMiddleware} from '../../common/middlewares/document-exists.middleware.js';
 import {PrivateRouteMiddleware} from '../../common/middlewares/private-route.middleware.js';
+import {ConfigInterface} from '../../common/config/config.interface.js';
 
 type ParamsGetOffer = {
   offerId: string;
@@ -26,10 +27,11 @@ type ParamsGetOffer = {
 export default class OfferController extends Controller {
   constructor(
     @inject(Component.LoggerInterface) logger: LoggerInterface,
+    @inject(Component.ConfigInterface) configService: ConfigInterface,
     @inject(Component.OfferServiceInterface) private readonly offerService: OfferServiceInterface,
     @inject(Component.CommentServiceInterface) private readonly commentService: CommentServiceInterface
   ) {
-    super(logger);
+    super(logger, configService);
 
     this.logger.info('Register routes for OfferController…');
     this.addRoute({
