triple-sun/buy-and-sell-2

1.3. Добавит команду для получения помощи.
Создадим ещё одну простую команду — `--help`. Эта команда возвращает информацию о доступных командах. Также она используется в качестве команды по умолчанию. Если пользователь ничего не ввёл, вызывается именно эта команда.

src/cli-command/help-command.ts
@@ -1,0 +1,20 @@
+import { CliCommandInterface } from './cli-command.interface.js';
+
+export default class HelpCommand implements CliCommandInterface {
+  public readonly name = '--help';
+
+  public async execute(): Promise<void> {
+    console.log(`
+        Программа для подготовки данных для REST API сервера.
+
+        Пример:
+            main.js --<command> [--arguments]
+
+        Команды:
+            --version:                   # выводит номер версии
+            --help:                      # печатает этот текст
+            --import <path>:             # импортирует данные из TSV
+            --generator <n> <path> <url> # генерирует произвольное количество тестовых данных
+        `);
+  }
+}

htmlacademy/buy-and-sell-rest-demo

src/cli-command/help-command.ts
@@ -1,0 +1,20 @@
+import { CliCommandInterface } from './cli-command.interface.js';
+
+export default class HelpCommand implements CliCommandInterface {
+  public readonly name = '--help';
+
+  public async execute(): Promise<void> {
+    console.log(`
+        Программа для подготовки данных для REST API сервера.
+
+        Пример:
+            main.js --<command> [--arguments]
+
+        Команды:
+            --version:                   # выводит номер версии
+            --help:                      # печатает этот текст
+            --import <path>:             # импортирует данные из TSV
+            --generate <n> <path> <url>  # генерирует произвольное количество тестовых данных
+        `);
+  }
+}