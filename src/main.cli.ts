ada18e440cb2cf234578f95945b0a6cd41739554
Igor Antonov <antonov.igor.khv@gmail.com>
2023-05-02 17:19:57 +0300

    1.14. Зарегистрируем команду `ImportCommand`.
    Чтобы команда `--import` заработала, зарегистрируем её у менеджера `CLIApplication`.

--- src/main.cli.ts
+++ src/main.cli.ts
@@ -1,9 +1,10 @@
 import CLIApplication from './app/cli.js';
 import HelpCommand from './core/cli-command/helper.command.js';
+import ImportCommand from './core/cli-command/import.command.js';
 import VersionCommand from './core/cli-command/version.command.js';
 
 const myManager = new CLIApplication();
 myManager.registerCommands([
-  new HelpCommand, new VersionCommand
+  new HelpCommand, new VersionCommand, new ImportCommand,
 ]);
 myManager.processCommand(process.argv);
