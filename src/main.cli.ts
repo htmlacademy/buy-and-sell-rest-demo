0684b92d139c89979e5c4efb5091f5e1e5b61ef3
Igor Antonov <antonov.igor.khv@gmail.com>
2023-05-02 17:22:56 +0300

    1.15. Добавит `shebang` сценария cli.
    Остался последний штрих. Команды работают, но для запуска приходится
    использовать `ts-node` или `node`. С консольными приложениями всё
    обычно происходит не так.
    
    Мы их запускаем в терминале. Чтобы наше приложение удалось запустить
    в терминале, нам необходимо прописать `sheband`.
    
    sheband — последовательность из символов `#` и `!`. После этого символа
    можно определить путь к приложению, которое умеет исполнять данный
    скрипт.
    
    Укажем в качестве такого приложения путь к Node.js.
    
    Попробуем собрать проект (`npm run build`) и запустить:
    `./dist/main.cli.js --help`. Ничего не выходит. Терминал выдаст ошибку
    «permission denied».
    
    Чтобы исправить ошибку — необходимо сделать файл запускаемым. Для этого
    добавим файлу дополнительный атрибут с помощью команды `chmod`. Выполните:
    `chmod u+x ./dist/main.cli.js`. После этого попробуйте запустить приложение
    в терминале.

--- src/main.cli.ts
+++ src/main.cli.ts
@@ -1,3 +1,5 @@
+#!/usr/bin/env node
+
 import CLIApplication from './app/cli.js';
 import HelpCommand from './core/cli-command/helper.command.js';
 import ImportCommand from './core/cli-command/import.command.js';
