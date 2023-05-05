7b0c0c696f8308d8a2db5ef04ad9af7fc91f4ebc
Igor Antonov <antonov.igor.khv@gmail.com>
2023-05-05 16:15:33 +0300

    2.7. Добавит генератор объявлений.
    Напишем класс `OfferGenerator`. Он имплементирует интерфейс
    `OfferGeneratorInterface`.
    
    Данные для генерации объявлений класс принимает через
    конструктор. Воспользуемся краткой записью: через параметр
    конструктора определим приватное свойство `mockData`.
    
    В методе `generate` напишем код для формирования случайного
    объявления. Для этого есть всё необходимое.
    
    Единственное, что пока пропустим — поле с датой публикации.

---
+++ src/modules/offer-generator/offer-generator.ts
@@ -0,0 +1,36 @@
+import dayjs from 'dayjs';
+import { OfferGeneratorInterface } from './offer-generator.interface.js';
+import { MockData } from '../../types/mock-data.type.js';
+import { generateRandomValue, getRandomItem, getRandomItems } from '../../core/helpers/index.js';
+import { OfferType } from '../../types/offer-type.enum.js';
+
+const MIN_PRICE = 500;
+const MAX_PRICE = 2000;
+
+const FIRST_WEEK_DAY = 1;
+const LAST_WEEK_DAY = 7;
+
+export default class OfferGenerator implements OfferGeneratorInterface {
+  constructor(private readonly mockData: MockData) {}
+
+  public generate(): string {
+    const categories = getRandomItems<string>(this.mockData.categories).join(';');
+    const title = getRandomItem<string>(this.mockData.titles);
+    const description = getRandomItem<string>(this.mockData.descriptions);
+    const photo = getRandomItem<string>(this.mockData.offerImages);
+    const type = getRandomItem([OfferType.Buy, OfferType.Sell]);
+    const price = generateRandomValue(MIN_PRICE, MAX_PRICE).toString();
+    const author = getRandomItem<string>(this.mockData.users);
+    const email = getRandomItem<string>(this.mockData.emails);
+    const avatar = getRandomItem<string>(this.mockData.avatars);
+    const createdDate = dayjs().subtract(generateRandomValue(FIRST_WEEK_DAY, LAST_WEEK_DAY), 'day').toISOString();
+
+    const [firstname, lastname] = author.split(' ');
+
+    return [
+      title, description, createdDate,
+      photo, type, price, categories,
+      firstname, lastname, email, avatar,
+    ].join('\t');
+  }
+}
