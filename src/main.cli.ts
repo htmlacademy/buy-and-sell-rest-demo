b373617b13560d778451412730414d04fa4affd2
Igor Antonov <antonov.igor.khv@gmail.com>
2023-05-05 16:28:35 +0300

    2.12. Зарегистрирует команду генерации объявлений.
    Чтобы воспользоваться командой `--generate` зарегистрируем её у
    менеджера. Точно так же, как и остальные.
    
    После регистрации запустите в одном терминале JSON-server —
    `npm run mock:server`. Затем откройте второй терминал и выполните
    команду `--generate`:
    
    ```
    npm run ts ./src/cli.ts -- --generate 100 ./mocks/test-data.tsv http://localhost:3123/api
    ```
    
    После её выполнения, в директории `mocks` появится файл `test-data.tsv`
    с сотней сгенерированных объявлений.

--- src/main.cli.ts
+++ src/main.cli.ts
@@ -4,9 +4,11 @@ import CLIApplication from './app/cli.js';
 import HelpCommand from './core/cli-command/helper.command.js';
 import ImportCommand from './core/cli-command/import.command.js';
 import VersionCommand from './core/cli-command/version.command.js';
+import GenerateCommand from './core/cli-command/generate-command.js';
 
 const myManager = new CLIApplication();
 myManager.registerCommands([
-  new HelpCommand, new VersionCommand, new ImportCommand,
+  new HelpCommand, new VersionCommand,
+  new ImportCommand, new GenerateCommand()
 ]);
 myManager.processCommand(process.argv);
