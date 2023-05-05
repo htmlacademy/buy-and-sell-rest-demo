020287a13ea17547f9d856ca050c9c1b890e6baf
Igor Antonov <antonov.igor.khv@gmail.com>
2023-05-05 15:48:30 +0300

    2.4. Добавит вспомогательные функции.
    Мы подбираемся к задаче на генерацию тестовых данных.
    При подготовке тестовых данных почти всегда требуются
    функции для получения случайных элементов. С написанием
    таких функций вы уже хорошо знакомы.
    
    Создадим новый модуль `random.ts` и в нём реализуем
    несколько функций:
    
    * `generateRandomValue` — для получения случайного числа в диапазоне.
    * `getRandomItems` — для получения случайных элементов массива.
    * `getRandomItem` — для получения случайного элемента массива.
    
    Обратите внимание, во всех функциях применяются дженерики.
    Типы заранее неизвестны.

---
+++ src/core/helpers/random.ts
@@ -0,0 +1,13 @@
+export function generateRandomValue(min:number, max: number, numAfterDigit = 0) {
+  return +((Math.random() * (max - min)) + min).toFixed(numAfterDigit);
+}
+
+export function getRandomItems<T>(items: T[]):T[] {
+  const startPosition = generateRandomValue(0, items.length - 1);
+  const endPosition = startPosition + generateRandomValue(startPosition, items.length);
+  return items.slice(startPosition, endPosition);
+}
+
+export function getRandomItem<T>(items: T[]):T {
+  return items[generateRandomValue(0, items.length - 1)];
+}
