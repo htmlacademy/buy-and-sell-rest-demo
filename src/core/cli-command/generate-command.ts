69ce1a8af9ea33c9c5b8900b3a8b2a21a47223e1
Igor Antonov <antonov.igor.khv@gmail.com>
2023-05-05 16:26:59 +0300

    2.11. Добавит генератор в команду `GenerateCommand`.
    Воспользуемся полученными данными и передадим их в генератор объявлений.
    Для этого создадим экземпляр класса `OfferGenerator`.
    
    Затем сформируем нужное количество объявлений. Запись в файл осуществим
    с помощью функции `appendFile`. По умолчанию она создаёт файл, если он
    не существует, а если существует, то дописывает в него данные.
    
    Обратите внимание, при записи очередной строки с данными добавляет
    символ перевода строки.
    
    Также акцентируем внимание на порядок действий: сначала генерируем
    строку с информацией по объявлению, затем сразу её записываем в файл.
    
    Может показаться, что удобней сначала собрать сгенерированные
    объявления в массив и записать их разом, но это не так.
    
    Для процесса Node.js выделяется определённый объём памяти. Если его
    превысить, то возникнет ошибка.
    
    Попробуйте в порядке эксперимента собрать большое количество объявлений
    в массив, а потом записать их разом с помощью функции `writeFile`
    или `appendFile`.

--- src/core/cli-command/generate-command.ts
+++ src/core/cli-command/generate-command.ts
@@ -1,6 +1,8 @@
 import got from 'got';
+import { appendFile } from 'node:fs/promises';
 import { CliCommandInterface } from './cli-command.interface.js';
 import { MockData } from '../../types/mock-data.type.js';
+import OfferGenerator from '../../modules/offer-generator/offer-generator.js';
 
 export default class GenerateCommand implements CliCommandInterface {
   public readonly name = '--generate';
@@ -14,6 +16,15 @@ export default class GenerateCommand implements CliCommandInterface {
       this.initialData = await got.get(url).json();
     } catch {
       console.log(`Can't fetch data from ${url}.`);
+      return;
     }
+
+    const offerGeneratorString = new OfferGenerator(this.initialData);
+
+    for (let i = 0; i < offerCount; i++) {
+      await appendFile(filepath, `${offerGeneratorString.generate()}\n`, 'utf8');
+    }
+
+    console.log(`File ${filepath} was created!`);
   }
 }
