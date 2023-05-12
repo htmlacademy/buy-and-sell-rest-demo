980bc162f486ffc7c89ff07e55809723d31d554c
Igor Antonov <antonov.igor.khv@gmail.com>
2023-05-12 17:18:42 +0300

    3.19. Добавит контейнер с зависимостями. WIP
    Перейдём к файлу `main.ts` (точка входа). В нём мы создаём экземпляры
    всех необходимых классов и зависимостей для них. Попробуем сделать то же
    самое с помощью возможностей `inversify`.
    
    Первым делом нам потребуется создать контейнер. В контексте библиотеки
    `inversify`, контейнер — это место, где происходит настройка и привязка
    зависимостей.
    
    При создании контейнера мы объясняем `inversify` какую реализацию
    следует использовать для определённого интерфейса.
    
    Например, интерфейсу `LoggerInterface` соответствует реализация в
    виде класса `PinoService`. А интерфейсу `ConfigInterface` реализация
    `ConfigService` и так далее.
    
    За счёт построения контейнера, в любом другом классе мы сможем получить
    нужную реализацию. При этом `inversify` возьмёт на себя создание
    необходимых экземпляров.
    
    WIP: Приложение не запускается.

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,20 @@
+import 'reflect-metadata';
+import { Container } from 'inversify';
 import PinoService from './core/logger/pino.service.js';
 import RestApplication from './app/rest.js';
 import ConfigService from './core/config/config.service.js';
+import { AppComponent } from './types/app-component.enum.js';
+import { LoggerInterface } from './core/logger/logger.interface.js';
+import { ConfigInterface } from './core/config/config.interface.js';
+import { RestSchema } from './core/config/rest.schema.js';
 
 async function bootstrap() {
-  const logger = new PinoService();
-  const config = new ConfigService(logger);
+  const container = new Container();
+  container.bind<RestApplication>(AppComponent.RestApplication).to(RestApplication);
+  container.bind<LoggerInterface>(AppComponent.LoggerInterface).to(PinoService);
+  container.bind<ConfigInterface<RestSchema>>(AppComponent.ConfigInterface).to(ConfigService);
 
-  const application = new RestApplication(logger, config);
+  const application = container.get<RestApplication>(AppComponent.RestApplication);
   await application.init();
 }
 
