b23da6944640566a32bf89544460040dc9d7a276
Igor Antonov <antonov.igor.khv@gmail.com>
2023-05-12 17:20:24 +0300

    3.22. Внедрит зависимости в `RestApplication`.
    Классу `RestApplication` тоже нужны зависимости. Воспользуемся
    знакомым приёмом: сначала задекорируем класс с помощью декоратора
    `injectable`, а затем воспользуемся `inject` для внедрения зависимостей.
    
    Обратите внимание, применять декоратор `inject` можно и для
    сокращённого синтаксиса при определении полей класса в конструкторе.
    
    Попробуйте запустить приложение и убедиться, что всё работает также,
    как и раньше.
    
    Обратите внимание, в точку входа приложения необходимо импортировать `reflect-metadata`. Иначе inversify не сможет работать и извлекать мета информацию

--- src/app/rest.ts
+++ src/app/rest.ts
@@ -1,11 +1,14 @@
 import { LoggerInterface } from '../core/logger/logger.interface.js';
 import { ConfigInterface } from '../core/config/config.interface.js';
 import { RestSchema } from '../core/config/rest.schema.js';
+import { AppComponent } from '../types/app-component.enum.js';
+import { inject, injectable } from 'inversify';
 
+@injectable()
 export default class RestApplication {
   constructor(
-    private readonly logger: LoggerInterface,
-    private readonly config: ConfigInterface<RestSchema>,
+    @inject(AppComponent.LoggerInterface) private readonly logger: LoggerInterface,
+    @inject(AppComponent.ConfigInterface) private readonly config: ConfigInterface<RestSchema>,
   ) {}
 
   public async init() {
