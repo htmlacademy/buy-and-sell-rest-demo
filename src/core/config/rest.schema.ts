94a322e34a9606ed4e08966033a8b2d6c1a03b6d
Igor Antonov <antonov.igor.khv@gmail.com>
2023-05-12 16:55:33 +0300

    3.13. Добавит схему конфигурации.
    `convict` поможет выполнить проверку конфигурации на основании схем.
    Попробуем описать схему.
    
    Создадим отдельный модуль `rest.schema.ts` и опишем в нём интерфейс
    `RestSchema`.
    
    В интерфейсе перечислим все имена переменных окружения, которые мы
    ожидаем получить. Пока такой переменной станет `PORT`. Через неё будем
    передавать номер порта, по которому будут подключаться клиенты.
    
    Затем определим саму схему. Для описания схемы потребуется `convict`.
    Функции `convict` передадим в параметр типа `RestSchema`, а
    аргументом объект с описанием схемы.
    
    Ключи схемы: по этим именам будем получать значения. Они могут
    отличаться от имён переменных окружения, но удобней использовать
    прописные буквы.
    
    Значением любого свойства схемы является объект. В нём обязательно
    определить несколько ключей: `env`, `default` и `format`. В ключе `doc`
    можно указать произвольный комментарий.
    
    В ключе `env` прописываем имя переменной окружения. Из неё `convict`
    извлечёт значение.
    
    Для каждого элемента схемы следует предусмотреть значение по умолчанию.
    Оно будет использоваться если указанная переменная не будет
    установлена.
    
    Если переменная не предусматривает значения по умолчанию (например,
    токен для доступа к БД), то в качестве значения по умолчанию следует
    использовать `null`. Это приведёт к ошибке, если переменная не будет
    установлена.

---
+++ src/core/config/rest.schema.ts
@@ -0,0 +1,14 @@
+import convict from 'convict';
+
+export type RestSchema = {
+  PORT: number;
+}
+
+export const configRestSchema = convict<RestSchema>({
+  PORT: {
+    doc: 'Port for incoming connections',
+    format: 'port',
+    env: 'PORT',
+    default: 4000
+  },
+});
