e019eba32d50d9f085c6691353fbb72973ceba61
Igor Antonov <antonov.igor.khv@gmail.com>
2023-05-12 16:25:13 +0300

    3.11. Добавит `ConfigService` в `Application`.
    В `main.ts` добавим создание экземпляра класса `ConfigService`. При
    создании экземпляра передадим в конструктор настроенный `logger`.
    
    Настроенный экземпляр `config` передадим в конструктор `Application`
    вторым аргументом.
    
    Внутри класса `Application` добавим один параметр для конструктора
    (для приёма экземпляра `ConfigService`).
    
    В методе `init` добавим вывод значения переменной `PORT`. Пока только
    для проверки.
    
    Протестируйте пример на данном шаге. Создайте в корне проекта
    файл `.env` и определите в нём переменную `PORT=5000`. Затем запустите
    приложение. Значение переменной должно быть выведено в терминал.

--- src/app/rest.ts
+++ src/app/rest.ts
@@ -1,11 +1,14 @@
 import { LoggerInterface } from '../core/logger/logger.interface.js';
+import { ConfigInterface } from '../core/config/config.interface';
 
 export default class RestApplication {
   constructor(
-    private readonly logger: LoggerInterface
+    private readonly logger: LoggerInterface,
+    private readonly config: ConfigInterface,
   ) {}
 
   public async init() {
     this.logger.info('Application initialization…');
+    this.logger.info(`Get value from env $PORT: ${this.config.get('PORT')}`);
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import PinoService from './core/logger/pino.service.js';
 import RestApplication from './app/rest.js';
+import ConfigService from './core/config/config.service.js';
 
 async function bootstrap() {
   const logger = new PinoService();
+  const config = new ConfigService(logger);
 
-  const application = new RestApplication(logger);
+  const application = new RestApplication(logger, config);
   await application.init();
 }
 
