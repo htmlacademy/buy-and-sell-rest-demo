21d5697ec35154e95bbdc1e1921c2a73e47f0172
Igor Antonov <antonov.igor.khv@gmail.com>
2023-05-17 11:51:48 +0300

    4.9. Завершит настройку подключения к базе данных.
    Для установки соединения с базой данных всё готово. Остаётся внедрить
    `DatabaseClientInterface` в виде зависимости в `RestApplication` и воспользоваться им в методе `init`. Внедрять будем по аналогии с другими
    зависимостями — через конструктор.
    
    В методе `_initDb` воспользуемся функцией `getMongoURI` и передадим ей значения всех переменных окружения. Получим строку соединения, а затем передадим аргументом в метод `connect`.
    
    На этом шаге попробуйте запустить приложение и убедиться, что
    соединение с сервером баз данных успешно устанавливается. Перед
    запуском не забудьте проверить установку всех переменных окружения
    в файле `.env`.
    
    И последнее, на что следует обратить внимание. Теперь наше приложение
    не завершает работу, оно остаётся запущенным.

--- src/app/rest.ts
+++ src/app/rest.ts
@@ -3,16 +3,35 @@ import { ConfigInterface } from '../core/config/config.interface.js';
 import { RestSchema } from '../core/config/rest.schema.js';
 import { AppComponent } from '../types/app-component.enum.js';
 import { inject, injectable } from 'inversify';
+import { DatabaseClientInterface } from '../core/database-client/database-client.interface';
+import { getMongoURI } from '../core/helpers/index.js';
 
 @injectable()
 export default class RestApplication {
   constructor(
     @inject(AppComponent.LoggerInterface) private readonly logger: LoggerInterface,
     @inject(AppComponent.ConfigInterface) private readonly config: ConfigInterface<RestSchema>,
+    @inject(AppComponent.DatabaseClientInterface) private readonly databaseClient: DatabaseClientInterface
   ) {}
+
+  private async _initDb() {
+    const mongoUri = getMongoURI(
+      this.config.get('DB_USER'),
+      this.config.get('DB_PASSWORD'),
+      this.config.get('DB_HOST'),
+      this.config.get('DB_PORT'),
+      this.config.get('DB_NAME'),
+    );
+
+    return this.databaseClient.connect(mongoUri);
+  }
 
   public async init() {
     this.logger.info('Application initialization…');
     this.logger.info(`Get value from env $PORT: ${this.config.get('PORT')}`);
+
+    this.logger.info('Init database…');
+    await this._initDb();
+    this.logger.info('Init database completed');
   }
 }
