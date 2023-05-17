7b716ffccf8ad67275a59358d16a12e02c38aad6
Igor Antonov <antonov.igor.khv@gmail.com>
2023-05-17 12:49:52 +0300

    4.12. Расширит интерфейс `UserDocument`. WIP
    При сохранении данных в базу, нередко возникает задача зафиксировать
    время создания и время обновления определённой записи. Это может быть
    полезно как для последующего анализа, так и для отдельных сценариев в
    работе приложения. Например, требуется вывести список документов из
    коллекции, предварительно отсортировав их по дате создания — сначала
    новые, затем старые.
    
    Решение заключается в расширении схемы. Можно добавить несколько полей,
    а затем расширить интерфейс `UserDocument`. На самом деле, напрямую
    прописывать дополнительные поля не нужно. Всё необходимое Mongoose
    сделает автоматически, достаточно задать соответствующую настройку.
    
    Проверим на практике. Сначала добавим в интерфейс `UserDocument` два
    поля `createdAt` и `updatedAt` типа `Date`. В первом будем хранить дату
    создания записи, во втором дату обновления.
    
    Затем при создании схемы передадим объект с настройками. В нём укажем
    ключ `timestamps` со значением `true`. Тем самым сообщим о
    необходимости учитывать дату создания и обновления записи. Нам не
    придётся самостоятельно готовить значение для `createdAt` и `updatedAt`.
    
    Попробуйте вновь проверить работу этого кода на примере создания нового
    пользователя:
    
    ```
      const user = await UserModel.create({
        email: 'test@email.local',
        avatarPath: 'keks.jpg',
        firstname: 'Keks',
        lastname: 'Unknown'
       });
    
     console.log(user);
    ```
    
    Обратите внимание, код остался прежним, но теперь при сохранении
    документа формируется значение для полей `createdAt` и `updatedAt`.
    
    WIP: Начальное состояние. Модель не используется по назначению.

--- src/modules/user/user.model.ts
+++ src/modules/user/user.model.ts
@@ -1,13 +1,18 @@
 import mongoose from 'mongoose';
 import { User } from '../../types/user.type.js';
 
-export interface UserDocument extends User, mongoose.Document {}
+export interface UserDocument extends User, mongoose.Document {
+  createdAt: Date,
+  updatedAt: Date,
+}
 
 const userSchema = new mongoose.Schema({
   email: String,
   avatarPath: String,
   firstname: String,
   lastname: String,
+}, {
+  timestamps: true,
 });
 
 export const UserModel = mongoose.model<UserDocument>('User', userSchema);
