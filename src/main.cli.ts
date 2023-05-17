cc807e6619b95b373cb5cfed0c6455b46832d231
Igor Antonov <antonov.igor.khv@gmail.com>
2023-05-17 18:20:00 +0300

    4.35. Добавит импорт `reflect-metadata`.

--- src/main.cli.ts
+++ src/main.cli.ts
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-
+import 'reflect-metadata';
 import CLIApplication from './app/cli.js';
 import HelpCommand from './core/cli-command/helper.command.js';
 import ImportCommand from './core/cli-command/import.command.js';
