b918d433cf4de7e5ed7efe6a9c2cfe0d0940e444
Igor Antonov <antonov.igor.khv@gmail.com>
2023-05-17 11:50:41 +0300

    4.8. Добавит привязку для `DatabaseClientInterface`.
    Добавим в контейнер новую привязку. На этот раз для `DatabaseClientInterface`. Свяжем с реализацией `MongoClientService`. Не забудем указать `inSingletonScope`. Достаточно одного экземпляра.

--- src/main.ts
+++ src/main.ts
@@ -7,12 +7,15 @@ import { AppComponent } from './types/app-component.enum.js';
 import { LoggerInterface } from './core/logger/logger.interface.js';
 import { ConfigInterface } from './core/config/config.interface.js';
 import { RestSchema } from './core/config/rest.schema.js';
+import { DatabaseClientInterface } from './core/database-client/database-client.interface.js';
+import MongoClientService from './core/database-client/mongo-client.service.js';
 
 async function bootstrap() {
   const container = new Container();
   container.bind<RestApplication>(AppComponent.RestApplication).to(RestApplication).inSingletonScope();
   container.bind<LoggerInterface>(AppComponent.LoggerInterface).to(PinoService).inSingletonScope();
   container.bind<ConfigInterface<RestSchema>>(AppComponent.ConfigInterface).to(ConfigService).inSingletonScope();
+  container.bind<DatabaseClientInterface>(AppComponent.DatabaseClientInterface).to(MongoClientService).inSingletonScope();
 
   const application = container.get<RestApplication>(AppComponent.RestApplication);
   await application.init();
