e35cbc667a4529d992a2507d6821b98068629d56
Igor Antonov <antonov.igor.khv@gmail.com>
2023-05-17 11:40:07 +0300

    4.5. Добавит класс `MongoClientService`.
    Следующим шагом запрограммируем класс и имплементируем интерфейс `DatabaseClientInterface`. Пока сделаем его максимально простым. Дополнительные возможности прикрутим в процессе разработки приложения.
    
    Для работы с базой данных потребуется подключить `mongoose`. Этот пакет
    мы уже установили (смотри коммит 4.1).
    
    Класс `MongoClientService` имплементирует интерфейс `DatabaseClientInterface`. Процесс подключения к БД неплохо залогировать. Следовательно, в классе `DatabaseService` потребуется доступ к логеру. Пометим класс декоратором `@injectbale` и внедрим логер в конструктор в качестве зависимости.
    
    Внутри методов `connect` и `disconnect` вызовем одноимённые методы
    объекта `mongoose`. Прокидывать `mongoose` наружу пока не будем.

---
+++ src/core/database-client/mongo-client.service.ts
@@ -0,0 +1,45 @@
+import { inject, injectable } from 'inversify';
+import mongoose, { Mongoose } from 'mongoose';
+import { DatabaseClientInterface } from './database-client.interface.js';
+import { AppComponent } from '../../types/app-component.enum.js';
+import { LoggerInterface } from '../logger/logger.interface.js';
+
+@injectable()
+export default class MongoClientService implements DatabaseClientInterface {
+  private isConnected = false;
+  private mongooseInstance: Mongoose | null = null;
+
+  constructor(
+    @inject(AppComponent.LoggerInterface) private readonly logger: LoggerInterface
+  ) {}
+
+  private async _connect(uri:string): Promise<void> {
+    this.mongooseInstance = await mongoose.connect(uri);
+    this.isConnected = true;
+  }
+
+  private async _disconnect(): Promise<void> {
+    await this.mongooseInstance?.disconnect();
+    this.isConnected = false;
+    this.mongooseInstance = null;
+  }
+
+  public async connect(uri: string): Promise<void> {
+    if (this.isConnected) {
+      throw new Error('MongoDB client already connected');
+    }
+
+    this.logger.info('Trying to connect to MongoDB…');
+    await this._connect(uri);
+    this.logger.info('Database connection established.');
+  }
+
+  public async disconnect(): Promise<void> {
+    if (!this.isConnected) {
+      throw new Error('Not connected to the database');
+    }
+
+    await this._disconnect();
+    this.logger.info('Database connection closed.');
+  }
+}
