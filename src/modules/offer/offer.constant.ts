bf9d818f056b72a143ece297b3c32a189bc383b0
Igor Antonov <antonov.igor.khv@gmail.com>
2023-05-19 17:09:55 +0300

    5.10. Реализует обновлённый интерфейс `OfferService`.
    В этом коммите реализуем все методы обновлённого интерфейса `OfferServiceInterface`.
    
    Принцип реализации не сильно отличается: по факту это комбинация методов, которые предоставляет Mongoose. Здесь мы добавляем вызов метода `sort` (для сортировки),
    `findByIdAndUpdate` (обновления документа), `limit` (ограничения вывода) и так далее.

---
+++ src/modules/offer/offer.constant.ts
@@ -0,0 +1,1 @@
+export const DEFAULT_OFFER_COUNT = 25;
--- src/modules/offer/offer.service.ts
+++ src/modules/offer/offer.service.ts
@@ -5,6 +5,9 @@ import { OfferEntity } from './offer.entity.js';
 import { AppComponent } from '../../types/app-component.enum.js';
 import { LoggerInterface } from '../../core/logger/logger.interface.js';
 import { OfferServiceInterface } from './offer-service.interface.js';
+import { SortType } from '../../types/sort-type.enum.js';
+import UpdateOfferDto from './dto/update-offer.dto.js';
+import { DEFAULT_OFFER_COUNT } from './offer.constant.js';
 
 @injectable()
 export default class OfferService implements OfferServiceInterface {
@@ -23,6 +26,64 @@ export default class OfferService implements OfferServiceInterface {
   public async findById(offerId: string): Promise<DocumentType<OfferEntity> | null> {
     return this.offerModel
       .findById(offerId)
+      .populate(['userId', 'categories'])
+      .exec();
+  }
+
+  public async find(): Promise<DocumentType<OfferEntity>[]> {
+    return this.offerModel
+      .find()
+      .populate(['userId', 'categories'])
+      .exec();
+  }
+
+  public async deleteById(offerId: string): Promise<DocumentType<OfferEntity> | null> {
+    return this.offerModel
+      .findByIdAndDelete(offerId)
+      .exec();
+  }
+
+  public async updateById(offerId: string, dto: UpdateOfferDto): Promise<DocumentType<OfferEntity> | null> {
+    return this.offerModel
+      .findByIdAndUpdate(offerId, dto, {new: true})
+      .populate(['userId', 'categories'])
+      .exec();
+  }
+
+  public async findByCategoryId(categoryId: string, count?: number): Promise<DocumentType<OfferEntity>[]> {
+    const limit = count ?? DEFAULT_OFFER_COUNT;
+    return this.offerModel
+      .find({categories: categoryId}, {}, {limit})
+      .populate(['userId', 'categories'])
+      .exec();
+  }
+
+  public async exists(documentId: string): Promise<boolean> {
+    return (await this.offerModel
+      .exists({_id: documentId})) !== null;
+  }
+
+  public async incCommentCount(offerId: string): Promise<DocumentType<OfferEntity> | null> {
+    return this.offerModel
+      .findByIdAndUpdate(offerId, {'$inc': {
+        commentCount: 1,
+      }}).exec();
+  }
+
+  public async findNew(count: number): Promise<DocumentType<OfferEntity>[]> {
+    return this.offerModel
+      .find()
+      .sort({createdAt: SortType.Down})
+      .limit(count)
+      .populate(['userId', 'categories'])
+      .exec();
+  }
+
+  public async findDiscussed(count: number): Promise<DocumentType<OfferEntity>[]> {
+    return this.offerModel
+      .find()
+      .sort({commentCount: SortType.Down})
+      .limit(count)
       .populate(['userId', 'categories'])
       .exec();
   }
