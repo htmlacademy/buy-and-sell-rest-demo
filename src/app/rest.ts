6be36c5e6335a31a0fd9eab24de6531873a71f63
Igor Antonov <antonov.igor.khv@gmail.com>
2023-05-25 18:03:13 +0300

    6.2. Добавит создание и запуск сервера Express.
    Добавим создание экземпляра сервера с помощью Express в класс `Application`. Экземпляр сохраним в приватном поле `expressApplication`.
    После этого в методе `init` воспользуемся экземпляром `express` и
    запустим сервер на прослушивание входящих подключений. Для этого вызовем метод `listen`. Ему нужно пережать порт, на который будем принимать подключения. Номер порта возьмём из конфигурации (`PORT`).

--- src/app/rest.ts
+++ src/app/rest.ts
@@ -5,16 +5,23 @@ import { AppComponent } from '../types/app-component.enum.js';
 import { inject, injectable } from 'inversify';
 import { DatabaseClientInterface } from '../core/database-client/database-client.interface';
 import { getMongoURI } from '../core/helpers/index.js';
+import express, { Express } from 'express';
 
 @injectable()
 export default class RestApplication {
+  private expressApplication: Express;
+
   constructor(
     @inject(AppComponent.LoggerInterface) private readonly logger: LoggerInterface,
     @inject(AppComponent.ConfigInterface) private readonly config: ConfigInterface<RestSchema>,
     @inject(AppComponent.DatabaseClientInterface) private readonly databaseClient: DatabaseClientInterface
-  ) {}
+  ) {
+    this.expressApplication = express();
+  }
 
   private async _initDb() {
+    this.logger.info('Init database…');
+
     const mongoUri = getMongoURI(
       this.config.get('DB_USER'),
       this.config.get('DB_PASSWORD'),
@@ -23,15 +30,23 @@ export default class RestApplication {
       this.config.get('DB_NAME'),
     );
 
-    return this.databaseClient.connect(mongoUri);
+    await this.databaseClient.connect(mongoUri);
+    this.logger.info('Init database completed');
+  }
+
+  private async _initServer() {
+    this.logger.info('Try to init server…');
+
+    const port = this.config.get('PORT');
+    this.expressApplication.listen(port);
+
+    this.logger.info(`🚀Server started on http://localhost:${this.config.get('PORT')}`);
   }
 
   public async init() {
     this.logger.info('Application initialization…');
-    this.logger.info(`Get value from env $PORT: ${this.config.get('PORT')}`);
 
-    this.logger.info('Init database…');
     await this._initDb();
-    this.logger.info('Init database completed');
+    await this._initServer();
   }
 }
