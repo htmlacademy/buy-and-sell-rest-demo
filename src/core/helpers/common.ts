b1e03c4a12b8e0f18067b0d6e3d3dad9c64446e6
Igor Antonov <antonov.igor.khv@gmail.com>
2023-05-26 16:56:42 +0300

    6.26. Добавит функцию `createErrorObject`.
    В `ExceptionFilter` мы вручную формируем объект с ошибкой, который будет отправлен пользователю. Это не очень удобно, когда есть разные типы ошибок. В таких случаях придётся дублировать код и если в будущем захотим расширить информацию в объекте, который отправляем пользователю, придётся пробежаться по всем участкам `ExceptionFilter`, где происходило формирование ошибок.
    
    Упростить эту задачу поможет функция `createErrorObject`. Пока она будет максимально простой. Аргументом она принимает текст ошибки и возвращает объект с одним свойством `error`. Этот объект будем отправлять клиенту.

--- src/core/helpers/common.ts
+++ src/core/helpers/common.ts
@@ -13,3 +13,9 @@ export const createSHA256 = (line: string, salt: string): string => {
 export function fillDTO<T, V>(someDto: ClassConstructor<T>, plainObject: V) {
   return plainToInstance(someDto, plainObject, { excludeExtraneousValues: true });
 }
+
+export function createErrorObject(message: string) {
+  return {
+    error: message,
+  };
+}
