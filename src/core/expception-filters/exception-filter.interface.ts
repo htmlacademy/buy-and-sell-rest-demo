8b455d1e67a38714f4f45e367ff232e1bca984da
Igor Antonov <antonov.igor.khv@gmail.com>
2023-05-26 16:30:27 +0300

    6.16. Добавит интерфейс `ExceptionFilterInterface`.
    `CategoryController` полностью готов, теперь пришло время подумать про обработку ошибок. От ошибок никуда не деться и они рано или поздно появится. Поэтому важно научиться их обрабатывать.
    
    Вы знаете, что для обработки ошибок достаточно научиться пользоваться исключениями. Но как быть с ошибками, которые могут возникнуть в обработчиках Express?
    
    Есть хорошая новость: Express из коробки умеет обрабатывать исключения, которые возникают в обработчиках. Если бросить ошибку в синхронном обработчике, то Express её автоматом поймает и отправит клиенту код `500` (Internal Server Error) и включит в ответ весь стек-трейс.
    
    Это неплохой вариант для начала, но клиенту обычно не нужен стек-трейс вызова функций, ему достаточно кода ошибки и понятного описания. Исправить стандартный обработчик ошибок легко, достаточно зарегистрировать собственный. В нём можно сделать более продвинутую обработку ошибок.
    
    Такие обработчики обычно называют `ExceptionFilter`. При переходе на другую технологию (например, ASP .NET Core) вы столкнётесь с аналогичным наименованием. Попробуем реализовать такой обработчик исключений в нашем проекте.
    
    Начнём с проектирования интерфейса `ExceptionFilter`. Предусмотрим в нём единственный метод `catch`. Сигнатура метода полностью соответствует сигнатуре обработчика ошибок в Express. Первым аргументом он принимает объект ошибки, а в остальных традиционный набор: объект запроса, объект ответа и функцию для передачи следующему обработчику.

---
+++ src/core/expception-filters/exception-filter.interface.ts
@@ -0,0 +1,5 @@
+import { NextFunction, Request, Response } from 'express';
+
+export interface ExceptionFilterInterface {
+  catch(error: Error, req: Request, res: Response, next:NextFunction): void;
+}
