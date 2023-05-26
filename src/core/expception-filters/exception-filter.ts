f77f2d40eb146e465c0ee9610f361f8391ffadda
Igor Antonov <antonov.igor.khv@gmail.com>
2023-05-26 16:33:14 +0300

    6.17. Добавит класс `ExceptionFilter`.
    Теперь добавим имплементацию `ExceptionFilterInterface`. Для этого создадим класс `ExceptionFilter`.
    
    В классе нам потребуется логер. Поэтому внедрим его сразу в виде
    зависимости через конструктор. В конструкторе добавим вывод информации о регистрации `ExceptionFilter`.
    
    Затем имплементируем метод `catch`. Пока сделаем самый простой вариант. Сделаем запись в лог сообщения ошибки (`error.message`) и отправим клиенту корректный ответ. Помните, даже если в сервисе произошла ошибка, следует отправить клиенту корректный код. В случае неизвестной ошибки таким кодом станет `500`.
    
    Помимо кода с ошибкой отправим объект с ошибкой. Здесь можно пойти дальше и внедрить отправку дополнительных констант для автоматической обработки ошибок клиентом, но пока не будем переусложнять.

---
+++ src/core/expception-filters/exception-filter.ts
@@ -0,0 +1,22 @@
+import { Request, Response, NextFunction } from 'express';
+import { StatusCodes } from 'http-status-codes';
+import { inject, injectable } from 'inversify';
+import { ExceptionFilterInterface } from './exception-filter.interface.js';
+import { LoggerInterface } from '../logger/logger.interface.js';
+import { AppComponent } from '../../types/app-component.enum.js';
+
+@injectable()
+export default class ExceptionFilter implements ExceptionFilterInterface {
+  constructor(
+    @inject(AppComponent.LoggerInterface) private readonly logger: LoggerInterface
+  ) {
+    this.logger.info('Register ExceptionFilter');
+  }
+
+  public catch(error: Error, _req: Request, res: Response, _next: NextFunction): void {
+    this.logger.error(error.message);
+    res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json({error: error.message});
+  }
+}
