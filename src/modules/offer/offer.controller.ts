88f921f577e71ead20c199c70e5826b0b6445117
Igor Antonov <antonov.igor.khv@gmail.com>
2023-05-30 11:39:00 +0300

    7.2. Добавит обработчик `show` (`OfferController`). WIP
    Внедрим в конструктор контроллера `OfferController` зависимость
    `offerService`. Сервис потребуется для работы с объявлениями в базе.
    Затем имплементируем обработчик `show`.
    
    На этот раз данные для поиска объявления (идентификатор) клиент будет
    передавать через параметры запроса. Все параметры, переданные клиентом,
    доступны через объект `params`. Типизируем его. Для этого опишем тип
    `ParamsOfferDetails`.
    
    Получив идентификатор объявления от клиента, воспользуемся сервисом и
    найдём объявление. Если объявление отсутствует — бросим ошибку `404`.
    В противном случае отдадим клиенту найденное объявление.
    
    Для тестирования добавим запрос в `queries.http`. Попробуйте выполнить
    его и убедиться, что сервис отдаёт полную информацию об объявлении.
    
    WIP: Контроллер `OfferController` в разработке.

--- src/modules/offer/offer.controller.ts
+++ src/modules/offer/offer.controller.ts
@@ -1,16 +1,23 @@
 import { StatusCodes } from 'http-status-codes';
 import { inject, injectable } from 'inversify';
 import { Request, Response } from 'express';
+import { ParamsDictionary } from 'express-serve-static-core';
 import { Controller } from '../../core/controller/controller.abstract.js';
 import { LoggerInterface } from '../../core/logger/logger.interface.js';
 import { AppComponent } from '../../types/app-component.enum.js';
 import { HttpMethod } from '../../types/http-method.enum.js';
 import HttpError from '../../core/errors/http-error.js';
+import { OfferServiceInterface } from './offer-service.interface.js';
+
+type ParamsOfferDetails = {
+  offerId: string;
+} | ParamsDictionary
 
 @injectable()
 export default class OfferController extends Controller {
   constructor(
     @inject(AppComponent.LoggerInterface) logger: LoggerInterface,
+    @inject(AppComponent.OfferServiceInterface) private readonly offerService: OfferServiceInterface,
   ) {
     super(logger);
 
@@ -18,11 +25,21 @@ export default class OfferController extends Controller {
     this.addRoute({path: '/:offerId', method: HttpMethod.Get, handler: this.show});
   }
 
-  public async show(_req: Request, _res: Response): Promise<void> {
-    throw new HttpError(
-      StatusCodes.NOT_IMPLEMENTED,
-      'Not implemented',
-      'OfferController'
-    );
+  public async show(
+    { params }: Request<ParamsOfferDetails>,
+    res: Response
+  ): Promise<void> {
+    const { offerId } = params;
+    const offer = await this.offerService.findById(offerId);
+
+    if (!offer) {
+      throw new HttpError(
+        StatusCodes.NOT_FOUND,
+        `Offer with id ${offerId} not found.`,
+        'OfferController'
+      );
+    }
+
+    this.ok(res, offer);
   }
 }
