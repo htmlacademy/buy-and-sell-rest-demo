45f227b684c3e5c892270d75a0e6d0c5b832d7ca
Igor Antonov <antonov.igor.khv@gmail.com>
2023-06-05 16:20:54 +0300

    8.4. Добавит метод `verifyUser` в `UserService`.
    Теперь нужно решить откуда вызывать метод для проверки пароля. Напрямую обратиться к модели нельзя (мы разделяем приложение на слои), поэтому
    расширим интерфейс `UserServiceInterface` и добавим метод `verifyUser`.
    
    Он принимает данные пользователя, необходимые для авторизации
    (`email`, `password`). Эти поля мы уже зафиксировали в DTO
    `LoginUserDto`.
    
    Алгоритм проверки такой: находим пользователя по email. Если не нашли, значит нет такого пользователя, вернём `null`. Если нашли, вызовем метод модели `verifyPassword`. Передадим пароль из `LoginUserDto` и соль.
    
    Если проверка пройдена — вернём `null`.

--- src/modules/user/user-service.interface.ts
+++ src/modules/user/user-service.interface.ts
@@ -2,10 +2,12 @@ import { DocumentType } from '@typegoose/typegoose';
 import CreateUserDto from './dto/create-user.dto.js';
 import { UserEntity } from './user.entity.js';
 import UpdateUserDto from './dto/update-user.dto.js';
+import LoginUserDto from './dto/login-user.dto.js';
 
 export interface UserServiceInterface {
   create(dto: CreateUserDto, salt: string): Promise<DocumentType<UserEntity>>;
   findByEmail(email: string): Promise<DocumentType<UserEntity> | null>;
   findOrCreate(dto: CreateUserDto, salt: string): Promise<DocumentType<UserEntity>>;
   updateById(userId: string, dto: UpdateUserDto): Promise<DocumentType<UserEntity> | null>;
+  verifyUser(dto: LoginUserDto, salt: string): Promise<DocumentType<UserEntity> | null>;
 }
--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -6,6 +6,7 @@ import { inject, injectable } from 'inversify';
 import { AppComponent } from '../../types/app-component.enum.js';
 import { LoggerInterface } from '../../core/logger/logger.interface.js';
 import UpdateUserDto from './dto/update-user.dto.js';
+import LoginUserDto from './dto/login-user.dto.js';
 
 @injectable()
 export default class UserService implements UserServiceInterface {
@@ -42,5 +43,19 @@ export default class UserService implements UserServiceInterface {
     return this.userModel
       .findByIdAndUpdate(userId, dto, {new: true})
       .exec();
+  }
+
+  public async verifyUser(dto: LoginUserDto, salt: string): Promise<DocumentType<UserEntity> | null> {
+    const user = await this.findByEmail(dto.email);
+
+    if (! user) {
+      return null;
+    }
+
+    if (user.verifyPassword(dto.password, salt)) {
+      return user;
+    }
+
+    return null;
   }
 }
