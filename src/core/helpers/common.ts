9b84c3e0705b78674e6569dc89a0d7a3e119f538
Igor Antonov <antonov.igor.khv@gmail.com>
2023-06-05 16:28:19 +0300

    8.5. Добавит функцию `createJWT`.
    Создавать новый токен мы будем в контроллере `UserController`. Нам для этого достаточно воспользоваться классом `SignJWT` из пакета `jose`.
    Как говорили выше, пакет `jose` предоставляет удобный API. В принципе, можем напрямую вызывать нужные методы прямо в `UserController`, но лучше сделаем небольшую функцию-обёртку — `createJWT`.
    
    Функция определяет несколько параметров:
    
    1. Алгоритм шифрования.
    2. Секрет
    3. Полезная нагрузка (payload). Данные, которые хотим поместить в токен.
    
    В качестве значения для первого аргумента мы будем использовать
    `HS256`. Сразу создадим для него отдельную константу в `user.constant.ts`.
    
    Внутри `createJWT` создадим экземпляр класса `SignJWT`. В конструктор ему передадим полезную нагрузку. Под полезной нагрузкой подразумеваются
    любые данные, которые мы хотим отправить клиенту вместе с токеном. Как правило, такими данными являются `email` и `id` пользователя. Понять какие данные следует поместить в токен можно на основании сценариев,
    в которых мы хотим их использовать. Нам точно потребуется идентификатор,
    чтобы подставлять его при создании каких-либо сущностей.
    
    Затем мы вызываем цепочку методов. Каждый метод решает маленькую задачу в формировании токена. `setProtectedHeader` подготовит заголовок в соответствии с алгоритмом шифрования, `setIssuedAt` заполнит поле `iat` (дата выпуска токена), `setExpirationTime` (установит дату истечения срока действия токена).
    
    В конце цепочки вызываем метод `sign`. Он сформирует сам токен.
    Аргументами передадим `SecretKey`. Получить секретный ключ поможет метод `createSecretkey` из встроенного модуля `crypto`.

--- src/core/helpers/common.ts
+++ src/core/helpers/common.ts
@@ -1,5 +1,6 @@
 import * as crypto from 'node:crypto';
 import { plainToInstance, ClassConstructor } from 'class-transformer';
+import * as jose from 'jose';
 
 export function getErrorMessage(error: unknown): string {
   return error instanceof Error ? error.message : '';
@@ -18,4 +19,12 @@ export function createErrorObject(message: string) {
   return {
     error: message,
   };
+}
+
+export async function createJWT(algorithm: string, jwtSecret: string, payload: object): Promise<string> {
+  return new jose.SignJWT({ ...payload })
+    .setProtectedHeader({ alg: algorithm })
+    .setIssuedAt()
+    .setExpirationTime('2d')
+    .sign(crypto.createSecretKey(jwtSecret, 'utf-8'));
 }
