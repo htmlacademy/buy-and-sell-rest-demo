eca1dfa13798e84492815e3f99feb563dad3bfe7
Igor Antonov <antonov.igor.khv@gmail.com>
2023-06-05 16:50:18 +0300

    8.7. Добавит `AuthenticateMiddleware`.
    Токены формируются. Теперь надо как-то извлечь информацию из `payload` токена. Это можно делать на уровне обработчика маршрута, но в этом случае, один и тот же код придётся дублировать в разных местах
    приложения. Чтобы этого избежать, создадим `AuthenticateMiddleware`,
    которую зарегистрируем глобально. Если к запросу будет приложен токен, то middleware сможет извлечь из него информацию и добавить к запросу.
    
    Начнём с извлечения информации. Сначала проверим наличие заголовка `authorization`. Если он есть, значит продолжаем работать. Затем
    извлечём сам токен. После этого проверим с помощью `jwtVerify`, Если всё ок, то сохраним в свойство запроса `user`. Таким образом, когда
    запрос долетит до обработчика маршрута, у нас будет вся необходимая информация.
    
    Однако здесь появляется проблема с типизацией. Так просто TypeScript не позволит модифицировать объект запроса. Нам нужно принудительно расширить интерфейс. Для этого создадим файл `custom.d.ts`, где опишем изменения в интерфейсе. Чтобы наши типы были найдены при запуске, добавим в `nodemon` аргумент `--files` для `ts-node`.
    
    Файл custom.d.ts в TypeScript представляет собой файл объявления типов (type declaration file), который позволяет расширять или изменять типы для существующих или сторонних модулей и библиотек. Он используется для описания пользовательских типов, которые не являются частью изначальной библиотеки или модуля.

---
+++ src/core/middleware/authenticate.middleware.ts
@@ -0,0 +1,36 @@
+import { NextFunction, Request, Response } from 'express';
+import { jwtVerify } from 'jose';
+import { StatusCodes } from 'http-status-codes';
+import { createSecretKey } from 'node:crypto';
+import { MiddlewareInterface } from './middleware.interface.js';
+import HttpError from '../errors/http-error.js';
+
+export class AuthenticateMiddleware implements MiddlewareInterface {
+  constructor(private readonly jwtSecret: string) {}
+
+  public async execute(req: Request, _res: Response, next: NextFunction): Promise<void> {
+    const authorizationHeader = req.headers?.authorization?.split(' ');
+    if (!authorizationHeader) {
+      return next();
+    }
+
+    const [, token] = authorizationHeader;
+
+    try {
+      const { payload } = await jwtVerify(
+        token,
+        createSecretKey(this.jwtSecret, 'utf-8')
+      );
+
+      req.user = { email: payload.email as string, id: payload.id as string };
+      return next();
+    } catch {
+
+      return next(new HttpError(
+        StatusCodes.UNAUTHORIZED,
+        'Invalid token',
+        'AuthenticateMiddleware')
+      );
+    }
+  }
+}
