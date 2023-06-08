fbc8093de53058c9a643f0666533db3897829427
Igor Antonov <antonov.igor.khv@gmail.com>
2023-06-08 17:02:40 +0300

    9.10. Добавит в контроллер метод `addStaticPath`.
    Добавим в базовый контроллер метод `addStaticPath`. Внутри него определим полный путь к директориям с ресурсами и воспользуемся функцией для трансформации из прошлого шага. Затем вызовем этот метод в `send`.
    Через него происходит любая отправка данных из контроллера.

--- src/core/controller/controller.abstract.ts
+++ src/core/controller/controller.abstract.ts
@@ -7,6 +7,9 @@ import { LoggerInterface } from '../logger/logger.interface.js';
 import { RouteInterface } from '../../types/route.interface.js';
 import { ConfigInterface } from '../config/config.interface.js';
 import { RestSchema } from '../config/rest.schema.js';
+import { UnknownRecord } from '../../types/unknown-record.type.js';
+import { getFullServerPath, transformObject } from '../helpers/index.js';
+import { STATIC_RESOURCE_FIELDS } from '../../app/rest.constant.js';
 
 @injectable()
 export abstract class Controller implements ControllerInterface {
@@ -34,7 +37,18 @@ export abstract class Controller implements ControllerInterface {
     this.logger.info(`Route registered: ${route.method.toUpperCase()} ${route.path}`);
   }
 
+  protected addStaticPath(data: UnknownRecord): void {
+    const fullServerPath = getFullServerPath(this.configService.get('HOST'), this.configService.get('PORT'));
+    transformObject(
+      STATIC_RESOURCE_FIELDS,
+      `${fullServerPath}/${this.configService.get('STATIC_DIRECTORY_PATH')}`,
+      `${fullServerPath}/${this.configService.get('UPLOAD_DIRECTORY')}`,
+      data
+    );
+  }
+
   public send<T>(res: Response, statusCode: number, data: T): void {
+    this.addStaticPath(data as UnknownRecord);
     res
       .type('application/json')
       .status(statusCode)
