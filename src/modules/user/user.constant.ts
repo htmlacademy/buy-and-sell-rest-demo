9d24aac016cd21010817440074a9fe753940f1e7
Igor Antonov <antonov.igor.khv@gmail.com>
2023-06-08 16:30:11 +0300

    9.5. Установит значение по умолчанию для поля `avatarPath`.
    Для загрузки изображения предусмотрен отдельный маршрут. Изображение аватара добавляется после создание пользователя. Поэтому удалим из
    `CreateUserDto` поле `avatarPath`.
    
    Затем немного модифицируем сервис `UserService`. Добавим в качестве аватара файл по умолчанию. Имя файла с аватаром по умолчанию сохраним в константе `DEFAULT_AVATAR_FILE_NAME`.

--- src/modules/user/user.constant.ts
+++ src/modules/user/user.constant.ts
@@ -1,1 +1,2 @@
 export const JWT_ALGORITHM = 'HS256';
+export const DEFAULT_AVATAR_FILE_NAME = 'default-avatar.jpg';
--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -7,6 +7,7 @@ import { AppComponent } from '../../types/app-component.enum.js';
 import { LoggerInterface } from '../../core/logger/logger.interface.js';
 import UpdateUserDto from './dto/update-user.dto.js';
 import LoginUserDto from './dto/login-user.dto.js';
+import { DEFAULT_AVATAR_FILE_NAME } from './user.constant.js';
 
 @injectable()
 export default class UserService implements UserServiceInterface {
@@ -16,7 +17,7 @@ export default class UserService implements UserServiceInterface {
   ) {}
 
   public async create(dto: CreateUserDto, salt: string): Promise<DocumentType<UserEntity>> {
-    const user = new UserEntity(dto);
+    const user = new UserEntity({...dto, avatarPath: DEFAULT_AVATAR_FILE_NAME});
     user.setPassword(dto.password, salt);
 
     const result = await this.userModel.create(user);
