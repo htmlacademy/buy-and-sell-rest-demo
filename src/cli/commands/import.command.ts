4c805bd6a4c52298e233f3dcf78c3d299c6cff8b
Igor Antonov <antonov.igor.khv@gmail.com>
2023-09-19 17:42:26 +0300

    1.13. Добавит в команду `--import` чтение файла.
    Вернёмся к команде `ImportCommand` и добавим код для чтения из файла. В методе `execute` добавим инициализацию класса `TSVFileReader`.
    
    Результат временно выведем в консоль.

--- src/cli/commands/import.command.ts
+++ src/cli/commands/import.command.ts
@@ -1,4 +1,5 @@
 import { Command } from './command.interface.js';
+import { TSVFileReader } from '../../shared/libs/file-reader/index.js';
 
 export class ImportCommand implements Command {
   public getName(): string {
@@ -6,6 +7,20 @@ export class ImportCommand implements Command {
   }
 
   public execute(...parameters: string[]): void {
-    // Чтение файла
+    const [filename] = parameters;
+    const fileReader = new TSVFileReader(filename.trim());
+
+    try {
+      fileReader.read();
+      console.log(fileReader.toArray());
+    } catch (err) {
+
+      if (!(err instanceof Error)) {
+        throw err;
+      }
+
+      console.error(`Can't import data from file: ${filename}`);
+      console.error(`Details: ${err.message}`);
+    }
   }
 }
