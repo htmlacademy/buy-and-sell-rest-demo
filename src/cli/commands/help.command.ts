9b917a1af27f516d531b9ee6700e35c8d67522d1
Igor Antonov <antonov.igor.khv@gmail.com>
2023-09-19 12:26:24 +0300

    1.3. Добавит команду `HelpCommand`.
    Создадим ещё одну команду — `--help`. Она возвращает информацию о доступных командах. Эта команда используется в качестве команды по умолчанию. То есть когда пользователь ничего не ввёл.

---
+++ src/cli/commands/help.command.ts
@@ -0,0 +1,20 @@
+import { Command } from './command.interface.js';
+
+export class HelpCommand implements Command {
+  public getName(): string {
+    return '--help';
+  }
+
+  public async execute(..._parameters: string[]): Promise<void> {
+    console.info(`
+        Программа для подготовки данных для REST API сервера.
+        Пример:
+            cli.js --<command> [--arguments]
+        Команды:
+            --version:                   # выводит номер версии
+            --help:                      # печатает этот текст
+            --import <path>:             # импортирует данные из TSV
+            --generate <n> <path> <url>  # генерирует произвольное количество тестовых данных
+    `);
+  }
+}
