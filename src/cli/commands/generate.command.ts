07aa9f6a9296b01af69cff326e4f4ef2606b6cf2
Igor Antonov <antonov.igor.khv@gmail.com>
2023-09-22 14:56:05 +0300

    2.10. Добавит запрос для получения данных. WIP
    Подключим в модуль команды `GenerateCommand` пакет `got`. Затем сделаем запрос к сервису JSON-Server.
    
    Для выполнения http-запросов `got` предоставляет набор
    знакомых методов. Название метода соответствует типу
    запроса: `get`, `post`, `put`, `delete` и так далее.
    
    Добавим вызов метода `get` для отправки GET-запроса сервису. Результатом выполнения станет объект запроса, у которого доступен метод `json`. Метод трансформирует `json` в объект.
    
    WIP: Линтер ругается на неиспользуемые переменные.

--- src/cli/commands/generate.command.ts
+++ src/cli/commands/generate.command.ts
@@ -1,15 +1,36 @@
+import got from 'got';
 import { Command } from './command.interface.js';
+import { MockServerData } from '../../shared/types/index.js';
 
 export class GenerateCommand implements Command {
+  private initialData: MockServerData;
+
+  private async load(url: string) {
+    try {
+      this.initialData = await got.get(url).json();
+    } catch {
+      throw new Error(`Can't load data from ${url}`);
+    }
+  }
+
   public getName(): string {
     return '--generate';
   }
 
-  public execute(...parameters: string[]): void {
+  public async execute(...parameters: string[]): Promise<void> {
     const [count, filepath, url] = parameters;
     const offerCount = Number.parseInt(count, 10);
 
-    // Код для получения данных с сервера.
-    // Формирование объявлений.
+    try {
+      await this.load(url);
+    } catch (error: unknown) {
+      console.error('Can\'t generate data');
+
+      if (error instanceof Error) {
+        console.error(error.message);
+      }
+    }
+
+
   }
 }
