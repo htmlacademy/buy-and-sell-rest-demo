63847a9723175acee2a8ec766dac77cd96e646aa
Igor Antonov <antonov.igor.khv@gmail.com>
2023-09-28 17:49:33 +0300

    3.18. Расширит тип `RestSchema`.
    Пока мы используем только одну переменную окружения, но нам вскоре потребуется больше. Потренируем подключать новые переменные окружения.
    
    Расширим определение типа `RestSchema`. Добавим `SALT` и `DB_HOST`. Напомним, имена соответствуют одноимённым переменным окружения.
    
    Через `SALT` будем передавать значение «соли» — случайный набор символов для хеширования пароля.
    
    `DB_HOST` будет содержать IP-адрес сервера баз данных. Что такое и как использовать базы данных узнаем позже, а пока только подготовим переменную.
    
    Обратите внимание на описание `SALT` в схеме. Значение «соли» обязательно должно задаваться снаружи. Поэтому в качестве значения указываем `null`. Если забыть установить переменную в `.env`, то
    `convict` выбросит исключение (это то что нам нужно).
    
    В переменной `DB_HOST` мы используем внешний форматер. Форматер (`format`) — функция, которая знает как проверить то или иное значение. В качестве значения мы указываем `ipaddress`.
    
    `ipaddress` — это внешний форматер. Мы подключили его из пакета `convict-format-with-validator`. В нём есть и другие форматеры.
    
    Повторимся, форматер — это функция. Вы можете определять собственные форматеры.

--- src/shared/libs/config/rest.schema.ts
+++ src/shared/libs/config/rest.schema.ts
@@ -1,7 +1,12 @@
 import convict from 'convict';
+import validator from 'convict-format-with-validator';
+
+convict.addFormats(validator);
 
 export type RestSchema = {
   PORT: number;
+  SALT: string;
+  DB_HOST: string;
 }
 
 export const configRestSchema = convict<RestSchema>({
@@ -10,5 +15,17 @@ export const configRestSchema = convict<RestSchema>({
     format: 'port',
     env: 'PORT',
     default: 4000
+  },
+  SALT: {
+    doc: 'Salt for password hash',
+    format: String,
+    env: 'SALT',
+    default: null
+  },
+  DB_HOST: {
+    doc: 'IP address of the database server (MongoDB)',
+    format: 'ipaddress',
+    env: 'DB_HOST',
+    default: '127.0.0.1'
   },
 });
