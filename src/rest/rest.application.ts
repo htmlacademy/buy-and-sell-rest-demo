adcbc94a0b8924dc8a4e1437db597f286f34c3be
Igor Antonov <antonov.igor.khv@gmail.com>
2023-09-28 17:34:43 +0300

    3.13. Добавит `Config` в `RestApplication`.
    В `main.ts` добавим создание экземпляра класса `RestConfig`. При создании экземпляра передадим в конструктор настроенный `logger`.
    
    Настроенный экземпляр `config` передадим в конструктор `RestApplication` вторым аргументом.
    
    Внутри класса `RestApplication` добавим один параметр для конструктора (для приёма экземпляра `RestConfig`).
    
    В методе `init` добавим вывод значения переменной `PORT`. Пока только для проверки.
    
    Протестируйте пример на данном шаге. Создайте в корне проекта файл `.env` и определите в нём переменную `PORT=5000`. Затем запустите
    приложение. Значение переменной должно быть выведено в терминал.

--- src/rest/rest.application.ts
+++ src/rest/rest.application.ts
@@ -1,11 +1,14 @@
 import { Logger } from '../shared/libs/logger/index.js';
+import { Config } from '../shared/libs/config/index.js';
 
 export class RestApplication {
   constructor(
-    private readonly logger: Logger
+    private readonly logger: Logger,
+    private readonly config: Config,
   ) {}
 
   public async init() {
     this.logger.info('Application initialization');
+    this.logger.info(`Get value from env $PORT: ${this.config.get('PORT')}`);
   }
 }
