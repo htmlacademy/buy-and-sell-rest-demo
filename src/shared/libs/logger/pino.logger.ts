bf845be38c2415a26102dd52d079944be0fe59cf
Igor Antonov <antonov.igor.khv@gmail.com>
2023-09-28 18:01:11 +0300

    3.22. Добавит декоратор `injectable` в `PinoLogger`. WIP
    Теперь необходимо воспользоваться декоратором `injectable` в классах, которые могут внедряться в виде зависимости.
    
    Для этого перед объявлением класса используем декоратор `@injectable`. Ему не нужно передавать дополнительных аргументов.
    
    Мы помечаем класс `PinoLogger`, что он может использоваться в качестве зависимости. `injectable` помещает метаданные о классе в контейнер.
    
    WIP: Приложение не запускается.

--- src/shared/libs/logger/pino.logger.ts
+++ src/shared/libs/logger/pino.logger.ts
@@ -1,8 +1,10 @@
 import { Logger as PinoInstance, pino, transport } from 'pino';
+import { injectable } from 'inversify';
+import { resolve } from 'node:path';
 import { Logger } from './logger.interface.js';
 import { getCurrentModuleDirectoryPath } from '../../helpers/index.js';
-import { resolve } from 'node:path';
 
+@injectable()
 export class PinoLogger implements Logger {
   private readonly logger: PinoInstance;
 
