32c9487fe5741212274f1743169c683c7fe1dbad
Igor Antonov <antonov.igor.khv@gmail.com>
2023-09-28 17:59:30 +0300

    3.21. Добавит контейнер с зависимостями. WIP
    Перейдём к файлу `main.rest.ts` (точка входа). В нём мы создаём экземпляры всех необходимых классов и зависимостей для них. Попробуем сделать то же
    самое с помощью возможностей `inversify`.
    
    Первым делом нам потребуется создать контейнер. В контексте библиотеки `inversify`, контейнер — это место, где происходит настройка и привязка
    зависимостей.
    
    При создании контейнера мы объясняем `inversify` какую реализацию следует использовать для определённого интерфейса.
    
    Например, интерфейсу `Logger` соответствует реализация в виде класса `PinoLogger`. А интерфейсу `Config` реализация `RestConfig` и так далее.
    
    За счёт построения контейнера, в любом другом классе мы сможем получить нужную реализацию. При этом `inversify` возьмёт на себя создание необходимых экземпляров.
    
    WIP: Приложение не запускается.

--- src/main.rest.ts
+++ src/main.rest.ts
@@ -1,12 +1,18 @@
-import { PinoLogger } from './shared/libs/logger/index.js';
+import 'reflect-metadata';
+import { Container } from 'inversify';
+import { Logger, PinoLogger } from './shared/libs/logger/index.js';
 import { RestApplication } from './rest/index.js';
-import { RestConfig } from './shared/libs/config/index.js';
+import { Config, RestConfig, RestSchema } from './shared/libs/config/index.js';
+import { Component } from './shared/types/index.js';
+
 
 async function bootstrap() {
-  const logger = new PinoLogger();
-  const config = new RestConfig(logger);
+  const container = new Container();
+  container.bind<RestApplication>(Component.RestApplication).to(RestApplication);
+  container.bind<Logger>(Component.Logger).to(PinoLogger);
+  container.bind<Config<RestSchema>>(Component.Config).to(RestConfig);
 
-  const application = new RestApplication(logger, config);
+  const application = container.get<RestApplication>(Component.RestApplication);
   await application.init();
 }
 
