ac70547bfd54947a6cdb501c2f268f24d850a7fd
Igor Antonov <antonov.igor.khv@gmail.com>
2023-09-28 17:30:17 +0300

    3.12. Добавит поддержку `dotenv` в `RestConfig`.
    Обновим класс `RestConfig`. Вместо прямого обращения к `process.env` считаем данные из `.env` файла. Для этого достаточно вызвать функцию `config` пакета `dontenv`.
    
    После этого необходимо проверить результат разбора файла. Для этого проверим значение свойства `error`. Если оно `true`, значит возникла ошибка и дальше продолжать смысла нет.
    
    Можно просто прервать работу всего приложения (завершить процесс), а можно бросить ошибку. Второй вариант более предпочтительней.
    
    Обратите внимание, мы также добавили поддержку логирования в наш класс. Настроенный логер будем передавать через конструктор.

--- src/shared/libs/config/rest.config.ts
+++ src/shared/libs/config/rest.config.ts
@@ -1,9 +1,22 @@
+import { DotenvParseOutput, config } from 'dotenv';
 import { Config } from './config.interface.js';
+import { Logger } from '../logger/index.js';
 
 export class RestConfig implements Config {
+  private readonly config: NodeJS.ProcessEnv;
+
   constructor(
-    private readonly config: NodeJS.ProcessEnv
-  ) {}
+    private readonly logger: Logger
+  ) {
+    const parsedOutput = config();
+
+    if (parsedOutput.error) {
+      throw new Error('Can\'t read .env file. Perhaps the file does not exists.');
+    }
+
+    this.config = <DotenvParseOutput>parsedOutput.parsed;
+    this.logger.info('.env file found and successfully parsed!');
+  }
 
   public get(key: string): string | undefined {
     return this.config[key];
