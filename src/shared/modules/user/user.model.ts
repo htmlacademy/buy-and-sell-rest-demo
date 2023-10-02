4826171308b107cd6d0c46c4e9b94b2fa6bea95c
Igor Antonov <antonov.igor.khv@gmail.com>
2023-10-02 16:01:25 +0300

    4.13. Добавит уникальность для поля email. WIP
    Некоторые из задач мы можем делегировать базе данных. Рассмотрим на примере хранения пользователей. В коллекции не может быть двух пользователей с одинаковым email. Это вполне логично, ведь email
    обычно используется в качестве логина.
    
    А как можно реализовать такую проверку? Самый простой вариант: перед созданием очередного документа выполнять проверку на наличие документа
    с определённым email. Если его нет — создаём запись, если есть — бросаем ошибку.
    
    Вариант рабочий, но эту работу можно делегировать базе данных. При определении схемы можно явно сказать, что определённое поле содержит
    только уникальные значения. Для этого следует воспользоваться
    расширенной записью при определении поля. Рассмотрим на примере `email`.
    
    Вместо типа `String` укажем в качестве значения для поля объект. В объекте опишем поле `type` и в нём укажем тип, вернём `String`. Затем
    добавим поле `unique` со значением `true`. С помощью него мы явно указали, что поле `email` содержит уникальные значения.
    
    В качестве теста попробуйте создать несколько записей с одинаковым email. Будет создана только одна, при создании следующей возникнет
    ошибка.
    
    WIP: Начальное состояние. Модель не используется по назначению.

--- src/shared/modules/user/user.model.ts
+++ src/shared/modules/user/user.model.ts
@@ -7,7 +7,10 @@ export interface UserDocument extends User, Document {
 }
 
 const userSchema = new Schema({
-  email: String,
+  email: {
+    type: String,
+    unique: true,
+  },
   avatarPath: String,
   firstname: String,
   lastname: String,
