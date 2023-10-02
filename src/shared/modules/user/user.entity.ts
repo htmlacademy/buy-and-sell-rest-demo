0721c14630e6900d05a6b29d72be21f0131cadf9
Igor Antonov <antonov.igor.khv@gmail.com>
2023-10-02 17:10:48 +0300

    4.18. Добавит поля `createdAt`, `updatedAt`. WIP
    Во время тестирования Mongoose мы разбирали пример добавления служебных полей `createdAt` и `updatedAt`. Напомним, в них учитывается дата и время создания, а также обновления документа. В прошлый раз для этого пришлось расширить интерфейс `UserDocument`, добавив дополнительные
    поля, а также активировав настройку при создании схемы.
    
    В `typegoose` эта задача решается в стиле ООП. Чтобы добавить `createdAt` и `updatedAt` можно воспользоваться наследованием. Родителем класса `User` должен стать класс `TimeStamps` из пакета `typegoose`.
    
    `TimeStamps` — это абстрактный класс, который определяет два
    перечисленных поля.

--- src/shared/modules/user/user.entity.ts
+++ src/shared/modules/user/user.entity.ts
@@ -1,7 +1,7 @@
-import { getModelForClass, prop } from '@typegoose/typegoose';
+import { defaultClasses, getModelForClass, prop } from '@typegoose/typegoose';
 import { User } from '../../types/index.js';
 
-export class UserEntity implements User {
+export class UserEntity extends defaultClasses.TimeStamps implements User {
   @prop({ unique: true, required: true })
   public email: string;
 
