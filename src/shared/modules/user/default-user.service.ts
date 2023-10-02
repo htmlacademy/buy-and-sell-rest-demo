422de74fa0e6daffafb89c1da6ecaaf5159625e8
Igor Antonov <antonov.igor.khv@gmail.com>
2023-10-02 18:14:56 +0300

    4.27. Добавит имплементацию методов UserService.
    Последнее, что осталось сделать — имплементировать методы `findByEmail`
    и `findOrCreate` в классе `UserService`. По сравнению с методом
    `create` они более простые.
    
    Для поиска документа следует воспользоваться методом `findOne`
    (если результатом должен стать один документ). Поиск выполняем по
    `email`. Следует помнить, что нужный документ может отсутствовать в
    базе. Поэтому мы предусмотрели в качестве результата выполнения метода
    `findByEmail` — `null`.
    
    Метод `findOrCreate` совмещает работу двух методов. Сначала в нём
    выполняется поиск по `email`, а затем проверка результата. Если
    документ удалось найти, он станет результатом выполнения метода
    `findOrCreate`. Если нет, эстафетная палочка передаётся методу
    `create`.

--- src/shared/modules/user/default-user.service.ts
+++ src/shared/modules/user/default-user.service.ts
@@ -22,4 +22,18 @@ export class DefaultUserService implements UserService {
 
     return result;
   }
+
+  public async findByEmail(email: string): Promise<DocumentType<UserEntity> | null> {
+    return this.userModel.findOne({email});
+  }
+
+  public async findOrCreate(dto: CreateUserDto, salt: string): Promise<DocumentType<UserEntity>> {
+    const existedUser = await this.findByEmail(dto.email);
+
+    if (existedUser) {
+      return existedUser;
+    }
+
+    return this.create(dto, salt);
+  }
 }
