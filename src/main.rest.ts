f522e20254d2137222dd315cc7e4858d3686445c
Igor Antonov <antonov.igor.khv@gmail.com>
2023-10-02 14:49:33 +0300

    4.8. Добавит привязку для `DatabaseClient`.
    Добавим в контейнер новую привязку. На этот раз для `DatabaseClient`. Свяжем с реализацией `MongoDatabaseClient`. Не забудем указать `inSingletonScope`. Достаточно одного экземпляра.

--- src/main.rest.ts
+++ src/main.rest.ts
@@ -4,6 +4,7 @@ import { Logger, PinoLogger } from './shared/libs/logger/index.js';
 import { RestApplication } from './rest/index.js';
 import { Config, RestConfig, RestSchema } from './shared/libs/config/index.js';
 import { Component } from './shared/types/index.js';
+import { DatabaseClient, MongoDatabaseClient } from './shared/libs/database-client/index.js';
 
 
 async function bootstrap() {
@@ -11,6 +12,7 @@ async function bootstrap() {
   container.bind<RestApplication>(Component.RestApplication).to(RestApplication).inSingletonScope();
   container.bind<Logger>(Component.Logger).to(PinoLogger).inSingletonScope();
   container.bind<Config<RestSchema>>(Component.Config).to(RestConfig).inSingletonScope();
+  container.bind<DatabaseClient>(Component.DatabaseClient).to(MongoDatabaseClient).inSingletonScope();
 
   const application = container.get<RestApplication>(Component.RestApplication);
   await application.init();
