2a984d22dda2c3563cd1369418dd0b597413be5d
Igor Antonov <antonov.igor.khv@gmail.com>
2023-10-02 17:46:24 +0300

    4.24. Разобьёт общий контейнер. WIP
    У библиотеки `inversify` есть метод `merge()`, который предназначен для разделения одного большого контейнера зависимостей на несколько. Давайте посмотрим как это можно сделать в нашем проекте.
    
    Сейчас в контейнере всего четыре зависимости: `Application`, `Logger`, `Config` и `DatabaseClient`. Однако дальнейшая работа предполагает управление большим количеством различных сервисов. Точка входа `main.rest.ts` может превратиться в очень длинный листинг подключенных компонентов.
    
    Мы можем разбить общий контейнер зависимостей на нескольно контейнеров, описывающих свою предметную область. Метод `merge()` принимает два и более контейнеров зависимостей и собирает их в один общий.
    
    Вместе с этим обернем вызовы функций в точке входа (`main.rest.ts`) в функцию `bootstrap()`. Такой подход повышает наглядность и функциональность кода проекта.
    
    WIP. Приложение не запускается. В метод `merge()` необходимо добавить как минимум еще одну зависимость. Поправим это в следующем коммите.

--- src/main.rest.ts
+++ src/main.rest.ts
@@ -1,20 +1,14 @@
 import 'reflect-metadata';
 import { Container } from 'inversify';
-import { Logger, PinoLogger } from './shared/libs/logger/index.js';
 import { RestApplication } from './rest/index.js';
-import { Config, RestConfig, RestSchema } from './shared/libs/config/index.js';
 import { Component } from './shared/types/index.js';
-import { DatabaseClient, MongoDatabaseClient } from './shared/libs/database-client/index.js';
+import { createRestApplicationContainer } from './rest/rest.container.js';
 
 
 async function bootstrap() {
-  const container = new Container();
-  container.bind<RestApplication>(Component.RestApplication).to(RestApplication).inSingletonScope();
-  container.bind<Logger>(Component.Logger).to(PinoLogger).inSingletonScope();
-  container.bind<Config<RestSchema>>(Component.Config).to(RestConfig).inSingletonScope();
-  container.bind<DatabaseClient>(Component.DatabaseClient).to(MongoDatabaseClient).inSingletonScope();
+  const appContainer = Container.merge(createRestApplicationContainer());
 
-  const application = container.get<RestApplication>(Component.RestApplication);
+  const application = appContainer.get<RestApplication>(Component.RestApplication);
   await application.init();
 }
 
