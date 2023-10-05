66df356f880937726650c19caa959240d0117bed
Igor Antonov <antonov.igor.khv@gmail.com>
2023-10-05 17:28:35 +0300

    5.14. Расширит интерфейс `UserService`.
    Метод для обновления `UserEntity` в настоящее время нереализован, поэтому расширим интерфейс `UserService` и добавить метод `updateById`. Метод имплементируем по аналогии с аналогичным в `DefaultOfferService`. Для выполнения фактического обновления потребуется создать `UpdateUserDto`.

--- src/shared/modules/user/default-user.service.ts
+++ src/shared/modules/user/default-user.service.ts
@@ -5,6 +5,7 @@ import { CreateUserDto } from './dto/create-user.dto.js';
 import { inject, injectable } from 'inversify';
 import { Component } from '../../types/index.js';
 import { Logger } from '../../libs/logger/index.js';
+import { UpdateUserDto } from './dto/update-user.dto.js';
 
 @injectable()
 export class DefaultUserService implements UserService {
@@ -35,5 +36,11 @@ export class DefaultUserService implements UserService {
     }
 
     return this.create(dto, salt);
+  }
+
+  public async updateById(userId: string, dto: UpdateUserDto): Promise<DocumentType<UserEntity> | null> {
+    return this.userModel
+      .findByIdAndUpdate(userId, dto, { new: true })
+      .exec();
   }
 }
