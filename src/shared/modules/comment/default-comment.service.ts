dd0c29f828f9ef4ac1d3f6eef5a66106dd554ecb
Igor Antonov <antonov.igor.khv@gmail.com>
2023-10-05 17:26:00 +0300

    5.13. Добавит `DefaultCommentService`.
    Следующим шагом подготовим всё необходимое для сохранения данных в
    базу. Начнём с интерфейса `CommentService`. Сервис должен уметь сохранять данные в базу (метод `create`) и извлекать комментарии для определённого объявления (метод `findByOfferId`).
    
    Сервиса будет внедряться в виде зависимости, поэтому сразу добавим
    информацию о нём и модели в контейнер зависимостей. Для этого добавим
    новый компонент (`Component`) и соответствующие привязки в `main.rest.ts`.

---
+++ src/shared/modules/comment/default-comment.service.ts
@@ -0,0 +1,32 @@
+import { inject, injectable } from 'inversify';
+import { CommentService } from './comment-service.interface.js';
+import { Component } from '../../types/index.js';
+import { DocumentType, types } from '@typegoose/typegoose';
+import { CommentEntity } from './comment.entity.js';
+import { CreateCommentDto } from './dto/create-comment.dto.js';
+
+@injectable()
+export class DefaultCommentService implements CommentService {
+  constructor(
+    @inject(Component.CommentModel) private readonly commentModel: types.ModelType<CommentEntity>
+  ) {}
+
+  public async create(dto: CreateCommentDto): Promise<DocumentType<CommentEntity>> {
+    const comment = await this.commentModel.create(dto);
+    return comment.populate('userId');
+  }
+
+  public async findByOfferId(offerId: string): Promise<DocumentType<CommentEntity>[]> {
+    return this.commentModel
+      .find({offerId})
+      .populate('userId');
+  }
+
+  public async deleteByOfferId(offerId: string): Promise<number> {
+    const result = await this.commentModel
+      .deleteMany({offerId})
+      .exec();
+
+    return result.deletedCount;
+  }
+}
---
+++ src/shared/modules/comment/index.ts
@@ -0,0 +1,4 @@
+export * from './comment-service.interface.js';
+export * from './comment.container.js';
+export * from './comment.entity.js';
+export * from './default-comment.service.js';
