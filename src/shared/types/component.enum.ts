3b00f8a687ec55a28fc78dc59ca542aac5d6cb28
Igor Antonov <antonov.igor.khv@gmail.com>
2023-10-10 14:38:06 +0300

    6.8. Добавит компонент `CategoryController`. WIP
    Добавим `CategoryController` в компоненты приложения. Компонент потребуется для добавления привязки в контейнер с зависимостями, чтобы потом получить к нему доступ в виде зависимостей.

--- src/shared/types/component.enum.ts
+++ src/shared/types/component.enum.ts
@@ -11,4 +11,5 @@ export const Component = {
   OfferModel: Symbol.for('OfferModel'),
   CommentService: Symbol.for('CommentService'),
   CommentModel: Symbol.for('CommentModel'),
+  CategoryController: Symbol.for('CategoryController'),
 } as const;
