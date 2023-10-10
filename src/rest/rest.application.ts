7fecf2fc4d64ba6e994360cb4a5b0103f94cc168
Igor Antonov <antonov.igor.khv@gmail.com>
2023-10-10 14:51:10 +0300

    6.10. Добавит регистрацию контроллеров. WIP
    Следующим шагом доработаем класс `RestApplication`. Ему необходимо научить регистрировать маршруты, которые мы определяем с помощью контроллеров. Для решения этой задачи создадим метод `_initControllers`.
    
    В этом методе зарегистрируем все экземпляры Express-роутеров с
    помощью метода `use` экземпляра `express`. Для `CategoryController` первым
    аргументом передадим название общий путь к ресурсу (`/categories`),
    а вторым — экземпляр нужного `Router`. Его получим через геттер
    `router` нужного контроллера. Сам контроллер `CategoryController`
    внедрим в виде зависимости.
    
    Получается, что маршруту, которые мы добавили в `CategoryController`
    будут доступны через сегмент `/categories`. Например,
    `GET http://localhost:4000/categories`.
    
    Вызов метода `_initControllers` напишем в методе `init`.
    
    WIP. Линтер ругается на неиспользуемые переменные.

--- src/rest/rest.application.ts
+++ src/rest/rest.application.ts
@@ -5,6 +5,7 @@ import { Config, RestSchema } from '../shared/libs/config/index.js';
 import { Component } from '../shared/types/index.js';
 import { DatabaseClient } from '../shared/libs/database-client/index.js';
 import { getMongoURI } from '../shared/helpers/index.js';
+import { Controller } from '../shared/libs/rest/index.js';
 
 @injectable()
 export class RestApplication {
@@ -14,6 +15,7 @@ export class RestApplication {
     @inject(Component.Logger) private readonly logger: Logger,
     @inject(Component.Config) private readonly config: Config<RestSchema>,
     @inject(Component.DatabaseClient) private readonly databaseClient: DatabaseClient,
+    @inject(Component.CategoryController) private readonly categoryController: Controller,
   ) {
     this.server = express();
   }
@@ -35,12 +37,20 @@ export class RestApplication {
     this.server.listen(port);
   }
 
+  private async _initControllers() {
+    this.server.use('/categories', this.categoryController.router);
+  }
+
   public async init() {
     this.logger.info('Application initialization');
 
     this.logger.info('Init database…');
     await this._initDb();
     this.logger.info('Init database completed');
+
+    this.logger.info('Init controllers');
+    await this._initControllers();
+    this.logger.info('Controller initialization completed');
 
     this.logger.info('Try to init server…');
     await this._initServer();
