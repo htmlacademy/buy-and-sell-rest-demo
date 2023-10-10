ee2ce8182ae46044853ab1d95dc6f7a74aca71f1
Igor Antonov <antonov.igor.khv@gmail.com>
2023-10-10 13:24:08 +0300

    6.2. Добавит создание и запуск сервера `express`.
    Добавим создание экземпляра сервера с помощью Express в класс `RestApplication`. Экземпляр сохраним в приватном поле `server`.
    После этого в методе `init` воспользуемся экземпляром `express` и
    запустим сервер на прослушивание входящих подключений. Для этого вызовем метод `listen`. Ему нужно передать порт, на который будем принимать подключения. Номер порта возьмём из конфигурации (`PORT`).

--- src/rest/rest.application.ts
+++ src/rest/rest.application.ts
@@ -1,4 +1,5 @@
 import { inject, injectable } from 'inversify';
+import express, { Express } from 'express';
 import { Logger } from '../shared/libs/logger/index.js';
 import { Config, RestSchema } from '../shared/libs/config/index.js';
 import { Component } from '../shared/types/index.js';
@@ -7,11 +8,15 @@ import { getMongoURI } from '../shared/helpers/index.js';
 
 @injectable()
 export class RestApplication {
+  private server: Express;
+
   constructor(
     @inject(Component.Logger) private readonly logger: Logger,
     @inject(Component.Config) private readonly config: Config<RestSchema>,
     @inject(Component.DatabaseClient) private readonly databaseClient: DatabaseClient,
-  ) {}
+  ) {
+    this.server = express();
+  }
 
   private async _initDb() {
     const mongoUri = getMongoURI(
@@ -25,12 +30,20 @@ export class RestApplication {
     return this.databaseClient.connect(mongoUri);
   }
 
+  private async _initServer() {
+    const port = this.config.get('PORT');
+    this.server.listen(port);
+  }
+
   public async init() {
     this.logger.info('Application initialization');
-    this.logger.info(`Get value from env $PORT: ${this.config.get('PORT')}`);
 
     this.logger.info('Init database…');
     await this._initDb();
     this.logger.info('Init database completed');
+
+    this.logger.info('Try to init server…');
+    await this._initServer();
+    this.logger.info(`🚀 Server started on http://localhost:${this.config.get('PORT')}`);
   }
 }
