733e18ca1fd991caf38aaea4cade38d8e12f273b
Igor Antonov <antonov.igor.khv@gmail.com>
2023-10-10 15:38:16 +0300

    6.17. Добавит класс `AppExceptionFilter`.
    Теперь добавим имплементацию `ExceptionFilter`. Для этого создадим класс `AppExceptionFilter`.
    
    В классе нам потребуется логер. Поэтому внедрим его сразу в виде
    зависимости через конструктор. В конструкторе добавим вывод информации о регистрации `AppExceptionFilter`.
    
    Затем имплементируем метод `catch`. Пока сделаем самый простой вариант. Сделаем запись в лог сообщения ошибки (`error.message`) и отправим клиенту корректный ответ. Помните, даже если в сервисе произошла ошибка, следует отправить клиенту корректный код. В случае неизвестной ошибки таким кодом станет `500`.
    
    Помимо кода с ошибкой отправим объект с ошибкой. Здесь можно пойти дальше и внедрить отправку дополнительных констант для автоматической обработки ошибок клиентом, но пока не будем переусложнять.

---
+++ src/shared/libs/rest/exception-filter/app-exception-filter.ts
@@ -0,0 +1,22 @@
+import { inject, injectable } from 'inversify';
+import { StatusCodes } from 'http-status-codes';
+import { NextFunction, Request, Response } from 'express';
+import { ExceptionFilter } from './exception-filter.interface.js';
+import { Logger } from '../../logger/index.js';
+import { Component } from '../../../types/index.js';
+
+@injectable()
+export class AppExceptionFilter implements ExceptionFilter {
+  constructor(
+    @inject(Component.Logger) private readonly logger: Logger
+  ) {
+    this.logger.info('Register AppExceptionFilter');
+  }
+
+  public catch(error: Error, _req: Request, res: Response, _next: NextFunction): void {
+    this.logger.error(error.message, error);
+    res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json({error: error.message});
+  }
+}
--- src/shared/libs/rest/index.ts
+++ src/shared/libs/rest/index.ts
@@ -3,3 +3,4 @@ export * from './types/route.interface.js';
 export * from './controller/controller.interface.js';
 export * from './controller/base-controller.abstract.js';
 export * from './exception-filter/exception-filter.interface.js';
+export * from './exception-filter/app-exception-filter.js';
