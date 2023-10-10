a25f31eba01dbd8d21f95f1b51e327ef3b3753f6
Igor Antonov <antonov.igor.khv@gmail.com>
2023-10-10 14:59:04 +0300

    6.11. Добавит код обработчика `index` в `CategoryController`.
    Вернёмся к `CategoryController`. Расширим конструктор и подключим `categoryService`. Его внедрим в виде зависимости.
    
    Затем опишем метод `index`. Вместо комментария добавим получение списка категорий, а затем их отправку клиенту. Обратите внимание, в `statusCode` мы передаём код, полученный из перечисления `http-status-codes`. Это удобней, чем помнить в голове соответствие всех кодов.
    
    В этом обработчике нет необходимости использовать параметр `req`, поэтому добавим ему подчёркивание, чтобы линтер не ругался.
    
    Чтобы проверить работу обработчика, в список запросов (`category.http`) добавим GET-запрос для получения списка категорий.

--- src/shared/modules/category/category.controller.ts
+++ src/shared/modules/category/category.controller.ts
@@ -3,11 +3,13 @@ import { Request, Response } from 'express';
 import { BaseController, HttpMethod } from '../../libs/rest/index.js';
 import { Logger } from '../../libs/logger/index.js';
 import { Component } from '../../types/index.js';
+import { CategoryService } from './category-service.interface.js';
 
 @injectable()
 export class CategoryController extends BaseController {
   constructor(
     @inject(Component.Logger) protected readonly logger: Logger,
+    @inject(Component.CategoryService) private readonly categoryService: CategoryService,
   ) {
     super(logger);
 
@@ -17,11 +19,12 @@ export class CategoryController extends BaseController {
     this.addRoute({ path: '/', method: HttpMethod.Post, handler: this.create });
   }
 
-  public index(req: Request, res: Response): void {
-    // Код обработчика
+  public async index(_req: Request, res: Response): Promise<void> {
+    const categories = await this.categoryService.find();
+    this.ok(res, categories);
   }
 
-  public create(req: Request, res: Response): void {
+  public create(_req: Request, _res: Response): void {
     // Код обработчика
   }
 }
