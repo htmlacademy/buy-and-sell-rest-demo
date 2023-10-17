a293feea3cae206741b66ce15afe99c4544148a7
Igor Antonov <antonov.igor.khv@gmail.com>
2023-10-17 15:48:45 +0300

    7.13. Добавит регистрацию middleware в `BaseController`.
    Передавать middleware при регистрации обработчиков маршрутов мы можем,
    но чтобы они заработали, необходимо их зарегистрировать. Для этого
    немного модифицируем метод `addRoute` в классе `BaseController`.
    
    Для начала позаботимся о привязке контекста и оборачивании middleware
    в `asyncHandler`. Для этого трансформируем массив `middlewares`.
    Обратите внимание, что мы используем оператор `?`, чтобы не делать
    проверку существования свойства `middlewares` в блоке `if`. Таким
    образом, в `middlewares` будет либо `undefined` (если middlewares не
    были переданы), либо массив из функций `execute` (middlewares).
    
    Затем заведём отдельную переменную, в которой сохраним общую цепочку
    обработчиков (вместе с middlewares). Если middleware есть, то копируем
    на них ссылки, а в конце добавляем оригинальный обработчик. В противном
    случае используем обработчик. Последним шагом заменим `wrapperAsyncHandler` на
    `allHandlers`.

--- src/shared/libs/rest/controller/base-controller.abstract.ts
+++ src/shared/libs/rest/controller/base-controller.abstract.ts
@@ -24,7 +24,12 @@ export abstract class BaseController implements Controller {
 
   public addRoute(route: Route) {
     const wrapperAsyncHandler = asyncHandler(route.handler.bind(this));
-    this._router[route.method](route.path, wrapperAsyncHandler);
+    const middlewareHandlers = route.middlewares?.map(
+      (item) => asyncHandler(item.execute.bind(item))
+    );
+    const allHandlers = middlewareHandlers ? [...middlewareHandlers, wrapperAsyncHandler] : wrapperAsyncHandler;
+
+    this._router[route.method](route.path, allHandlers);
     this.logger.info(`Route registered: ${route.method.toUpperCase()} ${route.path}`);
   }
 
