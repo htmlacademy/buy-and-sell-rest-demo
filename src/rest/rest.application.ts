c54a403e3d278d8250889a5915967199d65bac22
Igor Antonov <antonov.igor.khv@gmail.com>
2023-10-17 17:25:23 +0300

    7.31. Зарегистрирует middleware `express.static`.
    Файлы в директории `upload` должны быть доступны снаружи. То есть после
    загрузки файла клиент может обратиться к этому файлу через GET-запрос. Чтобы решить эту задачу, в Express есть встроенная middleware — `express.static`. Она позволяет отдавать клиенту
    статические файлы. В нашем случае это любой файл из директории `upload`.
    
    Зарегистрируем эту `express.static` в качестве глобальной middleware в методе `_initMiddleware` класса `RestApplication`.
    
    Стоит сразу отметить, что для production варианта — это не лучший вариант. Если подразумевается интенсивная работа (получение файлов клиентом), то лучше поручить выполнение этой задачи веб-серверу.
    
    Например, nginx. Это эффективней, чем загружать Node.js.
    
    P.S. Запустите проект и поместите любой файл в директорию `upload`.
    Затем откройте браузер и попробуйте обратиться к этому файлу. Например, `http://localhost:4000/upload/yourfile.jpg`. Вы должны увидеть содержимое файла.

--- src/rest/rest.application.ts
+++ src/rest/rest.application.ts
@@ -50,6 +50,10 @@ export class RestApplication {
 
   private async _initMiddleware() {
     this.server.use(express.json());
+    this.server.use(
+      '/upload',
+      express.static(this.config.get('UPLOAD_DIRECTORY'))
+    );
   }
 
   private async _initExceptionFilters() {
