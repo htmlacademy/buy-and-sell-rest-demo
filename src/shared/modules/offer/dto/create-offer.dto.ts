5e7c9936dc3402363e00eca6e94fb735bcc9089f
Igor Antonov <antonov.igor.khv@gmail.com>
2023-10-17 16:16:13 +0300

    7.19. Добавит валидаторы для `CreateOfferDto`.
    Опишем правила валидации для `CreateOfferDto`. Напомним, с помощью него мы создаём новые объявления. Чтобы указать правила валидации, мы должны
    воспользоваться одним из множества декораторов.
    
    Например, если мы хотим ограничить длину поля, то для этого `class-validator` предлагает декораторы `@MinLength` и `@MaxLength`. Другой кейс: проверка строки, которая содержит дату. Для этого есть декоратор `@IsDateString`. Ещё один полезный декоратор — `@IsMongoId`.
    С его помощью можно проверить поля, где должен передаваться идентификатор
    записи (`ObjectId`) на корректность.
    
    Каждый декоратор для валидации аргументами принимает объект настроек.
    Настройки разнятся от валидатора к валидатору и подробней про доступные
    можно прочитать в документации. Однако, у любого объекта настроек есть
    поле `message`. В него можно прописать сообщение, которое будет
    формироваться при ошибке валидации.
    
    Обратите внимание на применение декоратора для поля `categories`. В
    этом поле находится массив идентификаторов категорий. Чтобы
    провалидировать каждый элемент с помощью валидатора `@IsMongoId` можно
    указать в объекте настроек `each: true`. Таким образом, валидатор будет
    применён для каждого элемента массива.

--- src/shared/modules/offer/dto/create-offer.dto.ts
+++ src/shared/modules/offer/dto/create-offer.dto.ts
@@ -1,12 +1,34 @@
+import { IsArray, IsDateString, IsEnum, IsInt, IsMongoId, Max, MaxLength, Min, MinLength } from 'class-validator';
 import { OfferType } from '../../../types/index.js';
+import { CreateOfferValidationMessage } from './create-offer.messages.js';
 
 export class CreateOfferDto {
+  @MinLength(10, { message: CreateOfferValidationMessage.title.minLength })
+  @MaxLength(100, { message: CreateOfferValidationMessage.title.maxLength })
   public title: string;
+
+  @MinLength(20, { message: CreateOfferValidationMessage.description.minLength })
+  @MaxLength(1024, { message: CreateOfferValidationMessage.description.maxLength })
   public description: string;
+
+  @IsDateString({}, { message: CreateOfferValidationMessage.postDate.invalidFormat })
   public postDate: Date;
+
+  @MaxLength(256, { message: CreateOfferValidationMessage.image.maxLength })
   public image: string;
+
+  @IsEnum(OfferType, { message: CreateOfferValidationMessage.type.invalid })
   public type: OfferType;
+
+  @IsInt({ message: CreateOfferValidationMessage.price.invalidFormat })
+  @Min(100, { message: CreateOfferValidationMessage.price.minValue })
+  @Max(200000, { message: CreateOfferValidationMessage.price.maxValue })
   public price: number;
+
+  @IsArray({ message: CreateOfferValidationMessage.categories.invalidFormat })
+  @IsMongoId({ each: true, message: CreateOfferValidationMessage.categories.invalidId })
   public categories: string[];
+
+  @IsMongoId({ message: CreateOfferValidationMessage.userId.invalidId })
   public userId: string;
 }
--- src/shared/modules/offer/index.ts
+++ src/shared/modules/offer/index.ts
@@ -4,3 +4,4 @@ export * from './offer-service.interface.js';
 export * from './default-offer.service.js';
 export * from './offer.container.js';
 export * from './rdo/offer.rdo.js';
+export * from './dto/create-offer.messages.js';
