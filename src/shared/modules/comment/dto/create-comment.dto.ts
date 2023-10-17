411247563c3a584c8ed788db9d33de285d3a441f
Igor Antonov <antonov.igor.khv@gmail.com>
2023-10-17 17:06:06 +0300

    7.26. Добавит валидацию в `CreateCommentDto`.
     Повторим предыдущий опыт и добавим валидацию для `CreateCommentDto`.

--- src/shared/modules/comment/dto/create-comment.dto.ts
+++ src/shared/modules/comment/dto/create-comment.dto.ts
@@ -1,5 +1,14 @@
+import { IsMongoId, IsString, Length } from 'class-validator';
+import { CreateCommentMessages } from './create-comment.messages.js';
+
 export class CreateCommentDto {
+  @IsString({ message: CreateCommentMessages.text.invalidFormat })
+  @Length(5, 1024, { message: 'min is 5, max is 1024 '})
   public text: string;
+
+  @IsMongoId({ message: CreateCommentMessages.offerId.invalidFormat })
   public offerId: string;
+
+  @IsMongoId({ message: CreateCommentMessages.userId.invalidFormat })
   public userId: string;
 }
