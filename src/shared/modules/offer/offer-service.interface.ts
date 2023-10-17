36d4ed471d52044803038f1de1768e1034f0dd64
Igor Antonov <antonov.igor.khv@gmail.com>
2023-10-17 17:13:32 +0300

    7.28. Расширит интерфейс `OfferService`.
    Расширим интерфейс и подключим новую middleware для обработчика `getComments` в контроллере `OfferController`.

--- src/shared/modules/offer/offer-service.interface.ts
+++ src/shared/modules/offer/offer-service.interface.ts
@@ -2,8 +2,9 @@ import { CreateOfferDto } from './dto/create-offer.dto.js';
 import { DocumentType } from '@typegoose/typegoose';
 import { OfferEntity } from './offer.entity.js';
 import { UpdateOfferDto } from './dto/update-offer.dto.js';
+import { DocumentExists } from '../../types/index.js';
 
-export interface OfferService {
+export interface OfferService extends DocumentExists {
   create(dto: CreateOfferDto): Promise<DocumentType<OfferEntity>>;
   findById(offerId: string): Promise<DocumentType<OfferEntity> | null>;
   find(): Promise<DocumentType<OfferEntity>[]>;
