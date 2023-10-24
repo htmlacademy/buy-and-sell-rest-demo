5d6c3a45602763afa7739f38fbfc757eae6856a4
Igor Antonov <antonov.igor.khv@gmail.com>
2023-10-24 12:49:34 +0300

    8.2. Добавит в конфигурацию `JWT_SECRET`.
    При подготовке токена нам потребуется подготовить секрет — строка, которая будет использоваться в процессе шифрования. За время работы приложения она может меняться. Будем передавать её в переменной
    окружения. Заведём в конфигурации отдельную переменную окружения `JWT_SECRET`. Значения по умолчанию не будет (`null`). Затем добавим её в `.env`.

--- src/shared/libs/config/rest.schema.ts
+++ src/shared/libs/config/rest.schema.ts
@@ -12,6 +12,7 @@ export type RestSchema = {
   DB_PORT: string;
   DB_NAME: string;
   UPLOAD_DIRECTORY: string;
+  JWT_SECRET: string;
 }
 
 export const configRestSchema = convict<RestSchema>({
@@ -61,6 +62,12 @@ export const configRestSchema = convict<RestSchema>({
     doc: 'Directory for upload files',
     format: String,
     env: 'UPLOAD_DIRECTORY',
+    default: null
+  },
+  JWT_SECRET: {
+    doc: 'Secret for sign JWT',
+    format: String,
+    env: 'JWT_SECRET',
     default: null
   },
 });
