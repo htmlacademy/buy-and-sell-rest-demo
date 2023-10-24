7acc68b22f5c9ee53f7db4dd472614ca95c15f34
Igor Antonov <antonov.igor.khv@gmail.com>
2023-10-24 18:42:26 +0300

    8.9. Добавит `ParseTokenMiddleware`. WIP
    Токены формируются. Теперь надо как-то извлечь информацию из `payload` токена. Это можно делать на уровне обработчика маршрута, но в этом случае, один и тот же код придётся дублировать в разных местах
    приложения. Чтобы этого избежать, создадим `ParseTokenMiddleware`,
    которую зарегистрируем глобально. Если к запросу будет приложен токен, то middleware сможет извлечь из него информацию и добавить к запросу.
    
    Начнём с извлечения информации. Сначала проверим наличие заголовка `authorization`. Если он есть, значит продолжаем работать. Затем
    извлечём сам токен. После этого проверим с помощью `jwtVerify`, Если всё ок, то сохраним в свойство запроса `user`. Таким образом, когда
    запрос долетит до обработчика маршрута, у нас будет вся необходимая информация.

---
+++ src/shared/libs/rest/middleware/parse-token.middleware.ts
@@ -0,0 +1,46 @@
+import { NextFunction, Request, Response } from 'express';
+import { jwtVerify } from 'jose';
+import { StatusCodes } from 'http-status-codes';
+import { createSecretKey } from 'node:crypto';
+import { Middleware } from './middleware.interface.js';
+import { HttpError } from '../errors/index.js';
+import { TokenPayload } from '../../../modules/auth/index.js';
+
+function isTokenPayload(payload: unknown): payload is TokenPayload {
+  return (
+    (typeof payload === 'object' && payload !== null) &&
+    ('email' in payload && typeof payload.email === 'string') &&
+    ('firstname' in payload && typeof payload.firstname === 'string') &&
+    ('lastname' in payload && typeof payload.lastname === 'string') &&
+    ('id' in payload && typeof payload.id === 'string')
+  );
+}
+
+export class ParseTokenMiddleware implements Middleware {
+  constructor(private readonly jwtSecret: string) {}
+
+  public async execute(req: Request, _res: Response, next: NextFunction): Promise<void> {
+    const authorizationHeader = req.headers?.authorization?.split(' ');
+    if (!authorizationHeader) {
+      return next();
+    }
+
+    const [, token] = authorizationHeader;
+
+    try {
+      const { payload } = await jwtVerify(token, createSecretKey(this.jwtSecret, 'utf-8'));
+
+      if (isTokenPayload(payload)) {
+        req.tokenPayload = { ...payload };
+        return next();
+      }
+    } catch {
+
+      return next(new HttpError(
+        StatusCodes.UNAUTHORIZED,
+        'Invalid token',
+        'AuthenticateMiddleware')
+      );
+    }
+  }
+}
