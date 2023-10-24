67bc343b5a92e37f40b231ef2a11068c4955d7c1
Igor Antonov <antonov.igor.khv@gmail.com>
2023-10-24 17:15:27 +0300

    8.8. Добавит `login` в `UserController`.
    Теперь внедрим в качестве зависимости новый сервис в `UserController` и имплементируем обработчик `login`. Если всё прошло хорошо, то он вернёт новый токен. Проверить токен на корректность и посмотреть его содержимое вы можете на сайте https://jwt.io/.
    
    При тестировании попробуйте воспроизвести негативные сценарии: отправьте некорректные логин или пароль. `AuthExceptionFilter` должен перехватить ошибки.

--- src/shared/modules/user/index.ts
+++ src/shared/modules/user/index.ts
@@ -3,3 +3,5 @@ export * from './dto/create-user.dto.js';
 export * from './default-user.service.js';
 export * from './user.container.js';
 export * from './user.controller.js';
+export * from './dto/login-user.dto.js';
+export * from './user-service.interface.js';
