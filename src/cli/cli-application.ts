b356e797cf3d85ca5b3383bc57b5ebb109847ba9
Igor Antonov <antonov.igor.khv@gmail.com>
2024-01-10 17:52:45 +0300

    1.6. Добавит менеджеру метод `processCommand` в `CLIApplication`.
    Команды регистрируются, пользовательский ввод парсится (разбирается). Остаётся научить приложение исполнять команды.
    
    Создадим отдельный публичный метод `processCommand`. Аргументом он получает пользовательский ввод и задействует ранее созданные методы.
    
    Обратите внимание, мы предусмотрели команду по умолчанию. Ей становится `--help`.

--- src/cli/cli-application.ts
+++ src/cli/cli-application.ts
@@ -1,9 +1,14 @@
 import { Command } from './commands/command.interface.js';
+import { CommandParser } from './command-parser.js';
 
 type CommandCollection = Record<string, Command>;
 
 export class CLIApplication {
   private commands: CommandCollection = {};
+
+  constructor(
+    private readonly defaultCommand: string = '--help'
+  ) {}
 
   public registerCommands(commandList: Command[]): void {
     commandList.forEach((command) => {
@@ -12,5 +17,24 @@ export class CLIApplication {
       }
       this.commands[command.getName()] = command;
     });
+  }
+
+  public getCommand(commandName: string): Command {
+    return this.commands[commandName] ?? this.getDefaultCommand();
+  }
+
+  public getDefaultCommand(): Command | never {
+    if (! this.commands[this.defaultCommand]) {
+      throw new Error(`The default command (${this.defaultCommand}) is not registered.`);
+    }
+    return this.commands[this.defaultCommand];
+  }
+
+  public processCommand(argv: string[]): void {
+    const parsedCommand = CommandParser.parse(argv);
+    const [commandName] = Object.keys(parsedCommand);
+    const command = this.getCommand(commandName);
+    const commandArguments = parsedCommand[commandName] ?? [];
+    command.execute(...commandArguments);
   }
 }
