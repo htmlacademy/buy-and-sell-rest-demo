94392b234264c99624879f6c0e5b2c0cdce54fda
Igor Antonov <antonov.igor.khv@gmail.com>
2024-01-10 17:42:48 +0300

    1.2. Добавит команду `VersionCommand`.
    Создадим новый модуль `version.command.ts` и опишем класс, который имплементирует ранее созданный интерфейс (`Command`).
    
    Команда `--version` должна считывать из `package.json` версию приложения. То есть значение свойства `version`.
    
    Для чтения файла воспользуемся встроенным модулем `readFileSync`. Импортируем из модуля `node:fs` функцию `readFileSync` и прочитаем с её помощью файл `package.json`. Не забываем указывать кодировку.
    
    Перед чтением файла воспользуемся функцией `resolve` из встроенного модуля `node:path`. Эта функция преобразует относительный путь в абсолютный.

---
+++ src/cli/commands/version.command.ts
@@ -0,0 +1,50 @@
+import { readFileSync } from 'node:fs';
+import { resolve } from 'node:path';
+import { Command } from './command.interface.js';
+
+type PackageJSONConfig = {
+  version: string;
+}
+
+function isPackageJSONConfig(value: unknown): value is PackageJSONConfig {
+  return (
+    typeof value === 'object' &&
+    value !== null &&
+    !Array.isArray(value) &&
+    Object.hasOwn(value, 'version')
+  );
+}
+
+export class VersionCommand implements Command {
+  constructor(
+    private readonly filePath: string = './package.json'
+  ) {}
+
+  private readVersion(): string {
+    const jsonContent = readFileSync(resolve(this.filePath), 'utf-8');
+    const importedContent: unknown = JSON.parse(jsonContent);
+
+    if (! isPackageJSONConfig(importedContent)) {
+      throw new Error('Failed to parse json content.');
+    }
+
+    return importedContent.version;
+  }
+
+  public getName(): string {
+    return '--version';
+  }
+
+  public async execute(..._parameters: string[]): Promise<void> {
+    try {
+      const version = this.readVersion();
+      console.info(version);
+    } catch (error: unknown) {
+      console.error(`Failed to read version from ${this.filePath}`);
+
+      if (error instanceof Error) {
+        console.error(error.message);
+      }
+    }
+  }
+}
