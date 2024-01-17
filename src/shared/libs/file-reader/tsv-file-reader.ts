c1467fde4341de29bd5f40854498a090e52fc2ec
Igor Antonov <antonov.igor.khv@gmail.com>
2024-01-17 07:56:41 +0300

    2.14. Рефакторит класс `TSVFileReader`. WIP
    Продолжим рефакторинг. Импортируем в модуль `TSVFileReader` класс `EventEmitter`
    из модуля `events`. Он потребуется нам для организации подписок на события. События помогут оповестить подписчиков о считывании очередной
    «полезной» порции данных.
    
    `EventEmitter` — это встроенный модуль в Node.js, который позволяет создавать и обрабатывать события. Он предоставляет функциональность для создания пользовательских объектов, которые могут генерировать и слушать события.
    
    WIP: Импорт по-прежнему не работает.

--- src/shared/libs/file-reader/tsv-file-reader.ts
+++ src/shared/libs/file-reader/tsv-file-reader.ts
@@ -1,14 +1,12 @@
+import EventEmitter from 'node:events';
 import { FileReader } from './file-reader.interface.js';
-import { readFileSync } from 'node:fs';
 
-export class TSVFileReader implements FileReader {
-  private rawData = '';
-
-  constructor(
-    private readonly filename: string
-  ) {}
+export class TSVFileReader extends EventEmitter implements FileReader {
+  constructor(private readonly filename: string) {
+    super();
+  }
 
   public read(): void {
-    this.rawData = readFileSync(this.filename, { encoding: 'utf-8' });
+    // Код для работы с потоками
   }
 }
