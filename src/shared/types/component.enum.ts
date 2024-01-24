de602b4de28aee6bdb71f936bedff92c96fda34b
Igor Antonov <antonov.igor.khv@gmail.com>
2024-01-24 17:42:05 +0300

    3.20. Добавит перечисление `Component`.
    Подготовим новый модуль с перечислением компонентов нашего приложения, которые будут участвовать при внедрении зависимостей.
    
    Новый модуль создаём в директории `types`, хотя фактически это не является описание типа в контексте TypeScript. Мы описываем простой константный объект. Ключи объекта — название компонентов, а если точней
    интерфейсов. Значения ключей — символы. Можно использовать строки, но символы уникальны «из коробки».
    
    Объект нам потребуется при подготовке контейнера зависимостей.

---
+++ src/shared/types/component.enum.ts
@@ -0,0 +1,5 @@
+export const Component = {
+  RestApplication: Symbol.for('RestApplication'),
+  Logger: Symbol.for('Logger'),
+  Config: Symbol.for('Config'),
+} as const;
--- src/shared/types/index.ts
+++ src/shared/types/index.ts
@@ -3,3 +3,4 @@ export * from './offer-type.enum.js';
 export * from './user.type.js';
 export * from './offer.type.js';
 export * from './mock-server-data.type.js';
+export * from './component.enum.js';
