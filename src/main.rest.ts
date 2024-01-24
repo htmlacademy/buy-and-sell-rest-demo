c692a7ccb9d4679341fa91b7f6574102f21becb2
Igor Antonov <antonov.igor.khv@gmail.com>
2024-01-24 17:35:15 +0300

    3.13. Добавит `Config` в `RestApplication`.
    В `main.ts` добавим создание экземпляра класса `RestConfig`. При создании экземпляра передадим в конструктор настроенный `logger`.
    
    Настроенный экземпляр `config` передадим в конструктор `RestApplication` вторым аргументом.
    
    Внутри класса `RestApplication` добавим один параметр для конструктора (для приёма экземпляра `RestConfig`).
    
    В методе `init` добавим вывод значения переменной `PORT`. Пока только для проверки.
    
    Протестируйте пример на данном шаге. Создайте в корне проекта файл `.env` и определите в нём переменную `PORT=5000`. Затем запустите
    приложение. Значение переменной должно быть выведено в терминал.

--- src/main.rest.ts
+++ src/main.rest.ts
@@ -1,10 +1,12 @@
 import { PinoLogger } from './shared/libs/logger/index.js';
 import { RestApplication } from './rest/index.js';
+import { RestConfig } from './shared/libs/config/index.js';
 
 async function bootstrap() {
   const logger = new PinoLogger();
+  const config = new RestConfig(logger);
 
-  const application = new RestApplication(logger);
+  const application = new RestApplication(logger, config);
   await application.init();
 }
 
