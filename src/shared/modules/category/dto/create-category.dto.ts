891a7e44b0e57aa18996d3d86869983eb214c0e1
Igor Antonov <antonov.igor.khv@gmail.com>
2024-01-30 07:53:02 +0300

    4.28. Добавит сущность и модель `Category`.
    По аналогии с `UserEntity` создадим сущность `CategoryEntity`, а затем
    опишем интерфейс сервиса `CategoryService`. Сервис должен уметь создавать категории и находить категорию по идентификатору. Этого
    достаточно для начала, а дополнительную функциональность нарастим в
    будущем.
    
    Нам также потребуется DTO `CreateCategoryDto`. Для этого создадим отдельный класс.

---
+++ src/shared/modules/category/dto/create-category.dto.ts
@@ -0,0 +1,3 @@
+export class CreateCategoryDto {
+  public name: string;
+}
---
+++ src/shared/modules/category/index.ts
@@ -0,0 +1,3 @@
+export * from './dto/create-category.dto.js';
+export * from './category-service.interface.js';
+export * from './category.entity.js';
