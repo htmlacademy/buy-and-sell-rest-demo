cfc4d760c964c657f1c35f26dd9871d087ac486d
Igor Antonov <antonov.igor.khv@gmail.com>
2024-01-30 07:39:49 +0300

    4.7. Добавит новый компонент в `Component`.
    В перечисление `Component` добавим новое значение — `DatabaseClient`. Это необходимо, чтобы добавить связку интерфейса `DatabaseClient`
    с определённым классом в IoC-контейнере.

--- src/shared/types/component.enum.ts
+++ src/shared/types/component.enum.ts
@@ -2,4 +2,5 @@ export const Component = {
   RestApplication: Symbol.for('RestApplication'),
   Logger: Symbol.for('Logger'),
   Config: Symbol.for('Config'),
+  DatabaseClient: Symbol.for('DatabaseClient'),
 } as const;
