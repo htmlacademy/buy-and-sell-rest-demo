3229b33ba74981deac62f77f89bfcce8e94aa5bc
Igor Antonov <antonov.igor.khv@gmail.com>
2024-01-30 07:50:23 +0300

    4.20. Добавит методы в `UserEntity`. WIP
    Пропишем значения по умолчанию для свойств и добавим признак
    обязательного заполнения.
    
    При описании класса модели мы можем создавать не только свойства, но и методы. Методы позволяют реализовать дополнительную логику. Например, с помощью методов можно рассчитывать значения свойств на основании
    других.
    
    Но какие методы могут потребоваться в модели `User`? Модель предусматривает поле `password`. В нём должен храниться пароль пользователя. Однако хранить пароли в открытом виде небезопасно.
    Поэтому мы можем предусмотреть метод, который будет хешировать пароль.
    
    Для реализации этой идеи сделаем свойство `password` приватным. Это необходимо, чтобы не было возможности напрямую взаимодействовать с
    содержимым поля. Пароль для пользователя будет устанавливаться с помощью метода `setPassword`.
    
    Для генерации хэша потребуется соль. Соль — случайный набор символов, который будет использоваться при хешировании. Метод `setPassword` аргументом будет принимать пароль и соль.
    
    Для получения хеша создадим в модуле `common.ts` вспомогательную функцию и воспользуемся в ней встроенным модулем `crypto`. Внутри
    функции `createSHA256` воспользуемся методом `createHmac`. Методу
    нужно передать алгоритм хеширования и соль. Существуют разные алгоритмы хеширования, мы воспользуемся `sha256`. Он быстр и надёжен.
    
    Затем запишем в свойство `password` результат выполнения метода `update` у функции хеширования. Методу передаём открытый пароль
    (из одноимённого аргумента).
    
    Получается, что теперь единственный способ установить пароль — воспользоваться методом `setPassword`. А как получить результат хеширования? Для этого опишем отдельный метод `getPassword`. Он возвращает содержимое поля `this.password`. То же самое можно сделать в виде геттера, но мы воспользуемся методом для консистентности.

---
+++ src/shared/helpers/hash.ts
@@ -0,0 +1,6 @@
+import * as crypto from 'node:crypto';
+
+export const createSHA256 = (line: string, salt: string): string => {
+  const shaHasher = crypto.createHmac('sha256', salt);
+  return shaHasher.update(line).digest('hex');
+};
--- src/shared/modules/user/user.entity.ts
+++ src/shared/modules/user/user.entity.ts
@@ -1,9 +1,16 @@
-import { defaultClasses, getModelForClass, prop } from '@typegoose/typegoose';
+import { defaultClasses, getModelForClass, prop, modelOptions } from '@typegoose/typegoose';
 import { User } from '../../types/index.js';
+import { createSHA256 } from '../../helpers/index.js';
 
 // eslint-disable-next-line @typescript-eslint/no-unsafe-declaration-merging
 export interface UserEntity extends defaultClasses.Base {}
 
+@modelOptions({
+  schemaOptions: {
+    collection: 'users',
+    timestamps: true,
+  }
+})
 // eslint-disable-next-line @typescript-eslint/no-unsafe-declaration-merging
 export class UserEntity extends defaultClasses.TimeStamps implements User {
   @prop({ unique: true, required: true })
@@ -12,11 +19,31 @@ export class UserEntity extends defaultClasses.TimeStamps implements User {
   @prop({ required: false, default: '' })
   public avatarPath: string;
 
-  @prop({ required: true })
+  @prop({ required: true, default: '' })
   public firstname: string;
 
-  @prop({ required: true })
+  @prop({ required: true, default: '' })
   public lastname: string;
+
+  @prop({ required: true, default: '' })
+  private password?: string;
+
+  constructor(userData: User) {
+    super();
+
+    this.email = userData.email;
+    this.avatarPath = userData.avatarPath;
+    this.firstname = userData.firstname;
+    this.lastname = userData.lastname;
+  }
+
+  public setPassword(password: string, salt: string) {
+    this.password = createSHA256(password, salt);
+  }
+
+  public getPassword() {
+    return this.password;
+  }
 }
 
 export const UserModel = getModelForClass(UserEntity);
