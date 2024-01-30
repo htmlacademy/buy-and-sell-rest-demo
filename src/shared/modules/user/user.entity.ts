11c2b8df3c77c5e573c4c44f26d2cdcd387daf3b
Igor Antonov <antonov.igor.khv@gmail.com>
2024-01-30 07:49:02 +0300

    4.19. Добавит поля `_id` и `id`. WIP
    У каждого документа есть уникальный идентификатор. Он не повторяется и позволяет идентифицировать определённый документ. Мы самостоятельно
    можем определить соответствующее поле в модели, но поскольку он
    потребуется для каждого документа, то придётся определять это поле в
    каждой модели.
    
    Этого можно избежать, если воспользоваться интерфейсом `Base`. Если просто добавить интерфейс в список `implements`, то придётся добавить в класс поля. Чтобы этого не делать, сделаем интерфейс с тем же именем,
    что и класс и унаследуем его от `Base`.
    
    При объявлении интерфейса и класса с одинаковым именем в TypeScript, будет выполнена операция объединения типов (type merging). В результате, и интерфейс, и класс будут содержать объединенные свойства и методы.
    
    Оба объявления типов будут объединены в один, и результирующий тип будет содержать свойства и методы из обоих объявлений. В данном случае, результирующий тип `UserEntity` будет иметь свойства и методы из интерфейса `UserEntity` и класса `UserEntity`.

--- src/shared/modules/user/user.entity.ts
+++ src/shared/modules/user/user.entity.ts
@@ -1,6 +1,10 @@
 import { defaultClasses, getModelForClass, prop } from '@typegoose/typegoose';
 import { User } from '../../types/index.js';
 
+// eslint-disable-next-line @typescript-eslint/no-unsafe-declaration-merging
+export interface UserEntity extends defaultClasses.Base {}
+
+// eslint-disable-next-line @typescript-eslint/no-unsafe-declaration-merging
 export class UserEntity extends defaultClasses.TimeStamps implements User {
   @prop({ unique: true, required: true })
   public email: string;
