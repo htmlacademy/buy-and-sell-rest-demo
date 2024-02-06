4b79b2302d711f0c15ff3757539ae2d875e0ec35
Igor Antonov <antonov.igor.khv@gmail.com>
2024-02-06 18:31:06 +0300

    5.10. Реализует обновлённый интерфейс `OfferService`.
    В этом коммите реализуем все методы обновлённого интерфейса `OfferService`.
    
    Принцип реализации не сильно отличается: по факту это комбинация методов, которые предоставляет Mongoose. Здесь мы добавляем вызов метода `sort` (для сортировки), `findByIdAndUpdate` (обновления документа), `limit` (ограничения вывода) и так далее.

--- src/shared/types/index.ts
+++ src/shared/types/index.ts
@@ -4,3 +4,4 @@ export * from './user.type.js';
 export * from './offer.type.js';
 export * from './mock-server-data.type.js';
 export * from './component.enum.js';
+export * from './sort-type.enum.js';
