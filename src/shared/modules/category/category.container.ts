ef8cb5b652bfc959e2617bcd58c1b5167b8db1fd
Igor Antonov <antonov.igor.khv@gmail.com>
2024-02-09 18:26:29 +0300

    6.9. Добавит `CategoryController` в контейнер. WIP
    Добавим привязку для `CategoryController` в контейнер. Это позволит внедрять `CategoryController` в виде зависимости.

--- src/shared/modules/category/category.container.ts
+++ src/shared/modules/category/category.container.ts
@@ -4,12 +4,15 @@ import { Component } from '../../types/index.js';
 import { CategoryService } from './category-service.interface.js';
 import { DefaultCategoryService } from './category.service.js';
 import { CategoryEntity, CategoryModel } from './category.entity.js';
+import { Controller } from '../../libs/rest/index.js';
+import { CategoryController } from './category.controller.js';
 
 export function createCategoryContainer() {
   const categoryContainer = new Container();
 
   categoryContainer.bind<CategoryService>(Component.CategoryService).to(DefaultCategoryService);
   categoryContainer.bind<types.ModelType<CategoryEntity>>(Component.CategoryModel).toConstantValue(CategoryModel);
+  categoryContainer.bind<Controller>(Component.CategoryController).to(CategoryController).inSingletonScope();
 
   return categoryContainer;
 }
