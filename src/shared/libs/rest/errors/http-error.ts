beac74e5a7a692906d424725e848ee2e31ed8637
Igor Antonov <antonov.igor.khv@gmail.com>
2024-02-09 18:42:53 +0300

    6.25. Добавит ошибку `HttpError`.
    В приложении ошибки могут быть разных типов. Пока мы используем только один — базовый класс `Error`. С его помощью мы можем бросить любую ошибку. Однако, не очень удобно использовать его повсеместно. Некоторые ошибки должны содержать больше информации.
    
    Например, если у нас существует пользователь или категория, то было бы здорово просто кинуть ошибку со всей необходимой информацией. Её сможет поймать `AppExceptionFilter` и выполнить все необходимые действия. Таким образом, код обработчиков станет немного чище — повторяющиеся действия.
    
    Создадим для этого класс `HttpError` и отнаследуемся от стандартного `Error`. Этому типу ошибок добавим передачу статус-кода и дополнительное поле `detail`. В него можно записывать информацию, которая поможет установить контекст проблемы.

---
+++ src/shared/libs/rest/errors/http-error.ts
@@ -0,0 +1,12 @@
+export class HttpError extends Error {
+  public httpStatusCode!: number;
+  public detail?: string;
+
+  constructor(httpStatusCode: number, message: string, detail?: string) {
+    super(message);
+
+    this.httpStatusCode = httpStatusCode;
+    this.message = message;
+    this.detail = detail;
+  }
+}
--- src/shared/libs/rest/index.ts
+++ src/shared/libs/rest/index.ts
@@ -6,3 +6,4 @@ export * from './exception-filter/exception-filter.interface.js';
 export * from './exception-filter/app-exception-filter.js';
 export * from './types/request.params.type.js';
 export * from './types/request-body.type.js';
+export * from './errors/index.js';
