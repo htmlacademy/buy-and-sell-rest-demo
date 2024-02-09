20ee412ddbf9f4b2022589d33a5e6d0793dbde1d
Igor Antonov <antonov.igor.khv@gmail.com>
2024-02-09 18:41:38 +0300

    6.23. Добавит `asyncHandler` в `BaseController`.
    Чтобы воспользоваться `express-async-handler` нужно просто обернуть обработчик маршрута функцией `asyncHandler`. Это несложно. Вопрос, где это делать применительно к нашему проекту?
    
    Первый вариант: внутри каждого контроллера. При добавлении маршрута при помощи `addRoute` можно обернуть функцию-обработчик.
    
    Второй вариант: сделать это один раз в абстрактном классе `Controller`. Поскольку все наши обработчики подразумевают работу с асинхронными функциями, будет неплохо переложить эту задачу на `Controller`.

--- src/shared/libs/rest/controller/base-controller.abstract.ts
+++ src/shared/libs/rest/controller/base-controller.abstract.ts
@@ -1,6 +1,7 @@
 import { injectable } from 'inversify';
 import { StatusCodes } from 'http-status-codes';
 import { Response, Router } from 'express';
+import asyncHandler from 'express-async-handler';
 import { Controller } from './controller.interface.js';
 import { Logger } from '../../logger/index.js';
 import { Route } from '../types/route.interface.js';
@@ -22,7 +23,8 @@ export abstract class BaseController implements Controller {
   }
 
   public addRoute(route: Route) {
-    this._router[route.method](route.path, route.handler.bind(this));
+    const wrapperAsyncHandler = asyncHandler(route.handler.bind(this));
+    this._router[route.method](route.path, wrapperAsyncHandler);
     this.logger.info(`Route registered: ${route.method.toUpperCase()} ${route.path}`);
   }
 
