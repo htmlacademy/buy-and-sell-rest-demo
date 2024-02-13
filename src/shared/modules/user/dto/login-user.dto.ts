173c1926ce3e3f128c2410d580505a94eae00e76
Igor Antonov <antonov.igor.khv@gmail.com>
2024-02-13 19:40:14 +0300

    7.23. Добавит валидацию для `LoginUserDto`.
    Аналогичным образом поступим с `LoginUserDto`. Добавим декораторы для
    валидации и подключим `ValidationDtoMiddleware` к обработчику маршрута.

--- src/shared/modules/user/dto/login-user.dto.ts
+++ src/shared/modules/user/dto/login-user.dto.ts
@@ -1,4 +1,10 @@
+import { IsEmail, IsString } from 'class-validator';
+import { CreateLoginUserMessage } from './login-user.messages.js';
+
 export class LoginUserDto {
+  @IsEmail({}, { message: CreateLoginUserMessage.email.invalidFormat })
   public email: string;
+
+  @IsString({ message: CreateLoginUserMessage.password.invalidFormat })
   public password: string;
 }
---
+++ src/shared/modules/user/dto/login-user.messages.ts
@@ -0,0 +1,8 @@
+export const CreateLoginUserMessage = {
+  email: {
+    invalidFormat: 'email must be a valid address',
+  },
+  password: {
+    invalidFormat: 'password is required',
+  }
+} as const;
