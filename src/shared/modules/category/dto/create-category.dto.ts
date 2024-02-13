17efab0232d47b92c54ed406d28585f1e11454d7
Igor Antonov <antonov.igor.khv@gmail.com>
2024-02-13 19:40:41 +0300

    7.24. Добавит валидацию `CreateCategoryDto`.
    Аналогичным образом поступим с `CreateCategoryDto`. Добавим декораторы
    для валидации и подключим `ValidationDtoMiddleware` к обработчику
    маршрута.

--- src/shared/modules/category/dto/create-category.dto.ts
+++ src/shared/modules/category/dto/create-category.dto.ts
@@ -1,3 +1,8 @@
+import { IsString, Length } from 'class-validator';
+import { CreateCategoryMessages } from './create-category.messages.js';
+
 export class CreateCategoryDto {
+  @IsString({ message: CreateCategoryMessages.name.invalidFormat })
+  @Length(4, 12, { message: CreateCategoryMessages.name.lengthField })
   public name: string;
 }
---
+++ src/shared/modules/category/dto/create-category.messages.ts
@@ -0,0 +1,6 @@
+export const CreateCategoryMessages = {
+  name: {
+    invalidFormat: 'name is required',
+    lengthField: 'min length is 4, max is 12'
+  }
+} as const;
