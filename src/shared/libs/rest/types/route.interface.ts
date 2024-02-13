85db81e7d7c5a1962d880af617c6e00f11b4b002
Igor Antonov <antonov.igor.khv@gmail.com>
2024-02-13 18:29:22 +0300

    7.12. Расширит интерфейс `Route`.
    Теперь обсудим как зарегистрировать middleware для отдельного
    обработчика. Для этого следует собрать все middleware в массив и
    указать этот массив в качестве обработчика (для определённого Router).
    Ничего сложного. Остаётся только решить как передать список middleware
    в метод `addRoute` класса `Controller`.
    
    Для этого придётся пересмотреть и расширить интерфейс `Route`. Добавим новое свойство `middlewares` типа
    `Middleware`. В качестве типа мы указали массив, так для одного обработчика маршрута может применять несколько middlewares. Обратите внимание, само свойство `middlewares` мы пометили
    опциональным (`?`). Для некоторых обработчиков маршрутов middleware
    может не требоваться. Кроме того, если `middlewares` сделать обязательным, то придётся обновлять все ранее определённые маршруты.

--- src/shared/libs/rest/types/route.interface.ts
+++ src/shared/libs/rest/types/route.interface.ts
@@ -1,8 +1,10 @@
 import { NextFunction, Request, Response } from 'express';
 import { HttpMethod } from './http-method.enum.js';
+import { Middleware } from '../middleware/middleware.interface.js';
 
 export interface Route {
   path: string;
   method: HttpMethod;
   handler: (req: Request, res: Response, next: NextFunction) => void;
+  middlewares?: Middleware[];
 }
