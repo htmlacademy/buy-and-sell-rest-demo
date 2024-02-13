d97193875c6b3c4fc52bc9894f9f8dd544fc0088
Igor Antonov <antonov.igor.khv@gmail.com>
2024-02-13 19:44:39 +0300

    7.33. Добавит middleware `UploadFileMiddleware`.
    Обработка запроса с целью извлечения файлов выполним в отдельной
    middleware. Создадим `UploadFileMiddleware`. Не забудьте, middleware должна имплементировать интерфейс `Middleware`.
    
    В методе `execute` реализуем создание экземпляра `diskStorage`.
    
    `multer` абстрагируется от места хранения файлов. Для этого он предоставляет абстракцию в виде хранилища (`storage`). Из коробки доступно два варианта хранилищ: `diskStorage` и `memoryStorage`. Мы решили сохранять файлы на диске, поэтому воспользуемся первым вариантом.
    
    Создадим хранилище и заполним свойства `destination` и `filename`. В первом следует указать путь для сохранения файлов. Путь будем передавать через конструктор (`uploadDirectory`). А вот свойство `filename` более сложное. Ему мы можем задать функцию, в которой можно
    реализовать свой алгоритм для именования полученных файлов.
    
    По умолчанию `multer` сохраняет все файлы без расширения, а в качестве имени использует уникальную строку. Мы можем переопределить это поведение. Расширение получим на основании `file.mimetype`, а в качестве имени воспользуемся уникальной строкой, полученной с помощью `randomUUID()` из встроенного модуля `crypto`.
    
    Последним шагом воспользуемся объектом `multer` и получим готовую middleware, сохраним её в `uploadSingleFileMiddleware``. Затем исполним.

---
+++ src/shared/libs/rest/middleware/upload-file.middleware.ts
@@ -0,0 +1,28 @@
+import { NextFunction, Request, Response } from 'express';
+import multer, { diskStorage } from 'multer';
+import { extension } from 'mime-types';
+import * as crypto from 'node:crypto';
+import { Middleware } from './middleware.interface.js';
+
+export class UploadFileMiddleware implements Middleware {
+  constructor(
+    private uploadDirectory: string,
+    private fieldName: string,
+  ) {}
+
+  public async execute(req: Request, res: Response, next: NextFunction): Promise<void> {
+    const storage = diskStorage({
+      destination: this.uploadDirectory,
+      filename: (_req, file, callback) => {
+        const fileExtention = extension(file.mimetype);
+        const filename = crypto.randomUUID();
+        callback(null, `${filename}.${fileExtention}`);
+      }
+    });
+
+    const uploadSingleFileMiddleware = multer({ storage })
+      .single(this.fieldName);
+
+    uploadSingleFileMiddleware(req, res, next);
+  }
+}
