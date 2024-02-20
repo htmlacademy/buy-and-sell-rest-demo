7d85f691949b9f10cc156d1599894deebe4b6521
Igor Antonov <antonov.igor.khv@gmail.com>
2024-02-20 18:39:39 +0300

    8.7. Подготовим контейнер зависимостей для модуля `Auth`. WIP
    Теперь подготовим инфраструктуру для нового модуля `Auth`. Соберём все необходимые зависимости в контейнере, внедрим нужные компоненты в `RestApplication`, зарегистрируем новый фильтр исключений, добавим компоненты в `Component` и так далее.

--- src/rest/rest.application.ts
+++ src/rest/rest.application.ts
@@ -20,6 +20,7 @@ export class RestApplication {
     @inject(Component.UserController) private readonly userController: Controller,
     @inject(Component.OfferController) private readonly offerController: Controller,
     @inject(Component.CommentController) private readonly commentController: Controller,
+    @inject(Component.AuthExceptionFilter) private readonly authExceptionFilter: ExceptionFilter,
   ) {
     this.server = express();
   }
@@ -57,6 +58,7 @@ export class RestApplication {
   }
 
   private async _initExceptionFilters() {
+    this.server.use(this.authExceptionFilter.catch.bind(this.authExceptionFilter));
     this.server.use(this.appExceptionFilter.catch.bind(this.appExceptionFilter));
   }
 
--- src/shared/types/component.enum.ts
+++ src/shared/types/component.enum.ts
@@ -16,4 +16,6 @@ export const Component = {
   UserController: Symbol.for('UserController'),
   OfferController: Symbol.for('OfferController'),
   CommentController: Symbol.for('CommentController'),
+  AuthService: Symbol.for('AuthService'),
+  AuthExceptionFilter: Symbol.for('AuthExceptionFilter'),
 } as const;
