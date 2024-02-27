95c42d70b3df0566e046f672e1d76296f6a0cbdd
Igor Antonov <antonov.igor.khv@gmail.com>
2024-02-27 18:56:55 +0300

    9.19. Добавит DTO для клиента.

---
+++ frontend/project/src/dto/user/user-with-token.dto.ts
@@ -0,0 +1,11 @@
+export default class UserWithTokenDto {
+  public email!: string ;
+
+  public avatarPath!: string;
+
+  public firstname!: string;
+
+  public lastname!: string;
+
+  public token!: string;
+}
