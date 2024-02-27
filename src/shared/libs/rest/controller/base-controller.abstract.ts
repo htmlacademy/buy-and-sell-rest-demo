9fd5c95d21f6389791488cb21be8147833d417f2
Igor Antonov <antonov.igor.khv@gmail.com>
2024-02-27 18:48:40 +0300

    9.10. Подключит `PathTransformer` в `BaseController`.
    А как воспользоваться `PathTransformer` для выполнения преобразований? Можно подключить его в каждый контроллер и прогонять отправляемые данные.
    
    Можно пойти более хитрым путём и имплементировать паттерн «Interceptor». То есть сделать абстракцию, которая позволит выполнять код до фактической отправки клиенту.
    
    Компромисным вариантом может стать модификация метода `send` в абстрактном контроллере. Именно он вызывается каждый раз, когда мы собираемся отправить данные клиенту.

--- src/shared/libs/rest/controller/base-controller.abstract.ts
+++ src/shared/libs/rest/controller/base-controller.abstract.ts
@@ -1,16 +1,21 @@
-import { injectable } from 'inversify';
+import { inject, injectable } from 'inversify';
 import { StatusCodes } from 'http-status-codes';
 import { Response, Router } from 'express';
 import asyncHandler from 'express-async-handler';
 import { Controller } from './controller.interface.js';
 import { Logger } from '../../logger/index.js';
 import { Route } from '../types/route.interface.js';
+import { PathTransformer } from '../transform/path-transformer.js';
+import { Component } from '../../../types/index.js';
 
 const DEFAULT_CONTENT_TYPE = 'application/json';
 
 @injectable()
 export abstract class BaseController implements Controller {
   private readonly _router: Router;
+
+  @inject(Component.PathTransformer)
+  private pathTranformer: PathTransformer;
 
   constructor(
     protected readonly logger: Logger
@@ -34,10 +39,11 @@ export abstract class BaseController implements Controller {
   }
 
   public send<T>(res: Response, statusCode: number, data: T): void {
+    const modifiedData = this.pathTranformer.execute(data as Record<string, unknown>);
     res
       .type(DEFAULT_CONTENT_TYPE)
       .status(statusCode)
-      .json(data);
+      .json(modifiedData);
   }
 
   public created<T>(res: Response, data: T): void {
