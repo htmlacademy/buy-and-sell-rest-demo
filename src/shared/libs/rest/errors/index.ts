81f25b4c9e221488bd33fbd4b3875a6b03cd950d
Igor Antonov <antonov.igor.khv@gmail.com>
2024-02-27 18:34:18 +0300

    9.2. Добавит ошибку `ValidationError`.
    Для ошибок валидации заведём отдельный тип ошибок — `ValidationError`. Вместо обычной `HttpError` воспользуемся `ValidationError` в `ValidateDtoMiddleware`. В ней же воспользуемся функцией для трансформации ошибок.

--- src/shared/libs/rest/errors/index.ts
+++ src/shared/libs/rest/errors/index.ts
@@ -1,1 +1,2 @@
 export * from './http-error.js';
+export * from './validation.error.js';
--- src/shared/libs/rest/middleware/validate-dto.middleware.ts
+++ src/shared/libs/rest/middleware/validate-dto.middleware.ts
@@ -1,19 +1,19 @@
 import { NextFunction, Request, Response } from 'express';
 import { ClassConstructor, plainToInstance } from 'class-transformer';
 import { validate } from 'class-validator';
-import { StatusCodes } from 'http-status-codes';
 import { Middleware } from './middleware.interface.js';
+import { ValidationError } from '../errors/index.js';
+import { reduceValidationErrors } from '../../../helpers/index.js';
 
 export class ValidateDtoMiddleware implements Middleware {
   constructor(private dto: ClassConstructor<object>) {}
 
-  public async execute({ body }: Request, res: Response, next: NextFunction): Promise<void> {
+  public async execute({ body, path }: Request, _res: Response, next: NextFunction): Promise<void> {
     const dtoInstance = plainToInstance(this.dto, body);
     const errors = await validate(dtoInstance);
 
     if (errors.length > 0) {
-      res.status(StatusCodes.BAD_REQUEST).send(errors);
-      return;
+      throw new ValidationError(`Validation error: ${path}`, reduceValidationErrors(errors));
     }
 
     next();
