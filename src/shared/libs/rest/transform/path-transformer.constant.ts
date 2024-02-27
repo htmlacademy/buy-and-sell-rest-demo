bff73acc94e1aba2586fe34781b78bc314fff6e2
Igor Antonov <antonov.igor.khv@gmail.com>
2024-02-27 18:51:10 +0300

    9.11. Добавит статичные изображения для категорий.
    Решим ещё одну задачу с изображениями. Согласно техническому заданию, категории тоже обладают изображениями. Их не может загружать клиент, все изображения для категорий статичны. Их можно взять из директории с макетом.
    
    Выберем шесть изображений для категорий и скопируем их в директорию со статичными файлами. Поскольку эти изображения размещаются в директории `static`, добавим имена этих файлов в `DEFAULT_STATIC_IMAGES`, чтобы потом предоставить корректный путь.

--- src/shared/libs/rest/transform/path-transformer.constant.ts
+++ src/shared/libs/rest/transform/path-transformer.constant.ts
@@ -1,5 +1,11 @@
 export const DEFAULT_STATIC_IMAGES = [
   'default-avatar.jpg',
+  'cat-auto.jpg',
+  'cat-books.jpg',
+  'cat-clothes.jpg',
+  'cat-games.jpg',
+  'cat-other.jpg',
+  'cat-sport.jpg',
 ];
 
 export const STATIC_RESOURCE_FIELDS = [
