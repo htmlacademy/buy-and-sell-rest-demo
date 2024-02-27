dc4c8e9ed9c436f2d5a6df7210f2f1c141b6f9c5
Igor Antonov <antonov.igor.khv@gmail.com>
2024-02-27 18:40:40 +0300

    9.7. Добавит директорию для хранения статики.
    Для хранения статичных ресурсов потребуется отдельная директория. В ней будем хранить изображения для категорий, аватар по умолчанию и все ресурсы, которые не будут изменяться. Определим в конфигурации новую
    переменную `STATIC_DIRECTORY_PATH`. По умолчанию такой директорией будет `static`. Зарегистрируем её с помощью `express.static` и разместим в директории изображение для аватара по умолчанию.

--- src/shared/libs/config/rest.schema.ts
+++ src/shared/libs/config/rest.schema.ts
@@ -14,6 +14,7 @@ export type RestSchema = {
   UPLOAD_DIRECTORY: string;
   JWT_SECRET: string;
   HOST: string;
+  STATIC_DIRECTORY_PATH: string;
 }
 
 export const configRestSchema = convict<RestSchema>({
@@ -76,5 +77,11 @@ export const configRestSchema = convict<RestSchema>({
     format: String,
     env: 'HOST',
     default: 'localhost'
+  },
+  STATIC_DIRECTORY_PATH: {
+    doc: 'Path to directory with static resources',
+    format: String,
+    env: 'STATIC_DIRECTORY_PATH',
+    default: 'static'
   },
 });
