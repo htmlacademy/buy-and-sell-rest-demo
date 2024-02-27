bb2ec00e030ee937fa35332f9e42f775d7adca55
Igor Antonov <antonov.igor.khv@gmail.com>
2024-02-27 18:55:17 +0300

    9.17. Подключит пакет CORS в качестве middleware.
    Зарегистрируем middleware `cors` в качестве глобальной middleware.

--- src/rest/rest.application.ts
+++ src/rest/rest.application.ts
@@ -1,5 +1,6 @@
 import { inject, injectable } from 'inversify';
 import express, { Express } from 'express';
+import cors from 'cors';
 import { Logger } from '../shared/libs/logger/index.js';
 import { Config, RestSchema } from '../shared/libs/config/index.js';
 import { Component } from '../shared/types/index.js';
@@ -65,6 +66,7 @@ export class RestApplication {
       express.static(this.config.get('STATIC_DIRECTORY_PATH'))
     );
     this.server.use(authenticateMiddleware.execute.bind(authenticateMiddleware));
+    this.server.use(cors());
   }
 
   private async _initExceptionFilters() {
