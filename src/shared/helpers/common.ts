5cb677adf9781dee2094ecddbb59b6248963f673
Igor Antonov <antonov.igor.khv@gmail.com>
2024-02-27 18:35:18 +0300

    9.3. Добавит `ValidationExceptionFilter`. WIP
    Добавим отдельный `ExceptionFilter` для обработки ошибок типа `ValidationError`. Эту обработку можно сделать
    в существующем фильтре, но тогда он станет большим. К тому же, для обработки отдельных ошибок может требоваться больше действий.
    
    WIP: Новый фильтр исключений пока не задействован.

--- src/shared/helpers/common.ts
+++ src/shared/helpers/common.ts
@@ -1,6 +1,6 @@
 import { ClassConstructor, plainToInstance } from 'class-transformer';
 import { ValidationError } from 'class-validator';
-import { ValidationErrorField } from '../libs/rest/index.js';
+import { ApplicationError, ValidationErrorField } from '../libs/rest/index.js';
 
 export function generateRandomValue(min:number, max: number, numAfterDigit = 0) {
   return +((Math.random() * (max - min)) + min).toFixed(numAfterDigit);
@@ -24,10 +24,8 @@ export function fillDTO<T, V>(someDto: ClassConstructor<T>, plainObject: V) {
   return plainToInstance(someDto, plainObject, { excludeExtraneousValues: true });
 }
 
-export function createErrorObject(message: string) {
-  return {
-    error: message,
-  };
+export function createErrorObject(errorType: ApplicationError, error: string, details: ValidationErrorField[] = []) {
+  return { errorType, error, details };
 }
 
 export function reduceValidationErrors(errors: ValidationError[]): ValidationErrorField[] {
